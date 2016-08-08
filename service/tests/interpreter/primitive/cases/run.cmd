992ce224aeabcf25a0bdff8c92c810de78fe4ba6
Hzj_jie <hzj_jie@hotmail.com>
2016-08-08 15:19:08 -0700

    simualtor_cases -> simulator_cases, and update .gitignore

--- service/tests/interpreter/primitive/cases/run.cmd
+++ service/tests/interpreter/primitive/cases/run.cmd
@@ -1,6 +1,6 @@
 
 setlocal
 path %PATH%;..\..\..\..\resource\gen;
-gen _simulator_cases sim1 sim1.txt sim2 sim2.txt > simualtor_cases.vb
+gen _simulator_cases sim1 sim1.txt sim2 sim2.txt > simulator_cases.vb
 endlocal
 
