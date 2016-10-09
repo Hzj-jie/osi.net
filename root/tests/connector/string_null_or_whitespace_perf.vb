c403b0f9d5546d33327a5963e349fe5032e46daf
Hzj_jie <hzj_jie@hotmail.com>
2016-10-09 14:13:29 -0700

    Revert last change

---
+++ root/tests/connector/string_null_or_whitespace_perf.vb
@@ -0,0 +1,10 @@
+﻿
+Imports osi.root.utt
+
+Public Class string_null_or_whitespace_perf
+    Inherits exec_export_case
+
+    Public Sub New()
+        MyBase.New(string_null_or_whitespace_perf_exe)
+    End Sub
+End Class
