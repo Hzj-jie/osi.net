fa8705dfea6df4562b97e707d7532176565ea3a8
Hzj_jie <hzj_jie@hotmail.com>
2016-10-26 01:25:51 -0700

    Several minor changes to ensure transmitter and its test project can build correctly.

--- service/tests/transmitter/My Project/AssemblyInfo.vb
+++ service/tests/transmitter/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("transmitter")> 
+<Assembly: AssemblyTitle("osi.tests.service.transmitter")>
 <Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("- Gemini -")> 
-<Assembly: AssemblyProduct("transmitter")> 
+<Assembly: AssemblyCompany("- Gemini -")>
+<Assembly: AssemblyProduct("osi.tests.service.transmitter")>
 <Assembly: AssemblyCopyright("Copyright © - Gemini - 2016")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("cd081551-3ac5-4e16-bc17-d1f91d42beb5")> 
+<Assembly: Guid("cd081551-3ac5-4e16-bc17-d1f91d42beb5")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.*")>
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
