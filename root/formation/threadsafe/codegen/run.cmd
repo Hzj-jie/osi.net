554fdcef837506d12816f3b0809d830c8d238892
Hzj_jie <hzj_jie@hotmail.com>
2016-11-03 23:22:57 -0700

    waitable_slimheapless with tests

--- root/formation/threadsafe/codegen/run.cmd
+++ root/formation/threadsafe/codegen/run.cmd
@@ -3,5 +3,7 @@ setlocal
 PATH %PATH%;..\..\..\codegen\precompile;
 precompile.exe qless2.vbp > qless2.vb
 precompile.exe heapless.vbp > heapless.vb
+precompile.exe waitable_slimqless2.vbp > waitable_slimqless2.vb
+precompile.exe waitable_slimheapless.vbp > waitable_slimheapless.vb
 endlocal
 
