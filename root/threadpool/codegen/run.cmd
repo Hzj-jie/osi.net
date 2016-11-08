652f977d8dd57ee252e72eabfb00abb75b63ae4d
Hzj_jie <hzj_jie@hotmail.com>
2016-11-08 03:41:21 -0800

    slimqless2_runner now becomes a precompiled file

--- root/threadpool/codegen/run.cmd
+++ root/threadpool/codegen/run.cmd
@@ -5,5 +5,7 @@ PATH %PATH%;..\..\codegen\precompile\;
 precompile iqless_threadpool.vbp > slimqless2_threadpool.vb
 precompile qless_threadpool.vbp > qless_threadpool.vb
 precompile slimheapless_threadpool.vbp > slimheapless_threadpool.vb
+precompile slimqless2_runner.vbp > slimqless2_runner.vb
+precompile slimheapless_runner.vbp > slimheapless_runner.vb
 
 endlocal
