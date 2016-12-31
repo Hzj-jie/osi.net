ca67fac4c71cc1a93504ddccc881471ab8f1e825
Hzj_jie <hzj_jie@hotmail.com>
2016-12-31 02:23:55 -0800

    To .Net 4.0 !

---
+++ service/device/share_device/shared_device.checker.vb
@@ -0,0 +1,6 @@
+﻿
+#If TODO Then
+Partial Public Class shared_device(Of PORT_T, ADDRESS_T, COMPONENT_T, DATA_T, PARAMETER_T)
+
+End Class
+#End If
---
+++ service/device/share_device/shared_device.vb
@@ -0,0 +1,23 @@
+﻿
+#If TODO Then
+Imports osi.root.connector
+Imports osi.service.selector
+
+' Share one local sharable device with several devices.
+' Refer to osi.service.selector.shared_component(Of PORT_T, ADDRESS_T, COMPONENT_T, DATA_T, PARAMETER_T)
+Partial Public Class shared_device(Of PORT_T, ADDRESS_T, COMPONENT_T, DATA_T, PARAMETER_T)
+    Private ReadOnly sc As shared_component(Of PORT_T, ADDRESS_T, idevice(Of COMPONENT_T), DATA_T, PARAMETER_T)
+
+    ' A shared_device is always created by shared_device.creator, shared_device.auto_exporter or
+    ' shared_device.manual_exporter.
+    Private Sub New(ByVal sc As shared_component(Of PORT_T, ADDRESS_T, idevice(Of COMPONENT_T), DATA_T, PARAMETER_T))
+        assert(Not sc Is Nothing)
+        Me.sc = sc
+    End Sub
+
+    Public Shared Operator +(ByVal this As shared_device(Of PORT_T, ADDRESS_T, COMPONENT_T, DATA_T, PARAMETER_T)) As _
+                            idevice(Of shared_device(Of PORT_T, ADDRESS_T, COMPONENT_T, DATA_T, PARAMETER_T))
+
+    End Operator
+End Class
+#End If
