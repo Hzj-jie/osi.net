c93be3095bdb11ff7981820d42e881030ff11c79
Hzj_jie <hzj_jie@hotmail.com>
2017-01-01 01:49:21 -0800

    threadpool_synchronize_inovke and queue_runner_synchronize_invoke should be singleton

--- root/threadpool/threadpool_synchronize_invoke.vb
+++ root/threadpool/threadpool_synchronize_invoke.vb
@@ -4,6 +4,16 @@ Imports osi.root.connector
 Public NotInheritable Class threadpool_synchronize_invoke
     Inherits synchronize_invoke
 
+    Public Shared ReadOnly instance As threadpool_synchronize_invoke
+
+    Shared Sub New()
+        instance = New threadpool_synchronize_invoke()
+    End Sub
+
+    Private Sub New()
+        MyBase.New()
+    End Sub
+
     Protected Overrides Sub push(ByVal v As Action)
         assert(thread_pool().queue_job(v))
     End Sub
--- root/utils/queue_runner/queue_runner_synchronize_invoke.vb
+++ root/utils/queue_runner/queue_runner_synchronize_invoke.vb
@@ -4,6 +4,16 @@ Imports osi.root.connector
 Public NotInheritable Class queue_runner_synchronize_invoke
     Inherits synchronize_invoke
 
+    Public Shared ReadOnly instance As queue_runner_synchronize_invoke
+
+    Shared Sub New()
+        instance = New queue_runner_synchronize_invoke()
+    End Sub
+
+    Private Sub New()
+        MyBase.New()
+    End Sub
+
     Protected Overrides Sub push(ByVal v As Action)
         assert(queue_runner.once(v))
     End Sub
