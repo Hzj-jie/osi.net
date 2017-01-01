91336405f45980c23f2779cde6b55cc359520aac
Hzj_jie <hzj_jie@hotmail.com>
2016-12-31 19:57:18 -0800

    Missing file

---
+++ root/tests/connector/managed_threadpool_behavior_test.vb
@@ -0,0 +1,50 @@
+﻿
+Imports System.Threading
+Imports osi.root.connector
+Imports osi.root.lock
+Imports osi.root.utt
+
+Public Class managed_threadpool_behavior_test
+    Inherits [case]
+
+    Private Shared Function queue_in_background_io_thread_case() As Boolean
+        Const size As Int32 = 1024
+        Dim running As atomic_int = Nothing
+        running = New atomic_int()
+        running.set(size)
+        Dim finished As AutoResetEvent = Nothing
+        finished = New AutoResetEvent(False)
+        For i As Int32 = 0 To size - 1
+            queue_in_managed_threadpool(Sub()
+                                            assert_true(Thread.CurrentThread().IsThreadPoolThread())
+                                            assert_true(Thread.CurrentThread().IsBackground())
+                                            If running.decrement() = 0 Then
+                                                assert(finished.force_set())
+                                            End If
+                                        End Sub)
+        Next
+        assert(finished.wait())
+        finished.Close()
+        Return True
+    End Function
+
+    Private Shared Function new_in_worker_thread_case() As Boolean
+        Dim finished As AutoResetEvent = Nothing
+        finished = New AutoResetEvent(False)
+        For i As Int32 = 0 To 1024 - 1
+            assert(start_thread(Nothing,
+                                Sub()
+                                    assert_false(Thread.CurrentThread().IsBackground())
+                                    assert(finished.force_set())
+                                End Sub))
+            assert(finished.wait())
+        Next
+        finished.Close()
+        Return True
+    End Function
+
+    Public Overrides Function run() As Boolean
+        Return queue_in_background_io_thread_case() AndAlso
+               new_in_worker_thread_case()
+    End Function
+End Class
