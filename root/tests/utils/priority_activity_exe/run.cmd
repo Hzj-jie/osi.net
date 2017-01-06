05b94cc73babeb7b20460609218a12c9d803c599
Hzj_jie <hzj_jie@hotmail.com>
2017-01-06 03:04:31 -0800

    priority_activity should be updated to match .Net version

--- root/tests/utils/priority_activity_exe/run.cmd
+++ root/tests/utils/priority_activity_exe/run.cmd
@@ -1,6 +1,9 @@
 
 setlocal
+msbuild /p:Configuration=Release
+move /Y bin\Release\* .
 path %PATH%;..\..\..\..\service\resource\gen
 zipgen.exe _priority_activity priority_activity_exe priority_activity.exe priority_activity_pdb priority_activity.pdb > priority_activity_exe.vb
+move /Y priority_activity_exe.vb ..\
 endlocal
 
