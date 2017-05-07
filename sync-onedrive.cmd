7d01505f93644fe63c31ab0f51a86187882470bf
Hzj_jie <hzj_jie@hotmail.com>
2017-05-07 16:21:01 -0700

    Copy only log folders

--- sync-onedrive.cmd
+++ sync-onedrive.cmd
@@ -1,5 +1,9 @@
 
 for /l %%x in (0,0,1) do (
-  robocopy f:\ %UserProfile%\OneDrive /MIR
+  robocopy f:\osi-root-utt-log\1-core %UserProfile%\OneDrive\osi-root-utt-log\1-core /MIR
+  robocopy f:\osi-root-utt-log\2-cores %UserProfile%\OneDrive\osi-root-utt-log\2-cores /MIR
+  robocopy f:\osi-root-utt-log\4-cores %UserProfile%\OneDrive\osi-root-utt-log\4-cores /MIR
+  robocopy f:\osi-root-utt-log\12-cores %UserProfile%\OneDrive\osi-root-utt-log\12-cores /MIR
+  robocopy f:\osi-root-utt-log\12-cores-deploys %UserProfile%\OneDrive\osi-root-utt-log\12-cores-deploys /MIR
   ping -n 301 127.0.0.1 > nul
 )
