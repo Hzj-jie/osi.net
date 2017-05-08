Hzj-jie/osi.net

commit ed2190d45874f4a764fdc111ecbc461156ba54d8

Ensure raise_error messages can be outputted to the console before global_init()

root/connector/error_handler/error_event.vb
@@ -43,31 +43,46 @@ Public Class error_event
                         ByVal err_type_char As Char,
                         ByVal msg() As Object,
                         ByVal additional_jump As Int32)
+        Dim r1a As Boolean = False
         Dim r2a As Boolean = False
         Dim r3a As Boolean = False
         Dim r4a As Boolean = False
+        Dim r5a As Boolean = False
         Dim r6a As Boolean = False
+        r1a = event_attached(R1Event)
         r2a = event_attached(R2Event)
         r3a = event_attached(R3Event)
         r4a = event_attached(R4Event)
+        r5a = event_attached(R5Event)
         r6a = event_attached(R6Event)
+        Dim unattached As Boolean = False
+        unattached = Not (r1a OrElse r2a OrElse r3a OrElse r4a OrElse r5a OrElse r6a)
 
         err_type_char = Char.ToLower(err_type_char)
 
-        SyncLock r1lock
-            RaiseEvent R1(err_type, err_type_char, msg, additional_jump + 1)
-        End SyncLock
         Dim merged_msg As String = Nothing
-        If r2a OrElse r3a OrElse r4a OrElse r6a Then
+        If unattached OrElse r2a OrElse r3a OrElse r4a OrElse r6a Then
             merged_msg = error_message.P(msg)
         End If
         Dim full_msg As String = Nothing
-        If r3a OrElse r4a OrElse r6a Then
+        If unattached OrElse r3a OrElse r4a OrElse r6a Then
             If merged_msg Is Nothing Then
                 assert_break()
             End If
             full_msg = error_message.P(err_type, err_type_char, merged_msg, additional_jump + 1)
         End If
+
+        If unattached Then
+            If full_msg Is Nothing Then
+                assert_break()
+            End If
+            ' Before global_init(), no event is attached, we at least should print the message to console.
+            write_console_line(full_msg)
+        End If
+
+        SyncLock r1lock
+            RaiseEvent R1(err_type, err_type_char, msg, additional_jump + 1)
+        End SyncLock
         If r2a Then
             If merged_msg Is Nothing Then
                 assert_break()

root/envs/timeslice.vb
@@ -42,26 +42,26 @@ Public Module _timeslice
     End Function
 
     Private Function decide_timeslice_length_ticks() As Int64
-        Dim timesliceLengthTicks As Int64 = 0
+        Dim timeslice_length_ticks As Int64 = 0
         Const count As Int32 = 8
-        Dim thisround As Int64 = 0
-        thisround = default_timeslice_length_ticks
+        Dim this_round As Int64 = 0
+        this_round = default_timeslice_length_ticks
         For i As Int32 = 0 To count - 1
             sleep(0)
             Dim startticks As Int64 = 0
             startticks = nowadays.high_res_ticks()
-            sleep_ticks(decide_timeslice_length_ticks_sleep_ticks(thisround))
-            thisround = (nowadays.high_res_ticks() - startticks + thisround) >> 1
-            timesliceLengthTicks += thisround
+            sleep_ticks(decide_timeslice_length_ticks_sleep_ticks(this_round))
+            this_round = (nowadays.high_res_ticks() - startticks + this_round) >> 1
+            timeslice_length_ticks += this_round
         Next
-        timesliceLengthTicks \= count
-        If timesliceLengthTicks <= milliseconds_to_ticks(1) Then
-            timesliceLengthTicks = default_timeslice_length_ticks
+        timeslice_length_ticks \= count
+        If timeslice_length_ticks <= milliseconds_to_ticks(1) Then
+            timeslice_length_ticks = default_timeslice_length_ticks
             raise_error("cannot determined length of timeslice, use default value as ",
                           ticks_to_milliseconds(default_timeslice_length_ticks), "ms.")
         End If
 
-        Return timesliceLengthTicks
+        Return timeslice_length_ticks
     End Function
 
     Sub New()
@@ -85,6 +85,8 @@ Public Module _timeslice
         eight_timeslice_length_ms = to_ms(eight_timeslice_length_ticks)
         sixteen_timeslice_length_ms = to_ms(sixteen_timeslice_length_ticks)
 
+        ' This function is usually executed before global_init(), so using raise_error() does not output the result to
+        ' file.
         If env_bool(env_keys("report", "timeslice", "length")) Then
             raise_error("timeslice length in ticks = ", timeslice_length_ticks)
         End If

commit 9e340c9b53098257a6a5b5f67feaa69628ae6e90

priority_activity_test is broken by change ed2190d45874f4a764fdc111ecbc461156ba54d8

root/connector/error_handler/error_event.vb
@@ -1,4 +1,8 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.constants
 
 Public Class error_event
@@ -78,6 +82,7 @@ Public Class error_event
             End If
             ' Before global_init(), no event is attached, we at least should print the message to console.
             write_console_line(full_msg)
+            Return
         End If
 
         SyncLock r1lock

root/utils/shell_less_process.vb
@@ -1,4 +1,8 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports System.ComponentModel
 Imports System.Diagnostics
 Imports System.IO
@@ -163,6 +167,10 @@ Public NotInheritable Class shell_less_process
 
     Public Function wait_for_exit(ByVal wait_ms As Int32) As Boolean
         Return proc().WaitForExit(If(wait_ms < 0, max_int32, wait_ms))
+    End Function
+
+    Public Function wait_for_exit(ByVal wait_ms As Int64) As Boolean
+        Return wait_for_exit(If(wait_ms >= max_int32, max_int32, CInt(wait_ms)))
     End Function
 
     Public Sub wait_for_exit()

service/resource/gen/gen.array.cs
@@ -66,6 +66,10 @@ public static class gen
         else
         {
             w();
+            w("Option Explicit On");
+            w("Option Infer Off");
+            w("Option Strict On");
+            w();
             w("\'this file is generated by /osi/service/resource/gen/gen.exe with");
             w("\'" + merge_args(args));
             w("\'so change /osi/service/resource/gen/gen.cs or resource files instead of this file");

service/resource/gen/gen.base64.cs
@@ -91,6 +91,10 @@ public static class gen
         else
         {
             w();
+            w("Option Explicit On");
+            w("Option Infer Off");
+            w("Option Strict On");
+            w();
             w("\'this file is generated by /osi/service/resource/gen/gen.exe with");
             w("\'" + merge_args(args));
             w("\'so change /osi/service/resource/gen/gen.cs or resource files instead of this file");

service/resource/gen/gen.cs
@@ -66,6 +66,10 @@ public static class gen
         else
         {
             w();
+            w("Option Explicit On");
+            w("Option Infer Off");
+            w("Option Strict On");
+            w();
             w("\'this file is generated by /osi/service/resource/gen/gen.exe with");
             w("\'" + merge_args(args));
             w("\'so change /osi/service/resource/gen/gen.cs or resource files instead of this file");

service/resource/gen/gen.vector.cs
@@ -66,6 +66,10 @@ public static class gen
         else
         {
             w();
+            w("Option Explicit On");
+            w("Option Infer Off");
+            w("Option Strict On");
+            w();
             w("\'this file is generated by /osi/service/resource/gen/gen.exe with");
             w("\'" + merge_args(args));
             w("\'so change /osi/service/resource/gen/gen.cs or resource files instead of this file");

service/resource/gen/zipgen.array.cs
@@ -67,6 +67,10 @@ public static class zipgen
         else
         {
             w();
+            w("Option Explicit On");
+            w("Option Infer Off");
+            w("Option Strict On");
+            w();
             w("\'this file is generated by /osi/service/resource/zipgen/zipgen.exe with");
             w("\'" + merge_args(args));
             w("\'so change /osi/service/resource/gen/gen.cs or resource files instead of this file");

service/resource/gen/zipgen.base64.cs
@@ -92,6 +92,10 @@ public static class zipgen
         else
         {
             w();
+            w("Option Explicit On");
+            w("Option Infer Off");
+            w("Option Strict On");
+            w();
             w("\'this file is generated by /osi/service/resource/zipgen/zipgen.exe with");
             w("\'" + merge_args(args));
             w("\'so change /osi/service/resource/gen/gen.cs or resource files instead of this file");

service/resource/gen/zipgen.cs
@@ -67,6 +67,10 @@ public static class zipgen
         else
         {
             w();
+            w("Option Explicit On");
+            w("Option Infer Off");
+            w("Option Strict On");
+            w();
             w("\'this file is generated by /osi/service/resource/zipgen/zipgen.exe with");
             w("\'" + merge_args(args));
             w("\'so change /osi/service/resource/gen/gen.cs or resource files instead of this file");

root/tests/utils/priority_activity_exe.vb
@@ -1,4 +1,8 @@
 
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 'this file is generated by /osi/service/resource/zipgen/zipgen.exe with
 'priority_activity_exe priority_activity_pdb
 'so change /osi/service/resource/gen/gen.cs or resource files instead of this file
@@ -12,12 +16,12 @@ Friend Module _priority_activity
     Public ReadOnly priority_activity_pdb() As Byte
 
     Sub New()
-        priority_activity_exe = Convert.FromBase64String(strcat_hint(CUInt(2796), _
-        "H4sIAAAAAAAEAO1Xb2wcRxV/u/Y59qW52vlj0pCm23MobhJvfDknOKmd2PE5zoHtGJ/jFnrSZW9vfB5nb2czs3fxVSUKSEArgSh/BKFCqkCVoKqEEEJqRISQEB9AoigqINEvpXwBxIe2H1A/oIrwZnZv72wfSkBCSMCc9zczb968ee/NzJvnuY8/Bx0A0InfnTsANyEoE3D3ch2/xEM/TMAPel59+KY2++rDS6tUGB5nZW5VDNtyXeYbRWLwqmtQ18hcyBkVViLmjh3xg6GMhWmAWa0Dnq6zJxpy34QkbNeGAeLY6Q5obw0iGA3FJoK2HugN0KyVUnrQ7ICJTwP0qr9mHVWqfAPlXghFvhdrY+QlgPuwKn0QYP89+CQqRqS6Kt3YP9/SN32y7mP9l+7QrnhT7xYRl0wuuA2hbqgjdEGgUEtB8oTJicPscOhSKGvnFr6zm9W8PRjU59WUGHwFF72Na2igvq57sHRD2TWsw/Fgbt/uuD6I9eEj3RwrL364U+9/Ps63Y7trEDch/kh//2Af1oM7JeySlEM492zuw2c1pUFgT23EHDbTw+nUSUmJgYN4BO0buAZwA+vbsp3zOXXLQnKsxgKXDlzMwTOxYL8HZi5mM1jfwP4oih4467BiqDNO12b26NAj9+KvWhr6A/t3h24MfQGoICRa+p1hDfAtLai7oFN7TO+CKwpHoUe/H57UJf0SvKF1wR8V/kzhE5rEgwrHFV5V9FnYj3MPKXxZUb4ENxE/p7+mxeHb+rtaAr6mXdN3wpd1hjx74TW1eKCB9NsntV64hatOqt6CdAV8FV7XZkLN1/QeeAc17kPsggcQ4/AoYi+kFJ5UOKkwq/CjCj+m0ELcA1S1ryisK3wWvqsfgC/CC/oh+Dp8Ae/vN3EXTiDuglNgwnaYQ9wFy4jvhyriAFxDPIzcJqQVPqZwStE/Ai8g5hTlSYU2vIJ4GX6PKOAt2Aed1xt2N8qtlpggy2l1sQKu6b0v7g6o70Yem9mb2xPQrulbaUzRHoJHgu7YHCtVHXIaKsJm3KFFWCRWCaaYK5hD4HFOfTJLXQIzVVqa9PFEFqs+gQwpVstlq+iQJm2KVZapoBtok0KQStGpL1G/LZlbJVKx+OXm0JLFy8Q/h9GWXGWtA40556hDlgkXlLlbB1HvFVqucstvO5whwubU2ziIenvUUTMWiWOtq5bYOnmBo6tsv92iXp3T8mrboYpnufXmwGLV9WmFKLpPi9ShfssoWSe2rAvErRVqllMl4HHKcBPqBcv2aQ0bJjJBri58UjFDaWboD4wW4QjMWdSNuMiKQ2xpFZi2zzgwQU3OmG/azHWJIoWsGWqVXSZ8aovNa2Rdn3Dm5QivUZtsGQ68SHg0HhwRVAlPGHZxW0Vz4apPnZYumivgQnENdYEyRjHLKVCX+iCIL10BBYtzqw4Uner59aCzxTHqPPdkoYTRlGCs9oDjnWYKfQiGbzzzp4lf5j4x/fnPvNj3qz98/3fQaWhad4cBWgwbfX2ym5Cgd2zTEtt6sfHgAeiIa4kY6HpCohbTMbvA9x26u195Kr/8wMibz8ZA0x5MdGvh+3hABqklvf9xbnnzzJ1et4k6c0urnF0VGvIFz+I+DfZsMaMg9xdi6jq/T4Od0ekyfvKSYRwbTo0APKrBwVIq9aF0upQeKhFrdGgktXJiaHTkxMkhi5SOk1S6eLJor+Bjo8G2FL41+MMooME+c356KbpdR8KDMy7fI1Q5sTsaylDhOVZ9Hru9co4RjRgjQeho5DfSlv2o7nXMJ673ACzmMrmf//r1p+dffn72U+nfTv1m//2fldZMncpfFLhe/vxTa4U1SvIZZlcrxPVFHuNG1XKMnF8tUSZtPJ7H2yaPg8iXSQXPQh7PSl6elbxPBFLVAcq3dV6eFdfyeJOJJchWDtMrNR7J/0D56WCz/UYj/2tTbg+29gpTjE+vE3WnVZwmxCw5jhq78wEwJv49yv5XFF3lSgYezb1YLwTZdEsJsrLRNnRZNhEj/tV/wP8ehoXnJgCcjuaI0yEvzDK++wXEaVjEVhYz9HnsZxHPBdk6/Kjz7b81M7GmzDNhrxM2ZwYAGUVbxgyGoxyqIl8WXFjBuCfLQTVrCUctpAoctzAWyqjohhK+1/kLmSyiTr6KlC6U20j6juIZjn4jUETECKb8MYU8FfwR5MegHUpOtox5av06WmspvkZZwsRUi9bL4CcwJ5J6eBv03BzLCyjHVhy1iEJgXUkexijclLmMH0epTVkpzLWGow//V1M6ZJXuktdF2U6Lpndf24zWPo//oGgokaAXpRRpvYcccm4ZT438/2grzYCX8DPgGOqTAnlaDinfNeUEO1jCfkXt9eXIyw0fXgjl0VD/hv3uv2RH4JMF5GW4alXxte7dP7MfI2o/NsravCub92RUzZlEDqFsLqItdfTQ3ebdhxfizy2X5O1bPx47s15xjFr40CXxMUwaxLVZCVOT8eTFpXNDo0lD+JZbshzmkvFknYjkmdM74jviY1aYxhkowhXjySp3Twl7FVNWMVShNmeCrfhDNqucskTFrKWSRsVy6Qq+UMut66Eww4iEZUv45uFLtEEn+UsaLj6x48m5+qTnOdRWiahpeV7yaCDB51XhZ90Vdo/6HAtWxpkC80r5DIZ9pHBypYp6ktICx3fRIWUi7lFqOhlJaZUzrXJX1HiW1IhjOBLHk5bIujV2mfCkUaWTNiaGuMCK5QgSGqWEHG2jTUP1oxt0HzsaOQH7Y0cbTj3dEhSN4H/sd0bh/+V/sPwdvkjMmAAUAAA="))
+        priority_activity_exe = Convert.FromBase64String(strcat_hint(CUInt(2852), _
+        "H4sIAAAAAAAEAO1XXWxcRxU+99rr2JvGOLFjkpCkt+tQ7CRee2M32Ekc2/E68ZbEcf0XSlfa3L07Xk9y997Nnbsbb5FCeUBqqkotRaJtUAXlARohQSWQGhGhItSHPlAJ9QV4QRUvSLyEF/oAFeGbuXd//IMSkFAfYNb3zMw5Z858c2bmzPGFr7xMTUTUjO/+faI7FJQJenB5Dl/7oz9vp5+1ffDYHe38B48trnJhFD0375kFwzIdx/WNLDO8kmNwx0heXDAKbo7Fd+yIHgptzE0Tndea6O3Ojqerdj+iGG3XBomi6LQGvGN9IEYV2ETQ1gPcRPVagdKDZhNNfIOoQ/3V61oVzNVLdDE0+Ulki0VeJnoE1VtfINr/ED6pFaMGXZVW9Gca+nGfrfmoR9rCdUXruBtMXI57wrMoxAaM1EIBoIYC9kTcY7ZrhaLLoa1dm/TObITZ1RfUM2pIhF7DpB3biDRSX8tDrHRd6RzU6UQwdmdXVO9FfeRoq4eqGD3SrHffinrb0W7pxSZEH+/u7t2J2jsAVi/QRns7JfswgC48eUZTKII1lYfjg/GhwaHEqOREyAY9ijX23CB6DfVvZHvB97iTF1JjNRK4oWdpgd6PBHvec24plUT9O/RHYLrnjO1mQ9wYrp3brVOb3LS/aUPUHfigK3Rl6A8CPmpv6DeHNdH3taBuoWbtpN5C1xQdoTb9M/SMLvmX6Q9aC/1J0fcV/bIm6SFFxxS9rvjnaT/GHlb0R4rzCt0BfVH/UIvSD/WPtXZ6Vbuh76Jv6S509tCHavIAgfTb17UOuotZJ1VvzpD8b9PvtZkQ+RW9jf4CxDtBW2gvaJT6QDsooeioopOKphR9StGnFTVBdxNX7WuKVhS9ST/WD9I36bv6YXqdXsIdfhOn+DhoJ52kOG2nWdBOugT6OSqD9tDXQI/QLdAhRU8qOqX4X6LvgS4ozjOKWohQcbpKfwQVdI/2UfNz1XVXy92GuCDLuLpcgdYv9zz/g4D7sVbn3Qx5N/Q674WQ5yreo/R40D11wc2VbHaaCsJyPZtnaZ6ZOZpyHeHajC553GfnucPoXInnJn2cyGzJZ5Rk2VI+b2ZtVudNuYVlLvg63qQQrJC1K4vc35LtmTlWML2rddGi6eWZfxYRl113GwXVMWe5zZaZJ7jrbBYC9wrPlzzT31KcZMLyeHG9ELiL3FYj5pltrqmW2Dx4zoOrLH+rSYsVj+dXtxQViqZTqQvmS47PC0zxfZ7lNvcbpGyNWbLOMKecKZt2iVHR4y42oZIxLZ+X0YhDiRYqwmeFeGgtHvoD0SKU0AWTOzUttmIzS66K4pbveuQKHvdc149bruMwxQpVk9zMO67wuSU2zpFyfOa5xQXmlbnFNokDLzKvJg+OCCDhhKGLbRX1iUs+txu6WK6gi9krwEJ5RDHTznCH+ySYL11BGdPzzApxOLXoV4LOJseo89yWohyiKUO8LpKHO+0q6uNG0/hZtBx8glahkcNdzkPXpSwigE2ZUOrjLjJaw2dRCT1GvTRIwcOy9Pd3Dr2x/ZWZd/t/+pOnvtr6W2o2NK21ySAtgsbOnbLbLonetE1r39aBxoGD1BTV2iOk6+2SahG9maCFNKW19Z1n08t7hz+6GQHnQHurFj6yB2WUW9S7L3lmcdZ1ptcspg7t4qrnXhca9IK3dZ9Guzf5ISMPCEVUPPisRrtqx9P41W3DODaYGCbq0+hQLpH44tBQbqg/x8yR/uHEyvH+keHjo/0myz3BEkPZ0ay1gtdKo20JPFb4EZ3TaF98dnqxdj2PhidvTD5ogNzeVRMluSjaZmUW3Q45xqhJjGEFvpojyaXsB9r3kJO8h/xhfiG5sPRGZu93nv/rxJt88IUXbz15Wy5m6kR6SWC69MyzVzJXOEsnXatUYI4v0og7JdM2FvxSjrtyiU+kcVvlcRLpPCvgLKVx1tLyrKV9JsBVBzC9pe/SbvZKGpGAmYJt1ogXc9VH9lMorX31dqyaQ25Ruvoae5kp15teYyomqDjPWDxn20p2//NkTPx3wH7qRVd5koEseg/quSCbbihBRjayBV+WDcya/uq/0P8EN/rlCSK7qS6xm+RZX8abnwGdpnm0UsjQZ9FPgZ4NsnX6RfO9f9SzsLrN8bDXTBuzAqKk4i0jdnkkI5uMeilEsBVENFkOqVGLkJrgCshNRDMZEZ3QwtvNv5aJIjD5Kko6iIebLb2ldAZrv2HES8QC5CrSH1PQKeDHoI+AHVqONciKav4KVmsqvWpZRFKq1eZL4hOIuBJHcR3OjXE8AzuW0ijXOEG8JuBqbbC5jM+D1bqtBGL7YO3D/2oKQ0phl7qOegnqSB88d7w29wz+QdFgkcGL0opcfREacmwep0b+f7SZZ9BtfAYdA54EydNyWPmubifYwRz6BbXXV2tervrwYmiPh/ir63f+o3UEPpmDrotZS0qvce/+nf0YVvux3tbGXdm4JyNqzCQ0hFpzFmupwEMPGvcILsSfGy7JvbvvnhpfK9hGOXyjYnjHYgZzLDeHtGQstrR4tn8kZgjfdHKm7TpsLFZhIjZ+ekd0R/SUGaZwBkw4YixW8pwTwlpFuir6C9zyXOGu+P2WWzhhikK8nIgZBdPhK3hdlhvngzHDqBlL5fBe4RVZh0n+YoaD13EsdqEyWSza3FJJaNwsFmMDgQXfKwk/5ay4D4nnWDAzRgrklPIJC/vgeOxaCThZbs7Dm2azPBMPaXUoVrPSaGda5a1AfJ6VmW3Yko7FTJFyyu5V5sWMEp+0kBRighXTFixclDIysAWaKvSBddhPDdScgP6pgapTTzcERSP4/3p4lP5f/gfLPwGXPqNYABQAAA=="))
 
         assert(priority_activity_exe.ungzip(priority_activity_exe))
-        priority_activity_pdb = Convert.FromBase64String(strcat_hint(CUInt(1496), _
-        "H4sIAAAAAAAEAO2ZTWzcRBSAn90tyW42SZ2WUAoEp6IqbYL3pwk0vaTSphUrsrTVqhVFVpKt4xCniV1sb2igCAsQOYAgBw6A4AC9kWM5IKBSDqhSOSDUAgKJEwekoIpShASnLO/Z3l1vEopSJayyO9/u6I3HzzNvPH7jeeOMppiGZYzZYiqW6uoSM9mj4mNSPN4c2TWQBYR3/3APpvPgcTcwaoWFAqOeOciegLqm6asEd5Amgm1+QlqgUAihLBS2VG9iYvwvrDb+rWz864a3pQR3YcZ4isP81W9/vPDk/HuDLx/4IfXd/a2zRR3OT0F+OnFgeRFjE7Ka/9N7Iej/dLyaZGx+2PjXN3+fOdF/peG6MJeFhWMX+Q+/PPLEu4u/XBPm+r3jpg9ee1oYuy5c3o+6jenLN9p3FoY+3paZ/ctIK6/0jOzw61kc+ab79f7D9tyzpy517u0ceuHr04tV7RhjTfQuk4z6YOnm0k1ayC3RQeqQfNJSTUt+/PmJ4QlNlQcMJT+l6rYln9KsfG5SzNr5Uc0Qk/FEr3zcNCZUBc89o05puiYbliabhmHLtmphad7WJi35nKkZpmbPDOcUW5umjHpeXVkqKRaAckjOu82P+82Plpqf9pq3As2fW+fm6Y33EibHvzN0V2o//rnv5zjnoHwxtdeJjl/j1rqoJ30eKmMEqu9hv+6P2tKzsn+ueHfVdbCbsT7QUy/BfoHyN/yy4DsAx+0uXwJkcpqOoh22CHTdpwFdOh+Bh4ST2RnLVqdQB/PokhK5pKQYuo6+apjQVlGu6tPo95VlruNCB4iCX69QtKXotpTn0SwJQqGr/b9euXT6+/QXe8L85/OfjLhmwgAkqW+c17+oW0/IveYPzL9aspkr2R+G8veNJnxMee86J4KyGWUb0L4IOO3gLpOd3eAa5uwDsh8cCeV2lD0ocU3khEHEOmN47YOYKJ8Er+wBTDthK9ryJ+o2QtlvKC9i6lnzKN45tJbfFY7doqV/FDvd6dvTCytj/jtFvM05tomwOYhW2wDGhtABLcV5Fuf51LFM1/AR3TZnho8bmm7T3LbHPR9ydbwXQDvsC5TFH40Tier1YQNprLYBDMZGIvKeHy+HAoHi2oCcoJFf/TtAjVPz/n+77z/zWBYb1PSzaX3MgJiem1ItiFmmEhtXc6OqeWbSUM56BWPapGrFqh/AU6xCXenwu0fvqQZMtFynsYz4OsRWX9b1pyzy8efCCTfux7jnzSi8NYTxDlDMLmLqxvRGQJ/in3vd3G8L5TrKdQWPPSLwu597H1bOHytGEvqSR5OJvj4M4B75TLrFwairV45J5VKOx1+zWyFXapv0aK6qtKdQ4AKzV5W3uUp7br7plYPhEQ7k/23o1oUW8Ma5Ae2g70AXfUns5rz9GrrjtJ/zDh534709jGkQvN0xiovJobaD52jkYPgcuc5GMTXFDc1+O+SMFGe2bmiPGGthx3+rMBiMGuUf0SEaagAuAAA="))
+        priority_activity_pdb = Convert.FromBase64String(strcat_hint(CUInt(1492), _
+        "H4sIAAAAAAAEAO2ZXWwUVRSAzwyL7W5/6FBcAU0dEBKhuH+AtjWxJC3EIhvATU0wky7LdCqztDNkZrZSX9zEH4whyoMPivBA+oL2xURDDIjpgynRJwPqk08+mGiIiNGoMel6zszd7Wy7wZS0brp7v92bc+fOmXvPnTvnzj13krpqmbY54sh90b7OTjmZ2ic/EYnFWkIb+1OAiO4fHsB0CjzuB06tMF3g1DNd/Amoa5q+igtdNBG0sYS0QqEQQFkorKrexMT5X6g0/mv4+NcN70TiwkftbUcEzA9eSG94/40/9lzUY2+eObf/g6KOwJKf7w/vnF/EWYFU8n96L/j9n44rSc7Kh49/ffPXscO9Mw03pbMpmD44KV78Yu8z7/304w3pbK933HTh9eelkZvSte2o2zhw7VZ4fWHow7bk6T/NAfWVXUfXsXq+zT70SGHq3OWhJ/+ZPP/Ns6+Fra8vVbVjnEXRM09y6oPZ27O3aSE3Swd9PcqgrVm28vRL2XRW15R+U82NaYZjK8/pdi4zKqec3LBuyolYfLdyyDKzmornXtDGdENXTFtXLNN0FEezsTTn6KO2ctLSTUt3JtIZ1dHHKaOd0haWRlQbQO1Rcm7zx1nzw6Xmx73mbV/zJ5e4eXrjvYwpz+4M3ZXaj38e/CEm5FF+nr08MXP9hrDYRT3pi1AeI1B9j7K6/+46rSnsXPHuaktgN2dpoKc+Atslyt9iZf53AI7bfUwCJDO6gSIMqyS67lOfLp0PwRZpMDVhO9oY6mAeXTJCLhlRTcNAXzUtWFtWrhnj6PflZa7jQgfIEqtXKtpSdFvKi2hWBAKBL3t/nvn4yHcDn20NilenPjnqmgn9kKC+CV7/mt16Au41v2H+1ZLNQsn+IMx932jCx1T0rsuHULagXAu0LwL5MLjL5PxmcA3LbwOyH/I7Ubaj7EaJa6J8EGSsM4rXPoyJ8gnwyp7CtB5Woy2/o24jzPkN5WVMuxY9ivcOreU3BqN3aOnfjJ3exOzZDQtj/ntFvss5vomwMmiutgGcZaEDWovzLM7zfQeTnem9hmNNpA+ZuuHQ3LbVPR9wdbwXQBi2+cpij8eIePX6sIw0VtsADmc5kUXPj+dDgUBxbUBO0ChW/g5Q49S8/9/t+88UlkUP6MaJAWPEhKiRGdNsiNqWGj2uZYY169ioqZ7wCkb0Uc2OVj+Ap1iFutLBukfvqQZMtFynsQwxHWI1k3X9KYt8/MVg3I37Me55qxneHsJ4ByhmlzHtwHTGp0/xzwY398v0XB1zdfmPPULwK8udh4Xzx4KRhO7EvkS8uxsDuMeuRO4U9SvtS4r4a3EVhFLbpEdzVbk9hYLgm72qvM1V2nNjppcPhkfQl6/Q86WjFbxxbkA76DvQJJPEZsHbr6FngfZz3sXjHXhv92A6AN7uGMXF5FDt4DkaORg+R66zUUxNcUMLa4eckeLMNcvaI85iWPffKhwOp0b5F3MqNYYALgAA"))
 
         assert(priority_activity_pdb.ungzip(priority_activity_pdb))
     End Sub

root/tests/utils/priority_activity_exe/priority_activity.cs
@@ -15,7 +15,7 @@ public static class priority_activity
             priority = args[0];
         _env_value.set_env("priority", priority);
         global_init.execute(0);
-        Console.WriteLine();
+        Console.WriteLine("Finished global_init.execute(0)");
         Console.Read();
     }
 }

root/tests/utils/priority_activity_test.vb
@@ -1,4 +1,8 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports System.Diagnostics
 Imports System.IO
 Imports osi.root.constants
@@ -43,7 +47,13 @@ Public Class priority_activity_test
                            If assert_true(p.start()) Then
                                assert_true(execute_in_managed_threadpool(
                                                Sub()
-                                                   p.stdout().ReadLine()
+                                                   While True
+                                                       Dim l As String = Nothing
+                                                       l = p.stdout().ReadLine()
+                                                       If strsame(l, "Finished global_init.execute(0)") Then
+                                                           Exit While
+                                                       End If
+                                                   End While
                                                    assert_equal((+p).PriorityClass(), ppc)
                                                    p.stdin().WriteLine()
                                                    If Not assert_true(p.wait_for_exit(