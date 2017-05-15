2ed903b7d4c463512e7226d22b9580f87ce53ce3
Hzj_jie <hzj_jie@hotmail.com>
2017-05-15 00:30:44 -0700

    optional and tests

---
+++ root/formation/optional.vb
@@ -0,0 +1,58 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+
+Public NotInheritable Class [optional]
+    Public Shared Function [New](Of T)(ByVal v As T) As [optional](Of T)
+        Return New [optional](Of T)(v)
+    End Function
+
+    Public Shared Function [New](Of T)() As [optional](Of T)
+        Return New [optional](Of T)()
+    End Function
+
+    Private Sub New()
+    End Sub
+End Class
+
+Public Class [optional](Of T)
+    Private ReadOnly b As Boolean
+    Private ReadOnly v As T
+
+    Private Sub New(ByVal b As Boolean, ByVal v As T)
+        Me.b = b
+        Me.v = v
+    End Sub
+
+    Public Sub New()
+        Me.New(False, Nothing)
+    End Sub
+
+    Public Sub New(ByVal v As T)
+        Me.New(True, v)
+    End Sub
+
+    Public Shared Widening Operator CType(ByVal this As [optional](Of T)) As Boolean
+        If this Is Nothing Then
+            Return False
+        Else
+            Return this.b
+        End If
+    End Operator
+
+    Public Shared Operator +(ByVal this As [optional](Of T)) As T
+        assert(this)
+        Return this.v
+    End Operator
+
+    Public Shared Operator -(ByVal this As [optional](Of T)) As T
+        If this Then
+            Return this.v
+        Else
+            Return Nothing
+        End If
+    End Operator
+End Class
---
+++ root/tests/formation/optional_test.vb
@@ -0,0 +1,21 @@
+﻿
+Imports osi.root.formation
+Imports osi.root.utt
+
+Public Class optional_test
+    Inherits [case]
+
+    Public Overrides Function run() As Boolean
+        Dim p As [optional](Of Object) = Nothing
+        p = [optional].[New](New Object())
+        assert_true(p)
+        assert_not_nothing(+p)
+        assert_not_nothing(-p)
+
+        p = [optional].[New](Of Object)()
+        assert_false(p)
+        assert_nothing(-p)
+
+        Return True
+    End Function
+End Class
