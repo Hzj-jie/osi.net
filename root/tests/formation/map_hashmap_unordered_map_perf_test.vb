d75e956436ceb1ae3f330f5d955acd1d5fce77ba
Hzj_jie <hzj_jie@hotmail.com>
2017-06-23 00:53:06 -0700

    unordered_map perf

--- root/tests/formation/map_hashmap_unordered_map_perf_test.vb
+++ root/tests/formation/map_hashmap_unordered_map_perf_test.vb
@@ -1,26 +1,32 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports System.Collections.Generic
 Imports osi.root.connector
 Imports osi.root.formation
 Imports osi.root.utt
 
-' Compare perf of map, hashmap and Dictionary
-Public Class map_hashmap_perf_test
+' Compare perf of map, hashmap, unordered_map and Dictionary
+Public Class map_hashmap_unordered_map_perf_test
     Inherits performance_comparison_case_wrapper
 
     Public Sub New()
-        MyBase.New(R(New map_case()), R(New hashmap_case()), R(New dictionary_case()))
+        MyBase.New(R(New map_case()), R(New hashmap_case()), R(New dictionary_case()), R(New unordered_map_case()))
     End Sub
 
     Protected Overrides Function min_rate_table() As Double(,)
         If isdebugmode() Then
-            Return {{0, 4, 10},
-                    {0.5, 0, -1},
-                    {0.2, -1, 0}}
+            Return {{0, 4, 10, -1},
+                    {0.5, 0, -1, -1},
+                    {0.2, -1, 0, -1},
+                    {-1, -1, -1, 0}}
         Else
-            Return {{0, 3, 4},
-                    {0.7, 0, -1},
-                    {0.5, -1, 0}}
+            Return {{0, 3, 4, -1},
+                    {0.7, 0, -1, -1},
+                    {0.5, -1, 0, -1},
+                    {-1, -1, -1, 0}}
         End If
     End Function
 
@@ -153,6 +159,41 @@ Public Class map_hashmap_perf_test
         Protected Overrides Sub clear()
             ' Dictionary.Clear does not really release the memory.
             m = New Dictionary(Of String, String)()
+        End Sub
+    End Class
+
+    Public Class unordered_map_manual_perf
+        Inherits commandline_specific_case_wrapper
+
+        Public Sub New()
+            MyBase.New(repeat(New unordered_map_case(), 1000000000))
+        End Sub
+    End Class
+
+    Private Class unordered_map_case
+        Inherits run_case
+
+        Private ReadOnly m As unordered_map(Of String, String) = Nothing
+
+        Public Sub New()
+            MyBase.New()
+            m = New unordered_map(Of String, String)()
+        End Sub
+
+        Protected Overrides Sub clear()
+            m.clear()
+        End Sub
+
+        Protected Overrides Sub [erase]()
+            m.erase(rnd_str())
+        End Sub
+
+        Protected Overrides Sub find()
+            m.find(rnd_str())
+        End Sub
+
+        Protected Overrides Sub insert()
+            m.emplace(rnd_str(), rnd_str())
         End Sub
     End Class
 End Class
