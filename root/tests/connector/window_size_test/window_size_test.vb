9fe520d27061e1efc789b0c56e83911c635ba9c8
Hzj_jie <hzj_jie@hotmail.com>
2017-08-02 15:23:56 -0700

    window_size_test

---
+++ root/tests/connector/window_size_test/window_size_test.vb
@@ -0,0 +1,38 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.root.utt
+
+Public Class window_size_test
+    Inherits commandline_specific_case_wrapper
+
+    Public Sub New()
+        MyBase.New(New window_size_case())
+    End Sub
+
+    Private Class window_size_case
+        Inherits [case]
+
+        Public Overrides Function run() As Boolean
+            Windows.Forms.Application.Run(New window_size_test_form(
+                    Sub(f As window_size_test_form)
+                        f.ControlBox() = False
+                        f.FormBorderStyle() = Windows.Forms.FormBorderStyle.None
+                        f.WindowState() = Windows.Forms.FormWindowState.Normal
+                        f.ControlBox() = False
+                        f.MinimizeBox() = False
+                        f.MaximizeBox() = False
+                        f.Width() = 0
+                        f.Height() = 0
+                        f.BeginInvoke(Sub()
+                                          Console.WriteLine(strcat("width: ", f.Width(), ", height: ", f.Height()))
+                                          f.Close()
+                                      End Sub)
+                    End Sub))
+            Return True
+        End Function
+    End Class
+End Class
---
+++ root/tests/connector/window_size_test/window_size_test_form.vb
@@ -0,0 +1,24 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+
+Public Class window_size_test_form
+    Private ReadOnly cb As Action(Of window_size_test_form)
+
+    Public Sub New(ByVal cb As Action(Of window_size_test_form))
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        assert(Not cb Is Nothing)
+        Me.cb = cb
+    End Sub
+
+    Private Sub window_size_test_form_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        cb(Me)
+    End Sub
+End Class
