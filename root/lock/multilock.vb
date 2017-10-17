c8c9df46d2bc32102d0e82d9afdb9b92dce1439a
Hzj_jie <hzj_jie@hotmail.com>
2017-10-16 17:20:16 -0700

    Coding style in multilock

--- root/lock/multilock.vb
+++ root/lock/multilock.vb
@@ -1,37 +1,42 @@
 ﻿
-Imports System.Threading
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.connector
+Imports osi.root.constants
 
 Public Class multilock(Of lock_t As {Structure, slimlock.islimlock})
-    Public Const defaultLockCount As Int64 = 8192
-    Private locks() As lock_t
+    Private Const default_lock_count As UInt32 = 8192
+    Private ReadOnly locks() As lock_t
 
-    Public Sub New(ByVal lockCount As Int64)
-        ReDim locks(lockCount - 1)
+    Public Sub New(ByVal lock_count As UInt32)
+        assert(lock_count > 0)
+        ReDim locks(CInt(lock_count - uint32_1))
     End Sub
 
     Public Sub New()
-        Me.New(defaultLockCount)
+        Me.New(default_lock_count)
     End Sub
 
-    Private Function locksigning(Of T)(ByVal i As T) As UInt32
-        Return signing(i) Mod locks.Length()
+    Private Function lock_signing(Of T)(ByVal i As T) As UInt32
+        Return signing(i) Mod array_size(locks)
     End Function
 
     Public Sub lock(Of T)(ByVal i As T)
-        locks(locksigning(i)).wait()
+        locks(CInt(lock_signing(i))).wait()
     End Sub
 
     Public Sub release(Of T)(ByVal i As T)
-        locks(locksigning(i)).release()
+        locks(CInt(lock_signing(i))).release()
     End Sub
 End Class
 
 Public Class multilock
     Inherits multilock(Of slimlock.monitorlock)
 
-    Public Sub New(ByVal lockCount As Int64)
-        MyBase.New(lockCount)
+    Public Sub New(ByVal lock_count As UInt32)
+        MyBase.New(lock_count)
     End Sub
 
     Public Sub New()
