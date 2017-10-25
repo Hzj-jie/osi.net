e303c13ce40b7ed65548994620e698edd0ab4a8c
Hzj_jie <hzj_jie@hotmail.com>
2017-10-24 21:10:39 -0700

    Coding style of performance.vb

--- root/constants/performance.vb
+++ root/constants/performance.vb
@@ -1,16 +1,20 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Public Module _performance
-    Public Class performanceCounterCategories
+    Public Class performance_counter_categories
         Public Const process As String = "process"
         Public Const processor As String = "Processor"
         Public Const memory As String = "Memory"
     End Class
 
-    Public Class performanceCounterCounters
-        Public Const processorTime As String = "% Processor Time"
-        Public Const availableMBytes As String = "Available MBytes"
-        Public Const availableBytes As String = "Available Bytes"
-        Public Const workingSet As String = "Working Set"
-        Public Const virtualBytes As String = "Virtual Bytes"
+    Public Class performance_counter_counters
+        Public Const processor_time As String = "% Processor Time"
+        Public Const available_m_bytes As String = "Available MBytes"
+        Public Const available_bytes As String = "Available Bytes"
+        Public Const working_set As String = "Working Set"
+        Public Const virtual_bytes As String = "Virtual Bytes"
     End Class
 End Module
