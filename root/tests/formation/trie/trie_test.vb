0342a4adcaf941ed3e5f794a81bee64acc70778f
Hzj_jie <hzj_jie@hotmail.com>
2017-11-23 11:17:10 -0800

    Crash

--- root/tests/formation/trie/trie_test.vb
+++ root/tests/formation/trie/trie_test.vb
@@ -6,7 +6,7 @@ Public Class trie_test
     Inherits repeat_case_wrapper
 
     Public Sub New()
-        MyBase.New(New trie_case(True), 1000000 << (If(isreleasebuild(), 2, 0)))
+        MyBase.New(New trie_case(True), CULng(1000000) << (If(isreleasebuild(), 2, 0)))
     End Sub
 End Class
 
@@ -14,6 +14,6 @@ Public Class trie_specified_test
     Inherits commandline_specified_case_wrapper
 
     Public Sub New()
-        MyBase.New(repeat(New trie_case(True), 1000000000 << (If(isreleasebuild(), 2, 0))))
+        MyBase.New(repeat(New trie_case(True), CULng(1000000000) << (If(isreleasebuild(), 2, 0))))
     End Sub
 End Class
