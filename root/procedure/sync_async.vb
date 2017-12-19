Hzj-jie/osi.net

commit 1c40b5d5f5a97ddf2f35e8b1c842e2f1b5f23e87

cluster.length should be uint64

service/storage/virtdisk/virtdisk.capinfo.vb
@@ -32,14 +32,14 @@ Partial Public Class virtdisk
             End If
         End Sub
 
-        Public Shared Operator +(ByVal i As capinfo) As Int64
+        Public Shared Operator +(ByVal i As capinfo) As UInt64
             If i Is Nothing Then
                 Return 0
             ElseIf i.is_fs Then
                 assert(Not i.di Is Nothing)
                 Return i.di.capacity()
             ElseIf i.is_ms Then
-                Return CLng(available_virtual_memory())
+                Return CULng(available_virtual_memory())
             Else
                 Return 0
             End If

service/storage/capinfo.vb
@@ -1,18 +1,21 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports System.IO
-Imports osi.root.constants
 Imports osi.root.connector
-Imports osi.root.utils
+Imports osi.root.constants
 
 Public Class capinfo
     Private ReadOnly di As DriveInfo
-    Private ReadOnly preserve_disk_capacity As Int64
+    Private ReadOnly reserve_disk_capacity As Int64
 
     Shared Sub New()
         assert(npos < 0)
     End Sub
 
-    Public Sub New(ByVal path_or_file As String, ByVal preserve_disk_capacity As UInt64)
+    Public Sub New(ByVal path_or_file As String, ByVal reserve_disk_capacity As UInt64)
         Try
             di = New DriveInfo(Path.GetPathRoot(Path.GetFullPath(path_or_file)))
         Catch ex As Exception
@@ -24,32 +27,29 @@ Public Class capinfo
                         ", the capacity will always return ",
                         npos)
         End Try
-        assert(preserve_disk_capacity <= max_int64)
-        Me.preserve_disk_capacity = preserve_disk_capacity
+        assert(reserve_disk_capacity <= max_int64)
+        Me.reserve_disk_capacity = CLng(reserve_disk_capacity)
     End Sub
 
     Public Sub New(ByVal path_or_file As String)
         Me.New(path_or_file, configuration.preserved_disk_capacity(path_or_file))
     End Sub
 
-    Public Function capacity() As Int64
+    Public Function capacity() As UInt64
         If di Is Nothing Then
-            Return npos
+            Return 0
         Else
-            Return do_(Function() As Int64
-                           Dim v As Int64 = 0
-                           v = di.AvailableFreeSpace() - preserve_disk_capacity
-                           If v <= 0 Then
-                               Return 0
-                           Else
-                               Return v
-                           End If
-                       End Function,
-                       npos)
+            Dim v As Int64 = 0
+            v = do_(Function() di.AvailableFreeSpace(), int64_0) - reserve_disk_capacity
+            If v <= 0 Then
+                Return 0
+            Else
+                Return CULng(v)
+            End If
         End If
     End Function
 
-    Public Shared Operator +(ByVal this As capinfo) As Int64
+    Public Shared Operator +(ByVal this As capinfo) As UInt64
         If this Is Nothing Then
             Return 0
         Else

service/storage/free_cluster/cluster.constants.vb
@@ -1,29 +1,31 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.constants
 Imports osi.root.connector
-Imports osi.root.utils
 
 Partial Public Class cluster
     Private Const INVALID_ID As Int64 = npos
     Private Const INVALID_OFFSET As Int64 = npos
     Private Const FREE_USED As Int64 = npos
     Private Const DISK_CLUSTER_SIZE As UInt16 = 512
-    Private Shared ReadOnly CLUSTERID_OFFSET As Int32 = 0
-    Private Shared ReadOnly USED_OFFSET As Int32 = CLUSTERID_OFFSET + sizeof_int64
-    Private Shared ReadOnly LENGTH_OFFSET As Int32 = USED_OFFSET + sizeof_int64
-    Private Shared ReadOnly NEXTCLUSTERID_OFFSET As Int32 = LENGTH_OFFSET + sizeof_int64
-    Private Shared ReadOnly PREVCLUSTERID_OFFSET As Int32 = NEXTCLUSTERID_OFFSET + sizeof_int64
+    Private Shared ReadOnly CLUSTERID_OFFSET As UInt32 = 0
+    Private Shared ReadOnly USED_OFFSET As UInt32 = CLUSTERID_OFFSET + sizeof_int64
+    Private Shared ReadOnly LENGTH_OFFSET As UInt32 = USED_OFFSET + sizeof_int64
+    Private Shared ReadOnly NEXTCLUSTERID_OFFSET As UInt32 = LENGTH_OFFSET + sizeof_int64
+    Private Shared ReadOnly PREVCLUSTERID_OFFSET As UInt32 = NEXTCLUSTERID_OFFSET + sizeof_int64
     Private Shared ReadOnly CHECKSUM() As Byte = Text.Encoding.Unicode().GetBytes("HHHH")
-    Private Shared ReadOnly CHECKSUM_SIZE As Int32 = array_size(CHECKSUM)
-    Private Shared ReadOnly CHECKSUM_OFFSET As Int32 = PREVCLUSTERID_OFFSET + sizeof_int64
-    Private Shared ReadOnly DATA_OFFSET As Int32 = CHECKSUM_OFFSET + CHECKSUM_SIZE
-    Private Shared ReadOnly STRUCTURE_SIZE As Int32 = DATA_OFFSET
-    Private Shared ReadOnly MIN_CLUSTER_LENGTH As Int32 =
+    Private Shared ReadOnly CHECKSUM_SIZE As UInt32 = array_size(CHECKSUM)
+    Private Shared ReadOnly CHECKSUM_OFFSET As UInt32 = PREVCLUSTERID_OFFSET + sizeof_int64
+    Private Shared ReadOnly DATA_OFFSET As UInt32 = CHECKSUM_OFFSET + CHECKSUM_SIZE
+    Private Shared ReadOnly STRUCTURE_SIZE As UInt32 = DATA_OFFSET
+    Private Shared ReadOnly MIN_CLUSTER_LENGTH As UInt32 =
         assert_return(DISK_CLUSTER_SIZE > DATA_OFFSET, DISK_CLUSTER_SIZE - DATA_OFFSET)
 
     Private Shared Sub verify_constants()
         assert(Not valid_id(INVALID_ID))
-        assert(Not valid_offset(INVALID_OFFSET))
         assert(FREE_USED < 0)
         assert(CHECKSUM_SIZE = sizeof_int64) 'not neccessary
     End Sub

service/storage/free_cluster/cluster.data.vb
@@ -1,21 +1,27 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.root.constants
 Imports osi.root.formation
 Imports osi.root.procedure
-Imports osi.root.connector
-Imports osi.root.utils
-Imports osi.root.lock
 
 Partial Public Class cluster
-    ' TODO: offset should be uint64.
-    Public Function read(ByVal buff() As Byte, Optional ByVal offset As Int32 = 0) As event_comb
+    Public Function read(ByVal buff() As Byte, Optional ByVal offset As UInt32 = 0) As event_comb
         Dim ec As event_comb = Nothing
         Return New event_comb(Function() As Boolean
                                   If free() OrElse empty() Then
                                       Return goto_end()
                                   Else
-                                      ec = virtdisk().read(data_disk_offset(), used_bytes(), buff, offset)
-                                      Return waitfor(ec) AndAlso
+                                      If used_bytes() > max_uint32 Then
+                                          Return False
+                                      Else
+                                          ec = virtdisk().read(data_disk_offset(), CUInt(used_bytes()), buff, offset)
+                                          Return waitfor(ec) AndAlso
                                              goto_next()
+                                      End If
                                   End If
                               End Function,
                               Function() As Boolean
@@ -28,10 +34,13 @@ Partial Public Class cluster
         Dim ec As event_comb = Nothing
         Dim buff() As Byte = Nothing
         Return New event_comb(Function() As Boolean
-                                  ReDim buff(used_bytes() - 1)
-                                  ec = read(buff)
-                                  Return waitfor(ec) AndAlso
-                                         goto_next()
+                                  If Not buff.resize(used_bytes()) Then
+                                      Return False
+                                  Else
+                                      ec = read(buff)
+                                      Return waitfor(ec) AndAlso
+                                             goto_next()
+                                  End If
                               End Function,
                               Function() As Boolean
                                   Return ec.end_result() AndAlso
@@ -72,7 +81,7 @@ Partial Public Class cluster
                                   If h Is Nothing OrElse isemptyarray(others) Then
                                       Return False
                                   Else
-                                      For i As Int32 = 0 To array_size(others) - 1
+                                      For i As Int32 = 0 To array_size_i(others) - 1
                                           If others(i) Is Nothing Then
                                               Return False
                                           End If
@@ -81,7 +90,7 @@ Partial Public Class cluster
                                   If h.has_next_id() Then
                                       Return False
                                   End If
-                                  For i As Int32 = 0 To array_size(others) - 2
+                                  For i As Int32 = 0 To array_size_i(others) - 2
                                       If others(i).has_prev_id() OrElse others(i).has_next_id() Then
                                           Return False
                                       End If
@@ -89,13 +98,13 @@ Partial Public Class cluster
                                           Return False
                                       End If
                                   Next
-                                  If others(array_size(others) - 1).has_prev_id() Then
+                                  If others(array_size_i(others) - 1).has_prev_id() Then
                                       Return False
                                   End If
 
-                                  ReDim ecs(array_size(others))
+                                  ReDim ecs(array_size_i(others))
                                   h.mark_free_as_empty()
-                                  For i As Int32 = 0 To array_size(others) - 1
+                                  For i As Int32 = 0 To array_size_i(others) - 1
                                       Dim l As cluster = Nothing
                                       l = If(i = 0, h, others(i - 1))
                                       others(i)._prev_id = l.id()
@@ -103,7 +112,7 @@ Partial Public Class cluster
                                       others(i).mark_free_as_empty()
                                       ecs(i) = l.write_structure()
                                   Next
-                                  ecs(array_size(others)) = others(array_size(others) - 1).write_structure()
+                                  ecs(array_size_i(others)) = others(array_size_i(others) - 1).write_structure()
                                   Return waitfor(ecs) AndAlso
                                          goto_next()
                               End Function,
@@ -114,13 +123,11 @@ Partial Public Class cluster
     End Function
 
     Public Function append(ByVal buff() As Byte,
-                           ByVal offset As Int64,
-                           ByVal count As Int64) As event_comb
+                           ByVal offset As UInt32,
+                           ByVal count As UInt32) As event_comb
         Dim ec As event_comb = Nothing
         Return New event_comb(Function() As Boolean
-                                  If offset < 0 OrElse
-                                     count < 0 OrElse
-                                     array_size(buff) < offset + count OrElse
+                                  If array_size(buff) < offset + count OrElse
                                      remain_bytes() < count Then
                                       Return False
                                   ElseIf count = 0 Then
@@ -151,8 +158,7 @@ Partial Public Class cluster
                               End Function)
     End Function
 
-    Public Function append(ByVal buff() As Byte,
-                           Optional ByVal offset As Int64 = 0) As event_comb
+    Public Function append(ByVal buff() As Byte, Optional ByVal offset As UInt32 = 0) As event_comb
         Return append(buff, offset, array_size(buff))
     End Function
 

service/storage/free_cluster/cluster.new.vb
@@ -1,7 +1,12 @@
 ﻿
-Imports osi.root.procedure
-Imports osi.root.formation
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.connector
+Imports osi.root.constants
+Imports osi.root.formation
+Imports osi.root.procedure
 Imports osi.root.utils
 
 Partial Public Class cluster
@@ -10,8 +15,8 @@ Partial Public Class cluster
     End Sub
 
     Private Sub New(ByVal id As Int64,
-                    ByVal offset As Int64,
-                    ByVal length As Int64,
+                    ByVal offset As UInt64,
+                    ByVal length As UInt64,
                     ByVal vd As virtdisk,
                     ByVal used As Int64,
                     ByVal prev_id As Int64,
@@ -28,11 +33,11 @@ Partial Public Class cluster
     End Sub
 
     'create a new cluster in the virtdisk
-    'the real cluster length does not guarantee to be same as length input
+    'the real cluster length is not guaranteed to be same as length input
     Public Shared Function ctor(ByVal vd As virtdisk,
                                 ByVal id As Int64,
-                                ByVal offset As Int64,
-                                ByVal length As Int64,
+                                ByVal offset As UInt64,
+                                ByVal length As UInt64,
                                 ByVal r As pointer(Of cluster)) As event_comb
         Dim ec As event_comb = Nothing
         Dim c As cluster = Nothing
@@ -45,7 +50,7 @@ Partial Public Class cluster
                                           length = MIN_CLUSTER_LENGTH
                                       ElseIf ((length - MIN_CLUSTER_LENGTH) Mod DISK_CLUSTER_SIZE) <> 0 Then
                                           length = MIN_CLUSTER_LENGTH +
-                                                   ((((length - MIN_CLUSTER_LENGTH) Mod DISK_CLUSTER_SIZE) + 1) *
+                                                   ((((length - MIN_CLUSTER_LENGTH) Mod DISK_CLUSTER_SIZE) + uint32_1) *
                                                     DISK_CLUSTER_SIZE)
                                       End If
                                       assert(((length + STRUCTURE_SIZE) Mod DISK_CLUSTER_SIZE) = 0)
@@ -75,18 +80,18 @@ Partial Public Class cluster
 
     Public Shared Function ctor(ByVal vd As virtdisk,
                                 ByVal id As Int64,
-                                ByVal length As Int64,
+                                ByVal length As UInt64,
                                 ByVal r As pointer(Of cluster)) As event_comb
-        Return ctor(vd, id, If(vd Is Nothing, INVALID_OFFSET, vd.size()), length, r)
+        Return ctor(vd, id, If(vd Is Nothing, uint64_0, vd.size()), length, r)
     End Function
 
     'create an existing cluster from the virtdisk
     Public Shared Function ctor(ByVal vd As virtdisk,
-                                ByVal offset As Int64,
+                                ByVal offset As UInt64,
                                 ByVal r As pointer(Of cluster)) As event_comb
         Dim id As pointer(Of Int64) = Nothing
         Dim used As pointer(Of Int64) = Nothing
-        Dim length As pointer(Of Int64) = Nothing
+        Dim length As pointer(Of UInt64) = Nothing
         Dim next_id As pointer(Of Int64) = Nothing
         Dim prev_id As pointer(Of Int64) = Nothing
         Dim ec As event_comb = Nothing
@@ -96,7 +101,7 @@ Partial Public Class cluster
                                   Else
                                       id = New pointer(Of Int64)()
                                       used = New pointer(Of Int64)()
-                                      length = New pointer(Of Int64)()
+                                      length = New pointer(Of UInt64)()
                                       next_id = New pointer(Of Int64)()
                                       prev_id = New pointer(Of Int64)()
                                       ec = read_structure(vd, offset, id, used, length, next_id, prev_id)

service/storage/free_cluster/cluster.status.vb
@@ -1,14 +1,21 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.root.constants
+
 Partial Public Class cluster
     Public Function id() As Int64
         Return _id
     End Function
 
-    Public Function disk_offset() As Int64
+    Public Function disk_offset() As UInt64
         Return _offset
     End Function
 
-    Public Function data_length() As Int64
+    Public Function data_length() As UInt64
         Return _length
     End Function
 
@@ -16,14 +23,15 @@ Partial Public Class cluster
         Return _vd
     End Function
 
-    Private Shared Function total_length(ByVal length As Int64) As Int64
+    Private Shared Function total_length(ByVal length As UInt64) As UInt64
         Return length + STRUCTURE_SIZE
     End Function
 
-    Public Function total_length() As Int64
+    Public Function total_length() As UInt64
         Return total_length(data_length())
     End Function
 
+    ' A free cluster returns -1 for this function.
     Public Function used() As Int64
         assert_valid_status()
         Return _used
@@ -38,11 +46,11 @@ Partial Public Class cluster
         Return used() = 0
     End Function
 
-    Public Function used_bytes() As Int64
-        Return If(free(), 0, used())
+    Public Function used_bytes() As UInt64
+        Return If(free(), uint64_0, CULng(used()))
     End Function
 
-    Public Function remain_bytes() As Int64
+    Public Function remain_bytes() As UInt64
         Return data_length() - used_bytes()
     End Function
 
@@ -50,19 +58,19 @@ Partial Public Class cluster
         Return remain_bytes() = 0
     End Function
 
-    Public Function data_disk_offset() As Int64
+    Public Function data_disk_offset() As UInt64
         Return disk_offset() + DATA_OFFSET
     End Function
 
-    Public Function remain_data_disk_offset() As Int64
+    Public Function remain_data_disk_offset() As UInt64
         Return data_disk_offset() + used_bytes()
     End Function
 
-    Private Shared Function end_disk_offset(ByVal offset As Int64, ByVal length As Int64) As Int64
+    Private Shared Function end_disk_offset(ByVal offset As UInt64, ByVal length As UInt64) As UInt64
         Return offset + total_length(length)
     End Function
 
-    Public Function end_disk_offset() As Int64
+    Public Function end_disk_offset() As UInt64
         Return end_disk_offset(disk_offset(), data_length())
     End Function
 

service/storage/free_cluster/cluster.structure.vb
@@ -1,16 +1,20 @@
 ﻿
-Imports osi.root.procedure
-Imports osi.root.formation
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.connector
+Imports osi.root.formation
+Imports osi.root.procedure
 Imports osi.root.utils
 
 Partial Public Class cluster
     Private Function create_structure() As Byte()
         Dim r() As Byte = Nothing
-        ReDim r(STRUCTURE_SIZE - 1)
+        r.resize(STRUCTURE_SIZE)
         assert(int64_bytes(id(), r, CLUSTERID_OFFSET))
         assert(int64_bytes(used(), r, USED_OFFSET))
-        assert(int64_bytes(data_length(), r, LENGTH_OFFSET))
+        assert(uint64_bytes(data_length(), r, LENGTH_OFFSET))
         assert(int64_bytes(next_id(), r, NEXTCLUSTERID_OFFSET))
         assert(int64_bytes(prev_id(), r, PREVCLUSTERID_OFFSET))
         memcpy(r, CHECKSUM_OFFSET, CHECKSUM, 0, CHECKSUM_SIZE)
@@ -20,13 +24,13 @@ Partial Public Class cluster
     Private Shared Function parse_structure(ByVal buff() As Byte,
                                             ByRef id As Int64,
                                             ByRef used As Int64,
-                                            ByRef length As Int64,
+                                            ByRef length As UInt64,
                                             ByRef next_id As Int64,
                                             ByRef prev_id As Int64) As Boolean
         Return assert(array_size(buff) = STRUCTURE_SIZE) AndAlso
                assert(bytes_int64(buff, id, CLUSTERID_OFFSET)) AndAlso
                assert(bytes_int64(buff, used, USED_OFFSET)) AndAlso
-               assert(bytes_int64(buff, length, LENGTH_OFFSET)) AndAlso
+               assert(bytes_uint64(buff, length, LENGTH_OFFSET)) AndAlso
                assert(bytes_int64(buff, next_id, NEXTCLUSTERID_OFFSET)) AndAlso
                assert(bytes_int64(buff, prev_id, PREVCLUSTERID_OFFSET)) AndAlso
                memcmp(buff, CHECKSUM_OFFSET, CHECKSUM, 0, CHECKSUM_SIZE) = 0 AndAlso
@@ -42,12 +46,12 @@ Partial Public Class cluster
     Private Shared Function parse_structure(ByVal buff() As Byte,
                                             ByVal id As pointer(Of Int64),
                                             ByVal used As pointer(Of Int64),
-                                            ByVal length As pointer(Of Int64),
+                                            ByVal length As pointer(Of UInt64),
                                             ByVal next_id As pointer(Of Int64),
                                             ByVal prev_id As pointer(Of Int64)) As Boolean
         Dim i As Int64 = 0
         Dim u As Int64 = 0
-        Dim l As Int64 = 0
+        Dim l As UInt64 = 0
         Dim n As Int64 = 0
         Dim p As Int64 = 0
         Return parse_structure(buff, i, u, l, n, p) AndAlso
@@ -75,10 +79,10 @@ Partial Public Class cluster
     End Function
 
     Private Shared Function read_structure(ByVal vd As virtdisk,
-                                           ByVal offset As Int64,
+                                           ByVal offset As UInt64,
                                            ByVal id As pointer(Of Int64),
                                            ByVal used As pointer(Of Int64),
-                                           ByVal length As pointer(Of Int64),
+                                           ByVal length As pointer(Of UInt64),
                                            ByVal next_id As pointer(Of Int64),
                                            ByVal prev_id As pointer(Of Int64)) As event_comb
         Dim r() As Byte = Nothing
@@ -86,7 +90,7 @@ Partial Public Class cluster
         Return New event_comb(Function() As Boolean
                                   If valid_virtdisk(vd) AndAlso
                                      valid_offset(offset) Then
-                                      ReDim r(STRUCTURE_SIZE - 1)
+                                      r.resize(STRUCTURE_SIZE)
                                       ec = vd.read(offset, STRUCTURE_SIZE, r)
                                       Return waitfor(ec) AndAlso
                                              goto_next()
@@ -95,10 +99,7 @@ Partial Public Class cluster
                                   End If
                               End Function,
                               Function() As Boolean
-                                  If length Is Nothing Then
-                                      'the following end_disk_offset logic requires length to be readable
-                                      length = New pointer(Of Int64)()
-                                  End If
+                                  length.renew()
                                   Return ec.end_result() AndAlso
                                          parse_structure(r, id, used, length, next_id, prev_id) AndAlso
                                          virtdisk_is_fitting(vd, offset, +length) AndAlso

commit a4d5bc72051043b70c48ff977588280f62504b87

file_index and file_key were broken by change 1c40b5d

root/procedure/sync_async.vb
@@ -1,7 +1,11 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
 Imports osi.root.delegates
 Imports osi.root.formation
-Imports osi.root.connector
 Imports osi.root.utils
 
 Public Module _sync_async

service/storage/key_value/impl/file_index.implement.vb
@@ -1,6 +1,10 @@
 ﻿
-Imports osi.root.formation
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.connector
+Imports osi.root.formation
 Imports osi.root.procedure
 Imports osi.root.utils
 
@@ -17,7 +21,7 @@ Partial Public Class file_index
 
     Public Function capacity(ByVal result As pointer(Of Int64)) As event_comb Implements ikeyvt2(Of atom).capacity
         Return sync_async(Function() As Int64
-                              Return ci.capacity()
+                              Return CLng(ci.capacity())
                           End Function,
                           result,
                           +result)
@@ -69,7 +73,7 @@ Partial Public Class file_index
     Public Function full(ByVal result As pointer(Of Boolean)) As event_comb Implements ikeyvt2(Of atom).full
         Dim ec As event_comb = Nothing
         Return New event_comb(Function() As Boolean
-                                  If ci.capacity() <= 0 Then
+                                  If ci.capacity() = 0 Then
                                       Return eva(result, True) AndAlso
                                              goto_end()
                                   Else

service/storage/key_value/impl/file_key.implement.vb
@@ -1,7 +1,9 @@
 ﻿
-Imports osi.root.constants
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.connector
-Imports osi.root.template
 Imports osi.root.formation
 Imports osi.root.procedure
 Imports osi.root.utils
@@ -18,7 +20,7 @@ Partial Public Class file_key
 
     Public Function capacity(ByVal result As pointer(Of Int64)) As event_comb Implements ikeyvt2(Of String).capacity
         Return sync_async(Function() As Int64
-                              Return ci.capacity()
+                              Return CLng(ci.capacity())
                           End Function,
                           result,
                           +result)
@@ -104,7 +106,7 @@ Partial Public Class file_key
                                   If ec.end_result() Then
                                       Dim v As vector(Of Byte()) = Nothing
                                       v = New vector(Of Byte())()
-                                      For i As Int32 = 0 To array_size(+r) - 1
+                                      For i As Int32 = 0 To array_size_i(+r) - 1
                                           Dim k() As Byte = Nothing
                                           If key((+r)(i), k) Then
                                               v.emplace_back(k)

service/storage/virtdisk/virtdisk.capinfo.vb
@@ -39,7 +39,7 @@ Partial Public Class virtdisk
                 assert(Not i.di Is Nothing)
                 Return i.di.capacity()
             ElseIf i.is_ms Then
-                Return CULng(available_virtual_memory())
+                Return available_virtual_memory()
             Else
                 Return 0
             End If