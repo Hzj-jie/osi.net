02a1d9e5e74686cf24b90d029c68d53ee6f5fd80
Hzj_jie <hzj_jie@hotmail.com>
2018-01-15 02:36:36 -0800

    Remove RETIRED code

--- service/convertor/convertor_register/initializer/codegen/run.cmd
+++
@@ -1,8 +0,0 @@
-
-setlocal
-path %PATH%;..\..\..\..\..\root\codegen\precompile;
-precompile npos_uint_bytes_all_binder.vbp > npos_uint_bytes_all_binder.vb
-precompile default_bytes_convertor_binder_initializer.vbp > default_bytes_convertor_binder_initializer.vb
-move /Y *.vb ..\
-endlocal
-
