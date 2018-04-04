66d7d3f39e3a175288045ea9a88c54708db3d370
Hzj_jie <hzj_jie@hotmail.com>
2018-04-03 22:51:56 -0700

    annotated_constructor_case

--- root/tests/connector/type_info_test.vb
+++ root/tests/connector/type_info_test.vb
@@ -3,6 +3,7 @@ Option Explicit On
 Option Infer Off
 Option Strict On
 
+Imports System.Reflection
 Imports osi.root.connector
 Imports osi.root.lock
 Imports osi.root.utt
@@ -38,5 +39,39 @@ Public NotInheritable Class type_info_test
         assert_equal(+finalizer_class.finalizer_called, 1)
         type_info(Of finalizer_class).finalizer()(f)
         assert_equal(+finalizer_class.finalizer_called, 2)
+    End Sub
+
+    Private Class annotated_constructor_test_attribute
+        Inherits Attribute
+    End Class
+
+    Private Class annotated_constructor_test_class
+        <annotated_constructor_test_attribute>
+        Public Sub New(ByVal i As UInt32,
+                       ByVal j As Int32,
+                       Optional ByVal k As String = "abc",
+                       Optional ByVal l As Object = Nothing)
+        End Sub
+    End Class
+
+    <test>
+    Private Shared Sub annotated_constructor_case()
+        Dim info As ConstructorInfo = Nothing
+        info = type_info(Of annotated_constructor_test_class) _
+                       .annotated_constructor_info(Of annotated_constructor_test_attribute)()
+        assert_not_nothing(info)
+        assert_equal(array_size_i(info.GetParameters()), 4)
+        assert_equal(info.GetParameters()(0).ParameterType(), GetType(UInt32))
+        assert_equal(info.GetParameters()(1).ParameterType(), GetType(Int32))
+        assert_equal(info.GetParameters()(2).ParameterType(), GetType(String))
+        assert_equal(info.GetParameters()(3).ParameterType(), GetType(Object))
+        assert_equal(info.GetParameters()(0).Name(), "i")
+        assert_equal(info.GetParameters()(1).Name(), "j")
+        assert_equal(info.GetParameters()(2).Name(), "k")
+        assert_equal(info.GetParameters()(3).Name(), "l")
+        assert_equal(info.GetParameters()(0).RawDefaultValue(), direct_cast(Of Object)(DBNull.Value))
+        assert_equal(info.GetParameters()(1).RawDefaultValue(), direct_cast(Of Object)(DBNull.Value))
+        assert_equal(info.GetParameters()(2).RawDefaultValue(), direct_cast(Of Object)("abc"))
+        assert_equal(info.GetParameters()(3).RawDefaultValue(), direct_cast(Of Object)(Nothing))
     End Sub
 End Class
