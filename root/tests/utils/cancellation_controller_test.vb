7a82171875077acde8d0c705883605536f3f5c93
Hzj_jie <hzj_jie@hotmail.com>
2018-09-12 23:41:56 -0700

    cancellation_controller

---
+++ root/tests/utils/cancellation_controller_test.vb
@@ -0,0 +1,67 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.constants
+Imports osi.root.event
+Imports osi.root.utils
+Imports osi.root.utt
+Imports osi.root.utt.attributes
+
+<test>
+Public NotInheritable Class cancellation_controller_test
+    <test>
+    Private Shared Sub should_cancel_others()
+        Dim c As cancellation_controller = Nothing
+        c = New cancellation_controller()
+        Dim manual As UInt32 = 0
+        Dim ref_counted As UInt32 = 0
+        Dim timeout As UInt32 = 0
+        Dim m As flip_events.manual_flip_event = Nothing
+        Dim r As flip_events.ref_counted_flip_event = Nothing
+        Dim t As flip_event = Nothing
+        m = c.manual(Sub()
+                         manual += uint32_1
+                     End Sub)
+        r = c.ref_counted(uint32_1,
+                          Sub()
+                              ref_counted += uint32_1
+                          End Sub)
+        t = c.timeout(1000, Sub()
+                                timeout += uint32_1
+                            End Sub)
+        assert_reference_equal(m, c.manual())
+        c.manual().raise_to_low()
+        r.unref()
+        assert_equal(manual, uint32_1)
+        assert_equal(ref_counted, uint32_0)
+        assert_equal(timeout, uint32_0)
+        assert_nothing(c.manual())
+        assert_nothing(c.ref_counted())
+        assert_nothing(c.timeout())
+    End Sub
+
+    <test>
+    Private Shared Sub should_cancel_previous()
+        Dim c As cancellation_controller = Nothing
+        c = New cancellation_controller()
+        Dim c1 As UInt32 = 0
+        Dim c2 As UInt32 = 0
+        Dim m As flip_events.manual_flip_event = Nothing
+        m = c.manual(Sub()
+                         c1 += uint32_1
+                     End Sub)
+        c.manual(Sub()
+                     c2 += uint32_1
+                 End Sub)
+        assert_not_reference_equal(m, c.manual())
+        m.raise_to_low()
+        c.manual().raise_to_low()
+        assert_equal(c1, uint32_0)
+        assert_equal(c2, uint32_1)
+    End Sub
+
+    Private Sub New()
+    End Sub
+End Class
---
+++ root/utils/cancellation_controller.vb
@@ -0,0 +1,74 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.root.event
+Imports osi.root.lock
+
+Public NotInheritable Class cancellation_controller
+    Private ReadOnly manual_ref As atomic_ref(Of flip_events.manual_flip_event)
+    Private ReadOnly ref_counted_ref As atomic_ref(Of flip_events.ref_counted_flip_event)
+    Private ReadOnly timeout_ref As atomic_ref(Of flip_event)
+
+    Public Sub New()
+        manual_ref.[New]()
+        ref_counted_ref.[New]()
+        timeout_ref.[New]()
+    End Sub
+
+    Public Function manual(ByVal when_cancel As Action) As flip_events.manual_flip_event
+        replace(manual_ref, flip_events.manual(), when_cancel)
+        Return manual()
+    End Function
+
+    Public Function manual() As flip_events.manual_flip_event
+        Return manual_ref.get()
+    End Function
+
+    Public Function ref_counted(ByVal init_value As UInt32, ByVal when_cancel As Action) As flip_events.ref_counted_flip_event
+        replace(ref_counted_ref, flip_events.ref_counted(init_value), when_cancel)
+        Return ref_counted()
+    End Function
+
+    Public Function ref_counted() As flip_events.ref_counted_flip_event
+        Return ref_counted_ref.get()
+    End Function
+
+    Public Function timeout(ByVal ms As UInt32, ByVal when_cancel As Action) As flip_event
+        replace(timeout_ref, flip_events.timeout(ms), when_cancel)
+        Return timeout()
+    End Function
+
+    Public Function timeout() As flip_event
+        Return timeout_ref.get()
+    End Function
+
+    Private Sub replace(Of T As flip_event)(ByVal ref As atomic_ref(Of T),
+                                            ByVal f As flip_event.[New](Of T),
+                                            ByVal when_cancel As Action)
+        assert(Not ref Is Nothing)
+        assert(Not f Is Nothing)
+        assert(Not when_cancel Is Nothing)
+        Dim old As T = Nothing
+        old = ref.exchange(f(flip_event.events.of_to_low(Sub()
+                                                             when_cancel()
+                                                             cancel_if_not(manual_ref, ref)
+                                                             cancel_if_not(ref_counted_ref, ref)
+                                                             cancel_if_not(timeout_ref, ref)
+                                                         End Sub)))
+        If Not old Is Nothing Then
+            old.cancel()
+        End If
+    End Sub
+
+    Private Shared Sub cancel_if_not(Of T As flip_event, T2 As flip_event) _
+                                    (ByVal ref As atomic_ref(Of T), ByVal cmp As atomic_ref(Of T2))
+        Dim f As flip_event = Nothing
+        f = ref.exchange(Nothing)
+        If Not f Is Nothing AndAlso object_compare(ref, cmp) <> 0 Then
+            f.cancel()
+        End If
+    End Sub
+End Class
