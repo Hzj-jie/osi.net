46eddb8e834d13324074d6f137be7e5809fde300
Hzj_jie <hzj_jie@hotmail.com>
2018-09-27 23:41:42 -0700

    missing file

---
+++ root/utils/invoker/invoker.undefined_delegate.vb
@@ -0,0 +1,12 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public NotInheritable Class invoker
+    Public Delegate Sub undefined_delegate_type()
+
+    ' TODO: Implementation
+    Private Sub New()
+    End Sub
+End Class
