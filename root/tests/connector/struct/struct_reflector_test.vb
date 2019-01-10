d7add4b0d6418cd45bc174db57abd820927bd6c4
Hzj_jie <hzj_jie@hotmail.com>
2019-01-09 16:09:24 -0800

    More struct_reflector_test

--- root/tests/connector/struct/struct_reflector_test.vb
+++ root/tests/connector/struct/struct_reflector_test.vb
@@ -74,14 +74,50 @@ Public NotInheritable Class struct_reflector_test
         assertion.reference_equal(e.b, o.b)
     End Sub
 
+    Private NotInheritable Class c2
+        Public ReadOnly a As String
+
+        Public Sub New()
+            a = guid_str()
+        End Sub
+    End Class
+
     <test>
     Private Shared Sub readonly_variables_case()
-
+        Dim a As c2 = Nothing
+        a = New c2()
+        Dim vs() As struct.variable = Nothing
+        assertion.is_true(struct.disassemble(a, vs))
+        assertion.equal(array_size_i(vs), 1)
+        Dim b As c2 = Nothing
+        assertion.is_true(struct.assemble(vs, b))
+        assertion.equal(a.a, b.a)
+        assertion.reference_equal(a.a, b.a)
     End Sub
+
+    Private NotInheritable Class c3
+        Private a As String
+
+        Public Sub New()
+            a = guid_str()
+        End Sub
+
+        Public Function g() As String
+            Return a
+        End Function
+    End Class
 
     <test>
     Private Shared Sub should_ignore_non_public_variables()
-
+        Dim a As c3 = Nothing
+        a = New c3()
+        Dim vs() As struct.variable = Nothing
+        assertion.is_true(struct.disassemble(a, vs))
+        assertion.equal(array_size_i(vs), 0)
+        Dim b As c3 = Nothing
+        assertion.is_true(struct.assemble(vs, b))
+        assertion.not_equal(a.g(), b.g())
+        assertion.not_reference_equal(a.g(), b.g())
     End Sub
 
     Private Sub New()
