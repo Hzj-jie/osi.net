83f609336a68d87ce476c1324a6c97724da8b24d
Hzj_jie <hzj_jie@hotmail.com>
2019-02-13 10:17:23 -0800

    Use the latest gen to generate exec_case_exe.vb

--- root/utt/selfhealth/codegen/run.cmd
+++ root/utt/selfhealth/codegen/run.cmd
@@ -1,6 +1,8 @@
 
 setlocal
+csc /o+ exec_case_exe.cs
 PATH %PATH%;..\..\..\..\service\resource\gen;
 zipgen.exe _exec_case_exe exec_case_exe exec_case_exe.exe > exec_case_exe.vb
+move /y exec_case_exe.vb ..\
 endlocal
 
