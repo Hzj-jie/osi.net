c35c63a4c7c6708eafb0a2802f8b2cb544bb03d0
Hzj_jie <hzj_jie@hotmail.com>
2019-10-08 23:49:36 -0700

    impl nlexer/escape

--- service/automata/nlexer/characters.vb
+++ service/automata/nlexer/characters.vb
@@ -11,6 +11,9 @@ Partial Public NotInheritable Class nlexer
         Public Const group_separator As Char = character.sheffer
         Public Const group_start As Char = character.left_mid_bracket
         Public Const group_end As Char = character.right_mid_bracket
+        Public Const optional_suffix As Char = character.question_mark
+        Public Const _0_or_more_suffix As Char = character.asterisk
+        Public Const _1_or_more_suffix As Char = character.plus_sign
 
         Private Sub New()
         End Sub
--- service/automata/nlexer/escape.vb
+++ service/automata/nlexer/escape.vb
@@ -3,12 +3,45 @@ Option Explicit On
 Option Infer Off
 Option Strict On
 
-Partial Public NotInheritable Class nlexer
-    Public Shared Function escape(ByVal s As String) As String
+Imports osi.root.connector
+Imports osi.root.constants
+Imports osi.root.formation
 
+Partial Public NotInheritable Class nlexer
+    Private Shared ReadOnly escapes() As pair(Of String, String) = {
+        make_escape(characters.matcher_separator),
+        make_escape(characters.group_separator),
+        make_escape(characters.group_start),
+        make_escape(characters.group_end),
+        make_escape(characters.optional_suffix),
+        make_escape(characters._0_or_more_suffix),
+        make_escape(characters._1_or_more_suffix),
+        make_escape(character.right_slash)
+    }
+
+    Private Shared Function make_escape(ByVal c As Char) As pair(Of String, String)
+        Return pair.emplace_of(character.right_slash + c, character.right_slash + "x" + Convert.ToByte(c).hex())
+    End Function
+
+    Public Shared Function escape(ByVal s As String) As String
+        assert(Not s Is Nothing)
+        If s.null_or_whitespace() Then
+            Return s
+        End If
+        For i As Int32 = 0 To array_size_i(escapes) - 1
+            s = s.Replace(escapes(i).first, escapes(i).second)
+        Next
+        Return s
     End Function
 
     Public Shared Function unescape(ByVal s As String) As String
-
+        assert(Not s Is Nothing)
+        If s.null_or_whitespace() Then
+            Return s
+        End If
+        For i As Int32 = 0 To array_size_i(escapes) - 1
+            s = s.Replace(escapes(i).second, escapes(i).first)
+        Next
+        Return s
     End Function
 End Class
