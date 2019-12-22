62a16036d392cad13b35840dfc338ecc268852a4
Hzj_jie <hzj_jie@hotmail.com>
2019-12-21 16:54:47 -0800

    cstyle_rules

--- service/compiler/cstyle/codegen/run.cmd
+++ service/compiler/cstyle/codegen/run.cmd
@@ -1,7 +1,7 @@
 
 setlocal
 path %PATH%;..\..\..\resource\gen\;
-zipgen.exe cstyle_rules rlexer_rule rlexer_rule.txt syntaxer_rule syntaxer_rule.txt > cstyle_rules.vb
+zipgen.exe cstyle_rules nlexer_rule nlexer_rule.txt rlexer_rule rlexer_rule.txt syntaxer_rule syntaxer_rule.txt > cstyle_rules.vb
 move /Y *.vb ..\
 endlocal
 
---
+++ service/compiler/cstyle/cstyle.vb
@@ -0,0 +1,16 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.service.automata
+Imports osi.service.resource
+
+Public NotInheritable Class cstyle
+    Public Shared ReadOnly nlp As nlp
+
+    Shared Sub New()
+        assert(nlp.of(cstyle_rules.nlexer_rule.as_text(), cstyle_rules.syntaxer_rule.as_text(), nlp))
+    End Sub
+End Class
