4d6bba95196cbba364787bcffa1503fdc433180c
Hzj_jie <hzj_jie@hotmail.com>
2020-01-11 23:42:50 -0800

    logic/exportable debug dump

--- service/compiler/logic/exportable.vb
+++ service/compiler/logic/exportable.vb
@@ -6,30 +6,41 @@ Option Strict On
 Imports System.Runtime.CompilerServices
 Imports osi.root.connector
 Imports osi.root.constants
+Imports osi.root.envs
 Imports osi.root.formation
 Imports osi.service.interpreter.primitive
 
 Namespace logic
     Public Module _exportable
+        Private ReadOnly debug_dump As Boolean
+
+        Sub New()
+            debug_dump = env_bool(env_keys("logic", "debug", "dump"))
+        End Sub
+
         <Extension()> Public Function import(ByVal e As interpreter.primitive.exportable,
                                              ByVal scope As scope,
                                              ByVal es() As exportable) As Boolean
             assert(Not e Is Nothing)
             If scope Is Nothing Then
                 Return False
-            Else
-                Dim o As vector(Of String) = Nothing
-                o = New vector(Of String)()
-                For i As Int32 = 0 To array_size_i(es) - 1
-                    If es(i) Is Nothing Then
-                        Return False
-                    End If
-                    If Not es(i).export(scope, o) Then
-                        Return False
-                    End If
-                Next
-                Return debug_assert(e.import(o.ToString(character.newline)))
             End If
+            Dim o As vector(Of String) = Nothing
+            o = New vector(Of String)()
+            For i As Int32 = 0 To array_size_i(es) - 1
+                If es(i) Is Nothing Then
+                    Return False
+                End If
+                If Not es(i).export(scope, o) Then
+                    Return False
+                End If
+            Next
+            Dim r As String = Nothing
+            r = o.ToString(character.newline);
+            If debug_dump Then
+                raise_error(error_type.user, "Debug dump of primitive ", r)
+            End If
+            Return debug_assert(e.import(r))
         End Function
 
         <Extension()> Public Function import(ByVal e As interpreter.primitive.exportable,
