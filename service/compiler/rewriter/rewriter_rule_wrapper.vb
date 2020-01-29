e1e52987af08eb0165b97537750dbeeb588fd3b9
Hzj_jie <hzj_jie@hotmail.com>
2020-01-29 12:03:42 -0800

    Merge rewriter / rewriter_wrapper into rewriter_rule_wrapper file

--- service/compiler/rewriter/rewriter_rule_wrapper.vb
+++ service/compiler/rewriter/rewriter_rule_wrapper.vb
@@ -10,6 +10,22 @@ Imports osi.service.interpreter.primitive
 Imports osi.service.compiler.rewriters
 Imports exportable = osi.service.compiler.logic.exportable
 
+Partial Public NotInheritable Class rewriters
+    Inherits code_gens(Of typed_node_writer)
+
+    Public Interface rewriter
+        Inherits code_gen(Of typed_node_writer)
+    End Interface
+
+    Public MustInherit Class rewriter_wrapper
+        Inherits code_gen_wrapper(Of typed_node_writer)
+
+        Protected Sub New(ByVal i As rewriters)
+            MyBase.New(i)
+        End Sub
+    End Class
+End Class
+
 Public NotInheritable Class rewriter_rule_wrapper
 End Class
 
