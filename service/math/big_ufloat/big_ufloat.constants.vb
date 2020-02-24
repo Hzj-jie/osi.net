975061198cdabb8f082c6472e1d0275e86349075
Hzj_jie <hzj_jie@hotmail.com>
2020-02-24 00:40:24 -0800

    First big_ufloat.str test

---
+++ service/math/big_ufloat/big_ufloat.constants.vb
@@ -0,0 +1,13 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Partial Public NotInheritable Class big_ufloat
+    Private NotInheritable Class constants
+        Public Const str_base As Byte = 10
+
+        Private Sub New()
+        End Sub
+    End Class
+End Class
---
+++ service/tests/math/big_ufloat/big_ufloat_str_test.vb
@@ -0,0 +1,23 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.utt
+Imports osi.root.utt.attributes
+Imports osi.service.math
+
+<test>
+Public NotInheritable Class big_ufloat_str_test
+    <test>
+    Private Shared Sub e()
+        Dim v As big_ufloat = Nothing
+        v = New big_ufloat(System.Math.E)
+
+        assertion.equal(v.str(), "2.718281828459044646706388")
+        '                         2.71828182845905
+    End Sub
+
+    Private Sub New()
+    End Sub
+End Class
