6d42e7f39835ce773ced8934468e56c9f408bb48
Hzj_jie <hzj_jie@hotmail.com>
2020-02-27 21:23:41 -0800

    reference_count_event_comb tests are flaky

--- root/tests/procedure/event_comb/reference_count/codegen/run.cmd
+++ root/tests/procedure/event_comb/reference_count/codegen/run.cmd
@@ -4,5 +4,6 @@ PATH %PATH%;..\..\..\..\..\codegen\precompile\;
 precompile reference_count_event_comb_test.vbp > reference_count_event_comb_test.vb
 precompile reference_count_event_comb_1_test.vbp > reference_count_event_comb_1_test.vb
 precompile reference_count_event_comb_2_test.vbp > reference_count_event_comb_2_test.vb
+move /y *.vb ..\
 endlocal
 
