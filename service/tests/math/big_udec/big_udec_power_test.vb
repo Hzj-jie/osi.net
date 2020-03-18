90423924461753c2723a9cc1f666726f0fcaf3cf
Hzj_jie <hzj_jie@hotmail.com>
2020-03-18 12:21:35 -0700

    More pi sequence-of-numbers

--- service/tests/math/big_udec/big_udec_power_test.vb
+++ service/tests/math/big_udec/big_udec_power_test.vb
@@ -123,7 +123,79 @@ Public NotInheritable Class big_udec_power_test
                 s.assert_sub(c)
             End If
         Next
-        assertion.equal(s.str(), "3.141623170236616979912392273028839981728932502884783801155901887")
+        assertion.equal(s.str(), "3.141600282926117160615236325805874484799183595558135389722686711")
+        '                         3.14159265
+    End Sub
+
+    <command_line_specified>
+    <test>
+    Private Shared Sub calculate_pi_sequence_of_numbers_1048577()
+        Dim s As big_udec = Nothing
+        s = big_udec.zero()
+        For i As UInt32 = 1 To 1048577 Step 2
+            Dim c As big_udec = Nothing
+            c = big_udec.fraction(CUInt(4), i)
+            If ((i \ 2) Mod 2) = 0 Then
+                s.add(c)
+            Else
+                s.assert_sub(c)
+            End If
+        Next
+        assertion.equal(s.str(), "3.141600282926117160615236325805874484799183595558135389722686711")
+        '                         3.14159265
+    End Sub
+
+    <command_line_specified>
+    <test>
+    Private Shared Sub calculate_pi_sequence_of_numbers_4194305()
+        Dim s As big_udec = Nothing
+        s = big_udec.zero()
+        For i As UInt32 = 1 To 4194305 Step 2
+            Dim c As big_udec = Nothing
+            c = big_udec.fraction(CUInt(4), i)
+            If ((i \ 2) Mod 2) = 0 Then
+                s.add(c)
+            Else
+                s.assert_sub(c)
+            End If
+        Next
+        assertion.equal(s.str(), "3.141600282926117160615236325805874484799183595558135389722686711")
+        '                         3.14159265
+    End Sub
+
+    <command_line_specified>
+    <test>
+    Private Shared Sub calculate_pi_sequence_of_numbers_16777217()
+        Dim s As big_udec = Nothing
+        s = big_udec.zero()
+        For i As UInt32 = 1 To 16777217 Step 2
+            Dim c As big_udec = Nothing
+            c = big_udec.fraction(CUInt(4), i)
+            If ((i \ 2) Mod 2) = 0 Then
+                s.add(c)
+            Else
+                s.assert_sub(c)
+            End If
+        Next
+        assertion.equal(s.str(), "3.141600282926117160615236325805874484799183595558135389722686711")
+        '                         3.14159265
+    End Sub
+
+    <command_line_specified>
+    <test>
+    Private Shared Sub calculate_pi_sequence_of_numbers_67108865()
+        Dim s As big_udec = Nothing
+        s = big_udec.zero()
+        For i As UInt32 = 1 To 67108865 Step 2
+            Dim c As big_udec = Nothing
+            c = big_udec.fraction(CUInt(4), i)
+            If ((i \ 2) Mod 2) = 0 Then
+                s.add(c)
+            Else
+                s.assert_sub(c)
+            End If
+        Next
+        assertion.equal(s.str(), "3.141600282926117160615236325805874484799183595558135389722686711")
         '                         3.14159265
     End Sub
 
