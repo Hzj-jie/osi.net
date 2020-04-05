Hzj-jie/osi.net

commit bb6c8085ee62ceb5255baab9f5890de3578e0a13

vector was broken by a96ca4836938e89954728f9cafd1789f618174f0

root/codegen/adaptive_array/adaptive_array.cs
@@ -139,9 +139,6 @@ public static class Program
         w(strcat(
 "    Implements ICloneable, ICloneable(Of " + class_name(a) + "), IComparable(Of " + class_name(a) + "), IComparable",
 "",
-"    Private Const size_limitation As UInt32 = (max_uint32 >> 1)",
-"    'Private Shared ReadOnly default_value As " + a.type + " = Nothing",
-"",
 "    Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32",
 "        assert(n <= max_array_size)",
 "        If n = max_array_size Then",
@@ -202,7 +199,7 @@ public static class Program
 "",
 "    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Function max_size() As UInt32",
-"        Return size_limitation",
+"        Return max_array_size",
 "    End Function",
 "",
 "    <MethodImpl(method_impl_options.aggressive_inlining)>",
@@ -252,58 +249,51 @@ public static class Program
 "        Return d(CInt(size() - uint32_1))",
 "    End Function",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Sub clear()",
 "        s = uint32_0",
-"#If 0 Then",
-"        If size() > uint32_0 Then",
-"            memclr(d, uint32_0, s)",
-"            s = uint32_0",
-"        End If",
-"#End If",
 "    End Sub",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Sub push_back(ByVal v As " + a.type + ")",
 "        reserve(size() + uint32_1)",
 "        d(CInt(size())) = v",
 "        s += uint32_1",
 "    End Sub",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Sub pop_back()",
 "        s -= uint32_1",
-"        'd(CInt(size())) = default_value",
 "    End Sub",
 "",
 "    Public Sub reserve(ByVal n As UInt32)",
-"        If capacity() < n Then",
-"            Dim ec As UInt32 = 0",
-"            ec = expected_capacity(n)",
-"            assert(ec >= uint32_1)",
-"            If empty() Then",
-"                ReDim d(CInt(ec - uint32_1))",
-"            Else",
-"                ReDim Preserve d(CInt(ec - uint32_1))",
-"            End If",
+"        If capacity() >= n Then",
+"            Return",
+"        End If",
+"        Dim ec As UInt32 = 0",
+"        ec = expected_capacity(n)",
+"        assert(ec >= uint32_1)",
+"        If empty() Then",
+"            ReDim d(CInt(ec - uint32_1))",
+"        Else",
+"            ReDim Preserve d(CInt(ec - uint32_1))",
 "        End If",
 "    End Sub",
 "",
 "    Public Sub resize(ByVal n As UInt32)",
-"        If capacity() < n Then",
-"            reserve(n)",
-"        'ElseIf size() > n Then",
-"            'memclr(d, n, size() - n)",
+"        reserve(n)",
+"        If size() < n Then",
+"            memclr(d, size(), n - size())",
 "        End If",
 "        s = n",
 "    End Sub",
 "",
 "    Public Sub resize(ByVal n As UInt32, ByVal v As " + a.type + ")",
-"        Dim os As UInt32 = 0",
-"        os = size()",
-"        If n > os Then",
-"            resize(n)",
-"            memset(d, os, n - os, v)",
-"        Else",
-"            resize(n)",
+"        reserve(n)",
+"        If size() < n Then",
+"            memset(d, size(), n - size(), v)",
 "        End If",
+"        s = n",
 "    End Sub",
 "",
 "    Public Sub shrink_to_fit()",
@@ -315,6 +305,7 @@ public static class Program
 "        End If",
 "    End Sub",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Function Clone() As Object Implements ICloneable.Clone",
 "        Return CloneT()",
 "    End Function",

root/formation/vector.adaptive_array.vb
@@ -18,9 +18,6 @@ Partial Public Class vector(Of T)
 Private Class adaptive_array_t
     Implements ICloneable, ICloneable(Of adaptive_array_t), IComparable(Of adaptive_array_t), IComparable
 
-    Private Const size_limitation As UInt32 = (max_uint32 >> 1)
-    'Private Shared ReadOnly default_value As T = Nothing
-
     Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32
         assert(n <= max_array_size)
         If n = max_array_size Then
@@ -81,7 +78,7 @@ Private Class adaptive_array_t
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function max_size() As UInt32
-        Return size_limitation
+        Return max_array_size
     End Function
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
@@ -131,58 +128,51 @@ Private Class adaptive_array_t
         Return d(CInt(size() - uint32_1))
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub clear()
         s = uint32_0
-#If 0 Then
-        If size() > uint32_0 Then
-            memclr(d, uint32_0, s)
-            s = uint32_0
-        End If
-#End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub push_back(ByVal v As T)
         reserve(size() + uint32_1)
         d(CInt(size())) = v
         s += uint32_1
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub pop_back()
         s -= uint32_1
-        'd(CInt(size())) = default_value
     End Sub
 
     Public Sub reserve(ByVal n As UInt32)
-        If capacity() < n Then
-            Dim ec As UInt32 = 0
-            ec = expected_capacity(n)
-            assert(ec >= uint32_1)
-            If empty() Then
-                ReDim d(CInt(ec - uint32_1))
-            Else
-                ReDim Preserve d(CInt(ec - uint32_1))
-            End If
+        If capacity() >= n Then
+            Return
+        End If
+        Dim ec As UInt32 = 0
+        ec = expected_capacity(n)
+        assert(ec >= uint32_1)
+        If empty() Then
+            ReDim d(CInt(ec - uint32_1))
+        Else
+            ReDim Preserve d(CInt(ec - uint32_1))
         End If
     End Sub
 
     Public Sub resize(ByVal n As UInt32)
-        If capacity() < n Then
-            reserve(n)
-        'ElseIf size() > n Then
-            'memclr(d, n, size() - n)
+        reserve(n)
+        If size() < n Then
+            memclr(d, size(), n - size())
         End If
         s = n
     End Sub
 
     Public Sub resize(ByVal n As UInt32, ByVal v As T)
-        Dim os As UInt32 = 0
-        os = size()
-        If n > os Then
-            resize(n)
-            memset(d, os, n - os, v)
-        Else
-            resize(n)
+        reserve(n)
+        If size() < n Then
+            memset(d, size(), n - size(), v)
         End If
+        s = n
     End Sub
 
     Public Sub shrink_to_fit()
@@ -194,6 +184,7 @@ Private Class adaptive_array_t
         End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function Clone() As Object Implements ICloneable.Clone
         Return CloneT()
     End Function

service/math/big_uint/big_uint.adaptive_array.vb
@@ -18,9 +18,6 @@ Partial Public Class big_uint
 Private Class adaptive_array_uint32
     Implements ICloneable, ICloneable(Of adaptive_array_uint32), IComparable(Of adaptive_array_uint32), IComparable
 
-    Private Const size_limitation As UInt32 = (max_uint32 >> 1)
-    'Private Shared ReadOnly default_value As UInt32 = Nothing
-
     Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32
         assert(n <= max_array_size)
         If n = max_array_size Then
@@ -81,7 +78,7 @@ Private Class adaptive_array_uint32
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function max_size() As UInt32
-        Return size_limitation
+        Return max_array_size
     End Function
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
@@ -131,58 +128,51 @@ Private Class adaptive_array_uint32
         Return d(CInt(size() - uint32_1))
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub clear()
         s = uint32_0
-#If 0 Then
-        If size() > uint32_0 Then
-            memclr(d, uint32_0, s)
-            s = uint32_0
-        End If
-#End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub push_back(ByVal v As UInt32)
         reserve(size() + uint32_1)
         d(CInt(size())) = v
         s += uint32_1
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub pop_back()
         s -= uint32_1
-        'd(CInt(size())) = default_value
     End Sub
 
     Public Sub reserve(ByVal n As UInt32)
-        If capacity() < n Then
-            Dim ec As UInt32 = 0
-            ec = expected_capacity(n)
-            assert(ec >= uint32_1)
-            If empty() Then
-                ReDim d(CInt(ec - uint32_1))
-            Else
-                ReDim Preserve d(CInt(ec - uint32_1))
-            End If
+        If capacity() >= n Then
+            Return
+        End If
+        Dim ec As UInt32 = 0
+        ec = expected_capacity(n)
+        assert(ec >= uint32_1)
+        If empty() Then
+            ReDim d(CInt(ec - uint32_1))
+        Else
+            ReDim Preserve d(CInt(ec - uint32_1))
         End If
     End Sub
 
     Public Sub resize(ByVal n As UInt32)
-        If capacity() < n Then
-            reserve(n)
-        'ElseIf size() > n Then
-            'memclr(d, n, size() - n)
+        reserve(n)
+        If size() < n Then
+            memclr(d, size(), n - size())
         End If
         s = n
     End Sub
 
     Public Sub resize(ByVal n As UInt32, ByVal v As UInt32)
-        Dim os As UInt32 = 0
-        os = size()
-        If n > os Then
-            resize(n)
-            memset(d, os, n - os, v)
-        Else
-            resize(n)
+        reserve(n)
+        If size() < n Then
+            memset(d, size(), n - size(), v)
         End If
+        s = n
     End Sub
 
     Public Sub shrink_to_fit()
@@ -194,6 +184,7 @@ Private Class adaptive_array_uint32
         End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function Clone() As Object Implements ICloneable.Clone
         Return CloneT()
     End Function

commit f8fcb238c37b3bc80064230a989ea25323b50a12

vector_test was broken by bb6c8085ee62ceb5255baab9f5890de3578e0a13

root/codegen/adaptive_array/adaptive_array.cs
@@ -302,7 +302,14 @@ public static class Program
 "        ElseIf capacity() > size() Then",
 "            assert(size() >= uint32_1)",
 "            ReDim Preserve d(CInt(size() - uint32_1))",
+"        Else",
+"            assert(capacity() = size())",
 "        End If",
+"    End Sub",
+"",
+"    Public Sub clear_unused_slots()",
+"        assert(capacity() >= size())",
+"        memclr(d, size(), capacity() - size())",
 "    End Sub",
 "",
 "    <MethodImpl(method_impl_options.aggressive_inlining)>",

root/formation/vector.adaptive_array.vb
@@ -181,7 +181,14 @@ Private Class adaptive_array_t
         ElseIf capacity() > size() Then
             assert(size() >= uint32_1)
             ReDim Preserve d(CInt(size() - uint32_1))
+        Else
+            assert(capacity() = size())
         End If
+    End Sub
+
+    Public Sub clear_unused_slots()
+        assert(capacity() >= size())
+        memclr(d, size(), capacity() - size())
     End Sub
 
     <MethodImpl(method_impl_options.aggressive_inlining)>

root/formation/vector.vb
@@ -222,6 +222,10 @@ Public NotInheritable Class vector(Of T)
         Return push_back(vs, start, n, True)
     End Function
 
+    Public Sub clear_unused_slots()
+        v.clear_unused_slots()
+    End Sub
+
     '.net implementations
     Public Function Clone() As Object Implements ICloneable.Clone
         Return CloneT()

service/math/big_uint/big_uint.adaptive_array.vb
@@ -181,7 +181,14 @@ Private Class adaptive_array_uint32
         ElseIf capacity() > size() Then
             assert(size() >= uint32_1)
             ReDim Preserve d(CInt(size() - uint32_1))
+        Else
+            assert(capacity() = size())
         End If
+    End Sub
+
+    Public Sub clear_unused_slots()
+        assert(capacity() >= size())
+        memclr(d, size(), capacity() - size())
     End Sub
 
     <MethodImpl(method_impl_options.aggressive_inlining)>

root/tests/formation/vector/vector_test.calls.vb
@@ -1,9 +1,12 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports osi.root.connector
-Imports osi.root.utt
-Imports osi.root.formation
 Imports osi.root.constants
-Imports osi.root.utils
+Imports osi.root.formation
+Imports osi.root.utt
 
 Partial Public Class vector_test
     Partial Private Class vector_case
@@ -22,16 +25,19 @@ Partial Public Class vector_test
                 assertion.equal(v.size() - oldsize, till,
                              "cannot update size property after ", till, " push_back operations.")
             Else
-                Dim t As Int32 = 0
+                Dim t As UInt32 = 0
                 t = v.size()
             End If
         End Sub
 
         Private Sub clear()
+            Dim old_size As UInt32 = 0
+            old_size = v.size()
             v.clear()
             assertion.equal(v.size(), uint32_0, "v.size() <> 0 after clear.")
             If validation Then
-                For i As Int32 = 0 To v.capacity() - 1
+                v.clear_unused_slots()
+                For i As Int32 = 0 To CInt(v.capacity()) - 1
                     assertion.is_null(v.data()(i), "cannot clear ", i, " after clear operation.")
                 Next
             End If
@@ -42,12 +48,12 @@ Partial Public Class vector_test
                 Dim a() As String = Nothing
                 rndarray(a)
 
-                Dim oldsize As Int64
+                Dim oldsize As UInt32 = 0
                 oldsize = v.size()
                 assertion.is_true(v.push_back(a))
 
-                assertion.equal(oldsize + a.Length(), v.size())
-                assertion.is_true(memcmp(v.data(), oldsize, a, 0, a.Length()) = 0, "caught v.data <> a.data.")
+                assertion.equal(oldsize + array_size(a), v.size())
+                assertion.is_true(memcmp(v.data(), oldsize, a, 0, array_size(a)) = 0, "caught v.data <> a.data.")
             Else
                 v.data()
             End If
@@ -83,32 +89,32 @@ Partial Public Class vector_test
         Private Sub insert()
             'make sure there is at least one element, to avoid breaking v.back()
             v.push_back(guid_str())
-            Dim oldsize As Int64 = 0
+            Dim oldsize As UInt32 = 0
             oldsize = v.size()
             Dim last_back As String = Nothing
             last_back = v.back()
 
             Dim s As String = Nothing
             s = guid_str()
-            Dim p As Int64 = 0
+            Dim p As UInt32 = 0
             If rnd_bool() Then
                 p = oldsize
             Else
-                p = rnd_int(0, oldsize)
+                p = rnd_uint(0, oldsize)
             End If
             v.insert(p, s)
 
             If validation Then
-                assertion.equal(v.find(s), p)
+                assertion.equal(v.find(s), CInt(p))
                 If p = oldsize Then
-                    assertion.equal(v.size(), p + 1)
-                    assertion.equal(v.find(last_back), oldsize - 1)
+                    assertion.equal(v.size(), p + uint32_1)
+                    assertion.equal(v.find(last_back), CInt(oldsize - uint32_1))
                 Else
                     'do not allow nothing to be inserted in to the vector to avoid breaking erase case
                     assert(p < oldsize)
                     assertion.equal(oldsize, v.size() - 1)
                     'oldsize + 1 - 1 = v.size() - 1
-                    assertion.equal(v.find(last_back), oldsize)
+                    assertion.equal(v.find(last_back), CInt(oldsize))
                 End If
             End If
         End Sub
@@ -118,11 +124,11 @@ Partial Public Class vector_test
                 Dim a() As String = Nothing
                 rndarray(a)
 
-                Dim oldsize As Int64 = 0
-                oldsize = v.size()
+                Dim oldsize As Int32 = 0
+                oldsize = CInt(v.size())
                 assertion.is_true(v.push_back(a))
 
-                For i As Int64 = 0 To a.Length() - 1
+                For i As Int32 = 0 To a.Length() - 1
                     assertion.equal(v.find(a(i)), oldsize + i, "caught a(", i, ") is not at the place expected.")
                 Next
             Else
@@ -140,7 +146,7 @@ Partial Public Class vector_test
             assertion.is_not_null(a, "found a is nothing")
             assertion.equal(CUInt(a.Length()), v.size(), "a.length()<>v.size()")
             If validation Then
-                assertion.is_true(memcmp(v.data(), 0, a, 0, a.Length()) = 0, "found v is not same as a.")
+                assertion.is_true(memcmp(v.data(), 0, a, 0, array_size(a)) = 0, "found v is not same as a.")
             End If
         End Sub
 
@@ -168,16 +174,16 @@ Partial Public Class vector_test
             'make sure there is at least one element in the vector
             v.push_back(guid_str())
             Dim a() As String = Nothing
-            Dim i As Int32 = 0
-            Dim j As Int32 = 0
-            i = rnd_int(0, v.size())
-            j = rnd_int(0, v.size())
+            Dim i As UInt32 = 0
+            Dim j As UInt32 = 0
+            i = rnd_uint(0, v.size())
+            j = rnd_uint(0, v.size())
             If i > j Then
                 swap(i, j)
             End If
 
             If validation Then
-                ReDim a(j - i + 1)
+                ReDim a(CInt(j - i + uint32_1))
                 memcpy(a, 0, v.data(), i, j - i)
             End If
 
@@ -185,35 +191,35 @@ Partial Public Class vector_test
 
             'confirm at least one element here.
             v.push_back(guid_str())
-            i = rnd_int(0, v.size())
+            i = rnd_uint(0, v.size())
             If validation Then
                 copy(a(a.Length() - 2), v(i))
             End If
-            assertion.is_true(v.erase(i))
+            v.erase(i)
 
             'confirm at least one element here.
             v.push_back(guid_str())
-            i = rnd_int(0, v.size())
+            i = rnd_uint(0, v.size())
             If validation Then
                 copy(a(a.Length() - 1), v(i))
             End If
             assertion.is_true(v.erase(v(i)))
 
             If validation Then
-                For i = 0 To a.Length() - 1
-                    assertion.equal(v.find(a(i)), npos, "still find ", a(i), " in vector.")
+                For k As Int32 = 0 To a.Length() - 1
+                    assertion.equal(v.find(a(k)), npos, "still find ", a(k), " in vector.")
                 Next
             End If
         End Sub
 
         Private Sub shrink_to_fit()
             Dim a() As String = Nothing
-            ReDim a(v.size() - 1)
-            memcpy(a, v.data(), a.Length())
+            ReDim a(CInt(v.size()) - 1)
+            memcpy(a, v.data(), array_size(a))
             v.shrink_to_fit()
             assertion.equal(v.capacity(), v.size(), "v.capacity() <> v.size() after shrink_to_fit.")
             If validation Then
-                assertion.is_true(memcmp(v.data(), 0, a, 0, a.Length()) = 0, "v.data <> a after shrink_to_fit.")
+                assertion.is_true(memcmp(v.data(), 0, a, 0, array_size(a)) = 0, "v.data <> a after shrink_to_fit.")
             End If
         End Sub
 

root/tests/formation/vector/vector_test.vb
@@ -7,7 +7,7 @@ Imports osi.root.connector
 Imports osi.root.formation
 Imports osi.root.utt
 
-Partial Public Class vector_test
+Partial Public NotInheritable Class vector_test
     Inherits repeat_case_wrapper
     Private ReadOnly size_base As Int32 = 0
 