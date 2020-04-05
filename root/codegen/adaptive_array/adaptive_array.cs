Hzj-jie/osi.net

commit a96ca4836938e89954728f9cafd1789f618174f0

inline most of big_uint methods

root/codegen/adaptive_array/adaptive_array.cs
@@ -140,7 +140,7 @@ public static class Program
 "    Implements ICloneable, ICloneable(Of " + class_name(a) + "), IComparable(Of " + class_name(a) + "), IComparable",
 "",
 "    Private Const size_limitation As UInt32 = (max_uint32 >> 1)",
-"    Private Shared ReadOnly default_value As " + a.type + " = Nothing",
+"    'Private Shared ReadOnly default_value As " + a.type + " = Nothing",
 "",
 "    Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32",
 "        assert(n <= max_array_size)",
@@ -253,10 +253,13 @@ public static class Program
 "    End Function",
 "",
 "    Public Sub clear()",
+"        s = uint32_0",
+"#If 0 Then",
 "        If size() > uint32_0 Then",
 "            memclr(d, uint32_0, s)",
 "            s = uint32_0",
 "        End If",
+"#End If",
 "    End Sub",
 "",
 "    Public Sub push_back(ByVal v As " + a.type + ")",
@@ -267,7 +270,7 @@ public static class Program
 "",
 "    Public Sub pop_back()",
 "        s -= uint32_1",
-"        d(CInt(size())) = default_value",
+"        'd(CInt(size())) = default_value",
 "    End Sub",
 "",
 "    Public Sub reserve(ByVal n As UInt32)",
@@ -286,8 +289,8 @@ public static class Program
 "    Public Sub resize(ByVal n As UInt32)",
 "        If capacity() < n Then",
 "            reserve(n)",
-"        ElseIf size() > n Then",
-"            memclr(d, n, size() - n)",
+"        'ElseIf size() > n Then",
+"            'memclr(d, n, size() - n)",
 "        End If",
 "        s = n",
 "    End Sub",

root/formation/vector.adaptive_array.vb
@@ -19,7 +19,7 @@ Private Class adaptive_array_t
     Implements ICloneable, ICloneable(Of adaptive_array_t), IComparable(Of adaptive_array_t), IComparable
 
     Private Const size_limitation As UInt32 = (max_uint32 >> 1)
-    Private Shared ReadOnly default_value As T = Nothing
+    'Private Shared ReadOnly default_value As T = Nothing
 
     Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32
         assert(n <= max_array_size)
@@ -132,10 +132,13 @@ Private Class adaptive_array_t
     End Function
 
     Public Sub clear()
+        s = uint32_0
+#If 0 Then
         If size() > uint32_0 Then
             memclr(d, uint32_0, s)
             s = uint32_0
         End If
+#End If
     End Sub
 
     Public Sub push_back(ByVal v As T)
@@ -146,7 +149,7 @@ Private Class adaptive_array_t
 
     Public Sub pop_back()
         s -= uint32_1
-        d(CInt(size())) = default_value
+        'd(CInt(size())) = default_value
     End Sub
 
     Public Sub reserve(ByVal n As UInt32)
@@ -165,8 +168,8 @@ Private Class adaptive_array_t
     Public Sub resize(ByVal n As UInt32)
         If capacity() < n Then
             reserve(n)
-        ElseIf size() > n Then
-            memclr(d, n, size() - n)
+        'ElseIf size() > n Then
+            'memclr(d, n, size() - n)
         End If
         s = n
     End Sub

service/math/big_uint/big_uint.adaptive_array.vb
@@ -19,7 +19,7 @@ Private Class adaptive_array_uint32
     Implements ICloneable, ICloneable(Of adaptive_array_uint32), IComparable(Of adaptive_array_uint32), IComparable
 
     Private Const size_limitation As UInt32 = (max_uint32 >> 1)
-    Private Shared ReadOnly default_value As UInt32 = Nothing
+    'Private Shared ReadOnly default_value As UInt32 = Nothing
 
     Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32
         assert(n <= max_array_size)
@@ -132,10 +132,13 @@ Private Class adaptive_array_uint32
     End Function
 
     Public Sub clear()
+        s = uint32_0
+#If 0 Then
         If size() > uint32_0 Then
             memclr(d, uint32_0, s)
             s = uint32_0
         End If
+#End If
     End Sub
 
     Public Sub push_back(ByVal v As UInt32)
@@ -146,7 +149,7 @@ Private Class adaptive_array_uint32
 
     Public Sub pop_back()
         s -= uint32_1
-        d(CInt(size())) = default_value
+        'd(CInt(size())) = default_value
     End Sub
 
     Public Sub reserve(ByVal n As UInt32)
@@ -165,8 +168,8 @@ Private Class adaptive_array_uint32
     Public Sub resize(ByVal n As UInt32)
         If capacity() < n Then
             reserve(n)
-        ElseIf size() > n Then
-            memclr(d, n, size() - n)
+        'ElseIf size() > n Then
+            'memclr(d, n, size() - n)
         End If
         s = n
     End Sub

root/utils/utils.vbproj
@@ -59,6 +59,7 @@
     <Compile Include="assembly.vb" />
     <Compile Include="autolock\autolock_dll_exporter.vb" />
     <Compile Include="binding_flags.vb" />
+    <Compile Include="cached_value.vb" />
     <Compile Include="cancellation_controller.vb" />
     <Compile Include="choose_group.vb" />
     <Compile Include="console_key_info_mapping.vb" />

service/math/big_uint/big_uint.assert.vb
@@ -10,6 +10,7 @@ Partial Public NotInheritable Class big_uint
         Return b > 1 AndAlso b <= support_str_base
     End Function
 
+    ' TODO: This method should be used somewhere everywhere.
     Private Sub assert_no_extra_blank_position()
         assert(v.empty() OrElse v.back() > 0)
     End Sub

service/math/big_uint/big_uint.bit.vb
@@ -11,6 +11,7 @@ Imports osi.root.connector
 Imports osi.root.constants
 
 Partial Public NotInheritable Class big_uint
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Private Sub set_bit_count(ByVal s As UInt64)
         set_zero()
         Dim l As UInt32 = 0
@@ -25,17 +26,20 @@ Partial Public NotInheritable Class big_uint
         v.resize(l)
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Private Sub bit_pos(ByVal pos As UInt64, ByRef v_index As UInt32, ByRef b_index As Byte)
         bit_rpos(pos, v_index, b_index)
         v_index = v.size() - uint32_1 - v_index
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Private Sub bit_rpos(ByVal pos As UInt64,
                          ByRef v_index As UInt32,
                          ByRef b_rindex As Byte)
         bit_rpos(pos, v_index, b_rindex, Nothing, False, True)
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Private Sub bit_rpos(ByVal pos As UInt64,
                          ByRef v_index As UInt32,
                          ByRef b_rindex As Byte,
@@ -53,10 +57,8 @@ Partial Public NotInheritable Class big_uint
         v_index = CUInt(pos >> bit_count_in_uint32_shift)
         b_rindex = CByte(pos And bit_count_in_uint32_mask)
 #End If
-        If fill AndAlso overflow Then
-            If v_index >= v.size() Then
-                v.resize(v_index + uint32_1)
-            End If
+        If fill AndAlso overflow AndAlso v_index >= v.size() Then
+            v.resize(v_index + uint32_1)
         End If
     End Sub
 
@@ -64,29 +66,30 @@ Partial Public NotInheritable Class big_uint
     '1. if that is longer than me, ignore the overlength bits
     '2. if that is shorter than me, treat all the bits left as 0
     Private Function bit_wise_operation(ByVal that As big_uint, ByVal op As bit_wise_operator) As big_uint
-        If Not that Is Nothing AndAlso Not is_zero() Then
-            For i As UInt32 = 0 To v.size() - uint32_1
-                Dim t As UInt32 = 0
-                If i < that.v.size() Then
-                    t = that.v.get(i)
-                Else
-                    t = 0
-                End If
-                Select Case op
-                    Case bit_wise_operator.and
-                        v.set(i, v.get(i) And t)
-                    Case bit_wise_operator.or
-                        v.set(i, v.get(i) Or t)
-                    Case bit_wise_operator.xor
-                        v.set(i, v.get(i) Xor t)
-                    Case Else
-                        assert(False)
-                End Select
-            Next
-            If op = bit_wise_operator.and OrElse
-               op = bit_wise_operator.xor Then
-                remove_extra_blank()
+        If that Is Nothing Then
+            Return Me
+        End If
+        For i As UInt32 = 0 To v.size() - uint32_1
+            Dim t As UInt32 = 0
+            If i < that.v.size() Then
+                t = that.v.get(i)
+            Else
+                t = 0
             End If
+            Select Case op
+                Case bit_wise_operator.and
+                    v.set(i, v.get(i) And t)
+                Case bit_wise_operator.or
+                    v.set(i, v.get(i) Or t)
+                Case bit_wise_operator.xor
+                    v.set(i, v.get(i) Xor t)
+                Case Else
+                    assert(False)
+            End Select
+        Next
+        If op = bit_wise_operator.and OrElse
+           op = bit_wise_operator.xor Then
+            remove_extra_blank()
         End If
         Return Me
     End Function
@@ -160,6 +163,7 @@ Partial Public NotInheritable Class big_uint
         Return True
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub setbit(ByVal pos As UInt64, Optional ByVal value As Boolean = True)
         Dim vi As UInt32 = 0
         Dim bi As Byte = 0
@@ -167,6 +171,7 @@ Partial Public NotInheritable Class big_uint
         v.set(vi, v.get(vi).setbit(bi, value))
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function getbit(ByVal pos As UInt64) As Boolean
         Dim vi As UInt32 = 0
         Dim bi As Byte = 0
@@ -174,6 +179,7 @@ Partial Public NotInheritable Class big_uint
         Return v.get(vi).getbit(bi)
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub setrbit(ByVal pos As UInt64, Optional ByVal value As Boolean = True)
         Dim vi As UInt32 = 0
         Dim bi As Byte = 0
@@ -184,6 +190,7 @@ Partial Public NotInheritable Class big_uint
         End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function getrbit(ByVal pos As UInt64) As Boolean
         Dim vi As UInt32 = 0
         Dim bi As Byte = 0
@@ -191,10 +198,12 @@ Partial Public NotInheritable Class big_uint
         Return v.get(vi).getrbit(bi)
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function even() As Boolean
         Return Not odd()
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function odd() As Boolean
         '0 is even
         Return bit_count() > 0 AndAlso getrbit(0)

service/math/big_uint/big_uint.divide.vb
@@ -9,6 +9,7 @@ Imports osi.root.constants
 
 Public NotInheritable Class big_uint
     'store the result of me / that in me, and remainder will be the remainder
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Private Sub divide(ByVal that As UInt32, ByRef remainder As UInt32, ByRef divide_by_zero As Boolean)
         If that = 0 Then
             divide_by_zero = True
@@ -49,6 +50,7 @@ Public NotInheritable Class big_uint
         assert(remainder < that)
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Private Shared Sub divide_bit(ByVal that As big_uint,
                                   ByVal remainder As big_uint,
                                   ByVal result As big_uint)
@@ -100,6 +102,7 @@ Public NotInheritable Class big_uint
         End While
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Private Shared Sub divide_uint(ByVal that As big_uint,
                                    ByVal remainder As big_uint,
                                    ByVal result As big_uint)
@@ -111,9 +114,6 @@ Public NotInheritable Class big_uint
         that.left_shift(CULng(i) << bit_count_in_uint32_shift)
         While True
             While True
-                If remainder.uint32_size() < that.uint32_size() Then
-                    Exit While
-                End If
                 Dim n As UInt64 = 0
                 Dim d As UInt64 = 0
                 select_significant_divide_fraction(remainder, that, n, d)
@@ -146,6 +146,9 @@ Public NotInheritable Class big_uint
                     result.recursive_add(t32, i)
                 End If
                 remainder.assert_sub(that * t32)
+                If remainder.uint32_size() < that.uint32_size() Then
+                    Exit While
+                End If
             End While
 
             If i = 0 Then
@@ -162,7 +165,9 @@ Public NotInheritable Class big_uint
                                                           ByVal that As big_uint,
                                                           ByRef n As UInt64,
                                                           ByRef d As UInt64)
+#If DEBUG Then
         assert(remainder.uint32_size() >= that.uint32_size())
+#End If
         If remainder.uint32_size() = that.uint32_size() Then
             If remainder.uint32_size() > 1 Then
                 n = remainder.highest_uint64()

commit bb6c8085ee62ceb5255baab9f5890de3578e0a13

vector was broken by a96ca4836938e89954728f9cafd1789f618174f0

root/codegen/adaptive_array/adaptive_array.cs
@@ -139,9 +139,6 @@ public static class Program
         w(strcat(
 "    Implements ICloneable, ICloneable(Of " + class_name(a) + "), IComparable(Of " + class_name(a) + "), IComparable",
 "",
-"    Private Const size_limitation As UInt32 = (max_uint32 >> 1)",
-"    'Private Shared ReadOnly default_value As " + a.type + " = Nothing",
-"",
 "    Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32",
 "        assert(n <= max_array_size)",
 "        If n = max_array_size Then",
@@ -202,7 +199,7 @@ public static class Program
 "",
 "    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Function max_size() As UInt32",
-"        Return size_limitation",
+"        Return max_array_size",
 "    End Function",
 "",
 "    <MethodImpl(method_impl_options.aggressive_inlining)>",
@@ -252,58 +249,51 @@ public static class Program
 "        Return d(CInt(size() - uint32_1))",
 "    End Function",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Sub clear()",
 "        s = uint32_0",
-"#If 0 Then",
-"        If size() > uint32_0 Then",
-"            memclr(d, uint32_0, s)",
-"            s = uint32_0",
-"        End If",
-"#End If",
 "    End Sub",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Sub push_back(ByVal v As " + a.type + ")",
 "        reserve(size() + uint32_1)",
 "        d(CInt(size())) = v",
 "        s += uint32_1",
 "    End Sub",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Sub pop_back()",
 "        s -= uint32_1",
-"        'd(CInt(size())) = default_value",
 "    End Sub",
 "",
 "    Public Sub reserve(ByVal n As UInt32)",
-"        If capacity() < n Then",
-"            Dim ec As UInt32 = 0",
-"            ec = expected_capacity(n)",
-"            assert(ec >= uint32_1)",
-"            If empty() Then",
-"                ReDim d(CInt(ec - uint32_1))",
-"            Else",
-"                ReDim Preserve d(CInt(ec - uint32_1))",
-"            End If",
+"        If capacity() >= n Then",
+"            Return",
+"        End If",
+"        Dim ec As UInt32 = 0",
+"        ec = expected_capacity(n)",
+"        assert(ec >= uint32_1)",
+"        If empty() Then",
+"            ReDim d(CInt(ec - uint32_1))",
+"        Else",
+"            ReDim Preserve d(CInt(ec - uint32_1))",
 "        End If",
 "    End Sub",
 "",
 "    Public Sub resize(ByVal n As UInt32)",
-"        If capacity() < n Then",
-"            reserve(n)",
-"        'ElseIf size() > n Then",
-"            'memclr(d, n, size() - n)",
+"        reserve(n)",
+"        If size() < n Then",
+"            memclr(d, size(), n - size())",
 "        End If",
 "        s = n",
 "    End Sub",
 "",
 "    Public Sub resize(ByVal n As UInt32, ByVal v As " + a.type + ")",
-"        Dim os As UInt32 = 0",
-"        os = size()",
-"        If n > os Then",
-"            resize(n)",
-"            memset(d, os, n - os, v)",
-"        Else",
-"            resize(n)",
+"        reserve(n)",
+"        If size() < n Then",
+"            memset(d, size(), n - size(), v)",
 "        End If",
+"        s = n",
 "    End Sub",
 "",
 "    Public Sub shrink_to_fit()",
@@ -315,6 +305,7 @@ public static class Program
 "        End If",
 "    End Sub",
 "",
+"    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Function Clone() As Object Implements ICloneable.Clone",
 "        Return CloneT()",
 "    End Function",

root/formation/vector.adaptive_array.vb
@@ -18,9 +18,6 @@ Partial Public Class vector(Of T)
 Private Class adaptive_array_t
     Implements ICloneable, ICloneable(Of adaptive_array_t), IComparable(Of adaptive_array_t), IComparable
 
-    Private Const size_limitation As UInt32 = (max_uint32 >> 1)
-    'Private Shared ReadOnly default_value As T = Nothing
-
     Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32
         assert(n <= max_array_size)
         If n = max_array_size Then
@@ -81,7 +78,7 @@ Private Class adaptive_array_t
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function max_size() As UInt32
-        Return size_limitation
+        Return max_array_size
     End Function
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
@@ -131,58 +128,51 @@ Private Class adaptive_array_t
         Return d(CInt(size() - uint32_1))
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub clear()
         s = uint32_0
-#If 0 Then
-        If size() > uint32_0 Then
-            memclr(d, uint32_0, s)
-            s = uint32_0
-        End If
-#End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub push_back(ByVal v As T)
         reserve(size() + uint32_1)
         d(CInt(size())) = v
         s += uint32_1
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub pop_back()
         s -= uint32_1
-        'd(CInt(size())) = default_value
     End Sub
 
     Public Sub reserve(ByVal n As UInt32)
-        If capacity() < n Then
-            Dim ec As UInt32 = 0
-            ec = expected_capacity(n)
-            assert(ec >= uint32_1)
-            If empty() Then
-                ReDim d(CInt(ec - uint32_1))
-            Else
-                ReDim Preserve d(CInt(ec - uint32_1))
-            End If
+        If capacity() >= n Then
+            Return
+        End If
+        Dim ec As UInt32 = 0
+        ec = expected_capacity(n)
+        assert(ec >= uint32_1)
+        If empty() Then
+            ReDim d(CInt(ec - uint32_1))
+        Else
+            ReDim Preserve d(CInt(ec - uint32_1))
         End If
     End Sub
 
     Public Sub resize(ByVal n As UInt32)
-        If capacity() < n Then
-            reserve(n)
-        'ElseIf size() > n Then
-            'memclr(d, n, size() - n)
+        reserve(n)
+        If size() < n Then
+            memclr(d, size(), n - size())
         End If
         s = n
     End Sub
 
     Public Sub resize(ByVal n As UInt32, ByVal v As T)
-        Dim os As UInt32 = 0
-        os = size()
-        If n > os Then
-            resize(n)
-            memset(d, os, n - os, v)
-        Else
-            resize(n)
+        reserve(n)
+        If size() < n Then
+            memset(d, size(), n - size(), v)
         End If
+        s = n
     End Sub
 
     Public Sub shrink_to_fit()
@@ -194,6 +184,7 @@ Private Class adaptive_array_t
         End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function Clone() As Object Implements ICloneable.Clone
         Return CloneT()
     End Function

service/math/big_uint/big_uint.adaptive_array.vb
@@ -18,9 +18,6 @@ Partial Public Class big_uint
 Private Class adaptive_array_uint32
     Implements ICloneable, ICloneable(Of adaptive_array_uint32), IComparable(Of adaptive_array_uint32), IComparable
 
-    Private Const size_limitation As UInt32 = (max_uint32 >> 1)
-    'Private Shared ReadOnly default_value As UInt32 = Nothing
-
     Private Shared Function expected_capacity(ByVal n As UInt32) As UInt32
         assert(n <= max_array_size)
         If n = max_array_size Then
@@ -81,7 +78,7 @@ Private Class adaptive_array_uint32
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function max_size() As UInt32
-        Return size_limitation
+        Return max_array_size
     End Function
 
     <MethodImpl(method_impl_options.aggressive_inlining)>
@@ -131,58 +128,51 @@ Private Class adaptive_array_uint32
         Return d(CInt(size() - uint32_1))
     End Function
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub clear()
         s = uint32_0
-#If 0 Then
-        If size() > uint32_0 Then
-            memclr(d, uint32_0, s)
-            s = uint32_0
-        End If
-#End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub push_back(ByVal v As UInt32)
         reserve(size() + uint32_1)
         d(CInt(size())) = v
         s += uint32_1
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Sub pop_back()
         s -= uint32_1
-        'd(CInt(size())) = default_value
     End Sub
 
     Public Sub reserve(ByVal n As UInt32)
-        If capacity() < n Then
-            Dim ec As UInt32 = 0
-            ec = expected_capacity(n)
-            assert(ec >= uint32_1)
-            If empty() Then
-                ReDim d(CInt(ec - uint32_1))
-            Else
-                ReDim Preserve d(CInt(ec - uint32_1))
-            End If
+        If capacity() >= n Then
+            Return
+        End If
+        Dim ec As UInt32 = 0
+        ec = expected_capacity(n)
+        assert(ec >= uint32_1)
+        If empty() Then
+            ReDim d(CInt(ec - uint32_1))
+        Else
+            ReDim Preserve d(CInt(ec - uint32_1))
         End If
     End Sub
 
     Public Sub resize(ByVal n As UInt32)
-        If capacity() < n Then
-            reserve(n)
-        'ElseIf size() > n Then
-            'memclr(d, n, size() - n)
+        reserve(n)
+        If size() < n Then
+            memclr(d, size(), n - size())
         End If
         s = n
     End Sub
 
     Public Sub resize(ByVal n As UInt32, ByVal v As UInt32)
-        Dim os As UInt32 = 0
-        os = size()
-        If n > os Then
-            resize(n)
-            memset(d, os, n - os, v)
-        Else
-            resize(n)
+        reserve(n)
+        If size() < n Then
+            memset(d, size(), n - size(), v)
         End If
+        s = n
     End Sub
 
     Public Sub shrink_to_fit()
@@ -194,6 +184,7 @@ Private Class adaptive_array_uint32
         End If
     End Sub
 
+    <MethodImpl(method_impl_options.aggressive_inlining)>
     Public Function Clone() As Object Implements ICloneable.Clone
         Return CloneT()
     End Function