74054bcb797688b8e84648f196c9808973460435
Hzj_jie <hzj_jie@hotmail.com>
2020-04-05 04:31:38 -0700

    Improve big_udec_match_test to support longer input and performance

--- service/tests/math/big_udec/big_udec_match_test.vb
+++ service/tests/math/big_udec/big_udec_match_test.vb
@@ -13,25 +13,31 @@ Public NotInheritable Class big_udec_match_test
     <test>
     Private Shared Sub pi_fractional_str()
         Dim s As String = Nothing
-        s = big_udec.parse_fraction(Console.ReadLine()).as_str().with_upure_length(102400)
-        For i As Int32 = 1 To s.Length()
-            If Not constants.pi_1m().StartsWith(s.Substring(0, i)) OrElse i = s.Length() Then
+        s = big_udec.parse_fraction(IO.File.ReadAllText("input.txt")).as_str().with_upure_length(102400)
+        Dim till As Int32 = 0
+        till = min(s.Length(), constants.pi_1m().Length())
+        For i As Int32 = 0 To till - 1
+            If constants.pi_1m()(i) <> s(i) Then
                 Console.WriteLine(strcat("Match first ", i, " digits"))
                 Return
             End If
         Next
+        Console.WriteLine(strcat("Match first ", till, " digits"))
     End Sub
 
     <test>
     Private Shared Sub e_fractional_str()
         Dim s As String = Nothing
-        s = big_udec.parse_fraction(Console.ReadLine()).as_str().with_upure_length(102400)
-        For i As Int32 = 1 To s.Length()
-            If Not constants.e_1m().StartsWith(s.Substring(0, i)) OrElse i = s.Length() Then
+        s = big_udec.parse_fraction(IO.File.ReadAllText("input.txt")).as_str().with_upure_length(1024000)
+        Dim till As Int32 = 0
+        till = min(s.Length(), constants.e_1m().Length())
+        For i As Int32 = 0 To till - 1
+            If constants.e_1m()(i) <> s(i) Then
                 Console.WriteLine(strcat("Match first ", i, " digits"))
                 Return
             End If
         Next
+        Console.WriteLine(strcat("Match first ", till, " digits"))
     End Sub
 
     Private Sub New()
