f073b0b7d65f12e469e2ad89c9d1fc5095613356
Hzj_jie <hzj_jie@hotmail.com>
2020-04-05 15:43:13 -0700

    do not use do_not_parse_as_vb

--- service/tests/resource/run.cmd
+++ service/tests/resource/run.cmd
@@ -2,7 +2,7 @@
 setlocal
 cd /d "%~dp0"
 path %PATH%;..\..\resource\gen;
-gen _data data data > data.do_not_parse_as_vb
-zipgen _zipdata zipdata data > zipdata.do_not_parse_as_vb
+gen _data data data > data.vb
+zipgen _zipdata zipdata data > zipdata.vb
 endlocal
 
