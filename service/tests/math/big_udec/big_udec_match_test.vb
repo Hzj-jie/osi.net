f029a7e3572b2988bad3702d0baba201f9d4c895
Hzj_jie <hzj_jie@hotmail.com>
2020-04-05 05:43:01 -0700

    Use different inputs for big_udec_match_test

--- service/tests/math/big_udec/big_udec_match_test.vb
+++ service/tests/math/big_udec/big_udec_match_test.vb
@@ -4,40 +4,45 @@ Option Infer Off
 Option Strict On
 
 Imports osi.root.connector
+Imports osi.root.constants
 Imports osi.root.utt.attributes
 Imports osi.service.math
 
 <command_line_specified>
 <test>
 Public NotInheritable Class big_udec_match_test
+    Private Shared Sub match_digits(ByVal i As Int32)
+        raise_error(error_type.warning, "Match first ", i, " digits")
+    End Sub
+
     <test>
     Private Shared Sub pi_fractional_str()
         Dim s As String = Nothing
-        s = big_udec.parse_fraction(IO.File.ReadAllText("input.txt")).as_str().with_upure_length(102400)
+        s = big_udec.parse_fraction(IO.File.ReadAllText("pi.txt")).as_str().with_upure_length(102400)
         Dim till As Int32 = 0
         till = min(s.Length(), constants.pi_1m().Length())
         For i As Int32 = 0 To till - 1
             If constants.pi_1m()(i) <> s(i) Then
-                Console.WriteLine(strcat("Match first ", i, " digits"))
+                match_digits(i)
                 Return
             End If
         Next
-        Console.WriteLine(strcat("Match first ", till, " digits"))
+        match_digits(till)
     End Sub
 
     <test>
     Private Shared Sub e_fractional_str()
         Dim s As String = Nothing
-        s = big_udec.parse_fraction(IO.File.ReadAllText("input.txt")).as_str().with_upure_length(1024000)
+        s = big_udec.parse_fraction(IO.File.ReadAllText("e.txt")).as_str().with_upure_length(1024000)
         Dim till As Int32 = 0
         till = min(s.Length(), constants.e_1m().Length())
         For i As Int32 = 0 To till - 1
             If constants.e_1m()(i) <> s(i) Then
-                Console.WriteLine(strcat("Match first ", i, " digits"))
+                match_digits(i)
                 Return
             End If
         Next
-        Console.WriteLine(strcat("Match first ", till, " digits"))
+        match_digits(till)
     End Sub
 
     Private Sub New()
