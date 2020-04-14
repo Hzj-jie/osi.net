3367978150903129f4e31b220247e5804109a063
Hzj_jie <hzj_jie@hotmail.com>
2020-04-13 19:36:54 -0700

    Remove e_2m and pi_10m

--- service/tests/math/big_udec/big_udec_match_test.vb
+++ service/tests/math/big_udec/big_udec_match_test.vb
@@ -20,19 +20,27 @@ Public NotInheritable Class big_udec_match_test
         raise_error(error_type.warning, "Match more than first ", i, " digits")
     End Sub
 
+    Private Shared Function pi_str() As String
+        Return constants.pi_1m()
+    End Function
+
+    Private Shared Function e_str() As String
+        Return constants.e_1m()
+    End Function
+
     <test>
     Private Shared Sub pi_fractional_str()
         Dim s As String = Nothing
         s = big_udec.parse_fraction(IO.File.ReadAllText("pi.txt")).
                 as_str().
-                with_upure_length(constants.pi_1m().strlen())
+                with_upure_length(pi_str().strlen())
         For i As Int32 = 0 To s.Length() - 1
-            If constants.pi_1m()(i) <> s(i) Then
+            If pi_str()(i) <> s(i) Then
                 match_digits(i)
                 Return
             End If
         Next
-        match_more_digits(s.Length())
+        match_more_digits(pi_str().Length())
     End Sub
 
     <test>
@@ -40,14 +48,14 @@ Public NotInheritable Class big_udec_match_test
         Dim s As String = Nothing
         s = big_udec.parse_fraction(IO.File.ReadAllText("e.txt")).
                 as_str().
-                with_upure_length(constants.e_2m().strlen())
+                with_upure_length(e_str().strlen())
         For i As Int32 = 0 To s.Length() - 1
-            If constants.e_2m()(i) <> s(i) Then
+            If e_str()(i) <> s(i) Then
                 match_digits(i)
                 Return
             End If
         Next
-        match_more_digits(constants.e_2m().Length())
+        match_more_digits(e_str().Length())
     End Sub
 
     Private Sub New()
