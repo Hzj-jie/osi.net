efd48eb4e4f929467f6cce8befde35df6776453b
Hzj_jie <hzj_jie@hotmail.com>
2020-04-22 13:54:54 -0700

    Add big_udec_dec_str_perf

---
+++ service/tests/math/big_udec/big_udec_dec_str_perf.vb
@@ -0,0 +1,19 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.utt.attributes
+Imports osi.service.math
+
+<test>
+Public NotInheritable Class big_udec_dec_str_perf
+    <repeat(2)>
+    <test>
+    Private Shared Sub run()
+        big_udec.random(100000).as_str().with_upure_length(100000).ToString()
+    End Sub
+
+    Private Sub New()
+    End Sub
+End Class
