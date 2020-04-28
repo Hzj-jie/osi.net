5bb50554f3da42171f0c877d040a9a412d7822be
Hzj_jie <hzj_jie@hotmail.com>
2020-04-28 16:55:34 -0700

    enable b2style_test.calculate_pi_bbp

--- service/tests/compiler/b2style/b2style_test.vb
+++ service/tests/compiler/b2style/b2style_test.vb
@@ -208,6 +208,19 @@ Public NotInheritable Class b2style_test
         assertion.equal(io.output(), "40040012")
     End Sub
 
+    <test>
+    Private Shared Sub calculate_pi_bbp()
+        Dim io As console_io.test_wrapper = Nothing
+        io = New console_io.test_wrapper()
+        Dim e As executor = Nothing
+        assertion.is_true(b2style.with_functions(New interrupts(+io)).
+                          parse(_b2style_test_data.calculate_pi_bbp.as_text(), e))
+        assertion.is_not_null(e)
+        e.execute()
+        assertion.equal(io.output(), "3.1415926535897932384626433832795028841971693993751058209749445923")
+        '                             3.1415926535897932384626433832795028841971693993751058209749445923
+    End Sub
+
     Private Sub New()
     End Sub
 End Class
