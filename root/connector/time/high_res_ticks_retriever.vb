32f0032307af147fb06ea1a1a72c890c647fb3fc
Hzj_jie <hzj_jie@hotmail.com>
2020-05-03 00:14:57 -0700

    nowadays coding style

--- root/connector/time/high_res_ticks_retriever.vb
+++ root/connector/time/high_res_ticks_retriever.vb
@@ -1,10 +1,14 @@
 ﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports System.DateTime
 Imports System.Diagnostics
 Imports System.Threading
 Imports osi.root.constants
 
-Public Class high_res_ticks_retriever
+Public NotInheritable Class high_res_ticks_retriever
     Private Const revise_interval_ticks As Int64 = CLng(15) * minute_second * second_milli * milli_tick
     Private Shared ReadOnly perf_freq As Double
     Private Shared distance As Int64
@@ -32,39 +36,37 @@ Public Class high_res_ticks_retriever
     End Sub
 
     Public Shared Function high_res_ticks() As Int64
-        If Stopwatch.IsHighResolution Then
-            Dim c As Int64 = 0
-            c = qpc()
-            'looks like the QueryPerformanceCounter is not consistent with Now().Ticks()
-            If distance = 0 OrElse
-               c - last_revise_ticks >= revise_interval_ticks Then
-                For i As Int32 = 0 To 1
-                    Dim this As Int64 = 0
-                    this = (((Now().Ticks() - qpc()) -
-                             (qpc() - Now().Ticks())) >> 1)
-                    assert(this > 0)
-                    If i = 0 OrElse
-                       this < distance Then
-                        'for 32 bit processors
-                        Interlocked.Exchange(distance, this)
-                    End If
-                Next
-                c = qpc()
-                last_revise_ticks = c
-            End If
-            assert(distance > 0)
-            Return c + distance
-        Else
+        If Not Stopwatch.IsHighResolution Then
             Return Now().Ticks()
         End If
+        Dim c As Int64 = 0
+        c = qpc()
+        'looks like the QueryPerformanceCounter is not consistent with Now().Ticks()
+        If distance = 0 OrElse
+               c - last_revise_ticks >= revise_interval_ticks Then
+            For i As Int32 = 0 To 1
+                Dim this As Int64 = 0
+                this = (((Now().Ticks() - qpc()) -
+                             (qpc() - Now().Ticks())) >> 1)
+                assert(this > 0)
+                If i = 0 OrElse
+                       this < distance Then
+                    'for 32 bit processors
+                    Interlocked.Exchange(distance, this)
+                End If
+            Next
+            c = qpc()
+            last_revise_ticks = c
+        End If
+        assert(distance > 0)
+        Return c + distance
     End Function
 
     Public Shared Function query_performance_count() As Int64
         If Stopwatch.IsHighResolution Then
             Return qpc()
-        Else
-            Return Now().Ticks()
         End If
+        Return Now().Ticks()
     End Function
 
     Private Shared Function qpc() As Int64
