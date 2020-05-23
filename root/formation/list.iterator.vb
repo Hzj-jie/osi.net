Hzj-jie/osi.net

commit 7da6bf63b831355c18ce9833b13baad4a04986f6

unordered_map perf improvement

root/formation/list.iterator.vb
@@ -82,8 +82,7 @@ Partial Public Class list(Of T)
             Return p Is Nothing
 #Else
             Return p Is Nothing AndAlso
-                   (Not isdebugmode() OrElse
-                    assert(object_compare(Me, [end]) = 0))
+                   assert(object_compare(Me, [end]) = 0)
 #End If
         End Function
 #End If

root/codegen/adaptive_array/adaptive_array.cs
@@ -204,17 +204,16 @@ public static class Program
 "        Return d",
 "    End Function",
 "",
-"' Property access is expensive.",
-"#If 0 Then",
 "    Default Public Property at(ByVal p As UInt32) As " + a.type,
+"        <MethodImpl(method_impl_options.aggressive_inlining)>",
 "        Get",
 "            Return [get](p)",
 "        End Get",
+"        <MethodImpl(method_impl_options.aggressive_inlining)>",
 "        Set(ByVal value As " + a.type + ")",
 "            [set](p, value)",
 "        End Set",
 "    End Property",
-"#End If",
 "",
 "    <MethodImpl(method_impl_options.aggressive_inlining)>",
 "    Public Function [get](ByVal p As UInt32) As " + a.type,

root/connector/template/to_uint32.vb
@@ -20,9 +20,8 @@ Public NotInheritable Class fast_to_uint32(Of T)
             f = Function(i As T) As UInt32
                     If i Is Nothing Then
                         Return 0
-                    Else
-                        Return int32_uint32(i.GetHashCode())
                     End If
+                    Return int32_uint32(i.GetHashCode())
                 End Function
         End If
     End Sub

root/formation/array.vb
@@ -69,7 +69,9 @@ Public Class array(Of T, SIZE As _int64)
         End Get
         <MethodImpl(method_impl_options.aggressive_inlining)>
         Set(ByVal v As T)
+#If DEBUG Then
             assert(i < size())
+#End If
             Me.v(CInt(i)) = v
         End Set
     End Property
@@ -122,7 +124,9 @@ Public Class array(Of T)
         End Get
         <MethodImpl(method_impl_options.aggressive_inlining)>
         Set(ByVal v As T)
+#If DEBUG Then
             assert(i < size())
+#End If
             Me.v(CInt(i)) = v
         End Set
     End Property

root/formation/binary_tree/bst.vb
@@ -65,9 +65,7 @@ Public Class bst(Of T)
             Dim c As Int32 = 0
             c = n.compare(v)
             If c < 0 Then
-                assert(Not isdebugmode() OrElse
-                             l Is Nothing OrElse
-                             n.compare(l) > 0)
+                debug_assert(l Is Nothing OrElse n.compare(l) > 0)
                 l = n
                 If n.has_right_child() Then
                     n = n.right_child()
@@ -112,9 +110,7 @@ Public Class bst(Of T)
                 End If
             Else
                 assert(c > 0)
-                assert(Not isdebugmode() OrElse
-                       l Is Nothing OrElse
-                       n.compare(l) < 0)
+                debug_assert(l Is Nothing OrElse n.compare(l) < 0)
                 l = n
                 If n.has_left_child() Then
                     n = n.left_child()

root/formation/binary_tree/bt.iterator.vb
@@ -82,8 +82,7 @@ Partial Public Class bt(OF T)
             Return p Is Nothing
 #Else
             Return p Is Nothing AndAlso
-                   (Not isdebugmode() OrElse
-                    assert(object_compare(Me, [end]) = 0))
+                   assert(object_compare(Me, [end]) = 0)
 #End If
         End Function
 #End If

root/formation/codegen/static_iterator.vbp
@@ -40,8 +40,7 @@
             Return p Is Nothing
 #Else
             Return p Is Nothing AndAlso
-                   (Not isdebugmode() OrElse
-                    assert(object_compare(Me, [end]) = 0))
+                   assert(object_compare(Me, [end]) = 0)
 #End If
         End Function
 #End If

commit 23cc62ca5e3f462475587074eea9c965bba2b95e

list.iterator was broken by 7da6bf6

root/formation/codegen/list.iterator.vbp
@@ -11,6 +11,7 @@ Partial Public Class list(Of T)
     Public Structure iterator
 ##DEFINE TYPE node
 ##DEFINE DEFINE_OPERATOR_PLUS False
+##DEFINE TYPE_IS_STRUCT True
 ##INCLUDE random_access_iterator.vbp
 
         Private Function move_prev(ByVal s As UInt32) As iterator

root/formation/list.iterator.vb
@@ -64,7 +64,7 @@ Partial Public Class list(Of T)
         <MethodImpl(method_impl_options.aggressive_inlining)>
         Private Sub New(ByVal that As node)
 #End If
-#If Not False Then
+#If Not True Then
             assert(Not that Is Nothing)
 #End If
             p = that
@@ -78,7 +78,7 @@ Partial Public Class list(Of T)
 #Else
         <MethodImpl(method_impl_options.aggressive_inlining)>
         Public Function is_end() As Boolean
-#If False Then
+#If True Then
             Return p Is Nothing
 #Else
             Return p Is Nothing AndAlso