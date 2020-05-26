c60d95ecfb5ad7426cab60a15a9a3bfb3dd951e0
Zijie He <hzj_jie@hotmail.com>
2020-05-26 01:31:28 -0700

    Extension -> Extension()

--- root/formation/tuple2.ext.vb
+++ root/formation/tuple2.ext.vb
@@ -6,11 +6,11 @@ Option Strict On
 Imports System.Runtime.CompilerServices
 
 Public Module _tuple2
-    <Extension> Public Function first(Of T1, T2)(ByVal this As tuple(Of T1, T2)) As T1
+    <Extension()> Public Function first(Of T1, T2)(ByVal this As tuple(Of T1, T2)) As T1
         Return this._1()
     End Function
 
-    <Extension> Public Function second(Of T1, T2)(ByVal this As tuple(Of T1, T2)) As T2
+    <Extension()> Public Function second(Of T1, T2)(ByVal this As tuple(Of T1, T2)) As T2
         Return this._2()
     End Function
 End Module
