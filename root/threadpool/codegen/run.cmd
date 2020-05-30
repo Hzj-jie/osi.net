c1d040301c29846e68cfada292c55541adee0035
Hzj_jie <hzj_jie@hotmail.com>
2020-05-30 01:24:20 -0700

    qless_runner and qless_threadpool2

--- root/threadpool/codegen/run.cmd
+++ root/threadpool/codegen/run.cmd
@@ -7,8 +7,10 @@ precompile qless_threadpool.vbp > qless_threadpool.vb
 precompile slimheapless_threadpool.vbp > slimheapless_threadpool.vb
 precompile slimqless2_runner.vbp > slimqless2_runner.vb
 precompile slimheapless_runner.vbp > slimheapless_runner.vb
+precompile qless_runner.vbp > qless_runner.vb
 precompile slimqless2_threadpool2.vbp > slimqless2_threadpool2.vb
 precompile slimheapless_threadpool2.vbp > slimheapless_threadpool2.vb
+precompile qless_threadpool2.vbp > qless_threadpool2.vb
 precompile slimqless2_runner_synchronize_invoke.vbp > slimqless2_runner_synchronize_invoke.vb
 precompile slimheapless_runner_synchronize_invoke.vbp > slimheapless_runner_synchronize_invoke.vb
 
