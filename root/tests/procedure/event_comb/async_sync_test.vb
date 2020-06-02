14008b1620328fd784cf57500b8e06b8267d39c3
Hzj_jie <hzj_jie@hotmail.com>
2020-06-02 11:06:14 -0700

    async_sync_test should be slightly loosen when managed_threadpool is used

--- root/tests/procedure/event_comb/async_sync_test.vb
+++ root/tests/procedure/event_comb/async_sync_test.vb
@@ -10,8 +10,9 @@ Imports osi.root.lock
 Imports osi.root.procedure
 Imports osi.root.threadpool
 Imports osi.root.utt
+Imports tp = osi.root.threadpool.threadpool
 
-Public Class async_sync_test
+Public NotInheritable Class async_sync_test
     Inherits [case]
 
     Private Shared Function case1() As Boolean
@@ -39,15 +40,19 @@ Public Class async_sync_test
                                             Return goto_end()
                                         End Function))
         Next
-        assertion.is_true(w.WaitOne(CInt(seconds_to_milliseconds(10))))
+        If tp.in_restricted_threadpool_thread() Then
+            assertion.is_true(w.WaitOne(CInt(seconds_to_milliseconds(10))))
+        Else
+            assertion.is_true(w.WaitOne(CInt(seconds_to_milliseconds(60))))
+        End If
         Return True
     End Function
 
     Private Shared Function async_sync_with_huge_timeout() As Boolean
         assertion.is_true(async_sync(New event_comb(Function() As Boolean
-                                                  Return goto_end()
-                                              End Function),
-                               max_int64))
+                                                        Return goto_end()
+                                                    End Function),
+                                     max_int64))
         Return True
     End Function
 
