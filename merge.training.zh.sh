d015719493aadf20b3ff5d27bd22c6fcd656c892
Hzj_jie <hzj_jie@hotmail.com>
2020-06-17 23:56:15 -0700

    merge.training.zh.sh

---
+++ merge.training.zh.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm /cygdrive/c/temp/training.zh.txt2
+
+function process_folder {
+  pushd "$1"
+  find . -name '*.txt' | xargs -d '\n' cat >> /cygdrive/c/temp/training.zh.txt2
+  popd
+}
+
+process_folder /cygdrive/c/Users/Hzj_jie/Documents/git/training-data/zh/People\'s\ Newspaper/
+process_folder /cygdrive/f/My\ Books/pending/disk1/
+process_folder /cygdrive/f/My\ Books/pending/disk4/
+process_folder /cygdrive/c/Users/Hzj_jie/Documents/git/training-data/zh/CCTV\ News/2016-2019/
