1b0239d5ee8c05ed54563c2e2582b52c1e0ebb16
Hzj_jie <hzj_jie@hotmail.com>
2021-01-15 17:54:38 -0800

    stream tests

---
+++ root/tests/formation/stream/stream_percentile_test.vb
@@ -0,0 +1,21 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.formation
+Imports osi.root.utt
+Imports osi.root.utt.attributes
+
+<test>
+Public NotInheritable Class stream_percentile_test
+    <test>
+    Private Shared Sub percentile_test()
+        assertion.equal(streams.range(0, 100).aggregate(stream(Of Int32).aggregators.percentile(0.9)), 90)
+        assertion.equal(streams.range(0, 100).aggregate(stream(Of Int32).aggregators.percentile(0)), 0)
+        assertion.equal(streams.range(0, 100).aggregate(stream(Of Int32).aggregators.percentile(1)), 99)
+    End Sub
+
+    Private Sub New()
+    End Sub
+End Class
