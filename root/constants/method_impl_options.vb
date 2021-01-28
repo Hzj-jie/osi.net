ec9c13af1f47a4764bbe76ec7263a9b701e928c9
Hzj_jie <hzj_jie@hotmail.com>
2021-01-28 14:30:20 -0800

    do not allow to inline functions

--- root/constants/method_impl_options.vb
+++ root/constants/method_impl_options.vb
@@ -9,8 +9,9 @@ Public NotInheritable Class method_impl_options
 #If DEBUG Then
     Public Const aggressive_inlining As MethodImplOptions = zero
 #Else
+    Public Const aggressive_inlining As MethodImplOptions = MethodImplOptions.NoInlining
     'aggressive-inline + aggressive-optimization
-    Public Const aggressive_inlining As MethodImplOptions = DirectCast(256 Or 512, MethodImplOptions)
+    'Public Const aggressive_inlining As MethodImplOptions = DirectCast(256 Or 512, MethodImplOptions)
 #End If
 
     Public Const zero As MethodImplOptions = DirectCast(0, MethodImplOptions)
