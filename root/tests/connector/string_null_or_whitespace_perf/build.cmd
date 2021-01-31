b0de111d0823b07e25c4afd707103420aefe6606
Hzj_jie <hzj_jie@hotmail.com>
2021-01-31 00:06:43 -0800

    Revert "Avoid using link in string_null_or_whitespace_perf.exe"
    This reverts commit 5af6412885e70333ae7793e676b15626f1477ce7.

--- root/tests/connector/string_null_or_whitespace_perf/build.cmd
+++ root/tests/connector/string_null_or_whitespace_perf/build.cmd
@@ -4,7 +4,8 @@ setlocal
 call ..\..\..\..\init_vs_env.cmd
 
 vbc /target:module /out:tmp.mod /define:NO_REFERENCE /rootnamespace:osi.root.connector ..\..\..\connector\string_utils\char_detection.vb
-csc /target:exe /addmodule:tmp.mod /main:string_null_or_whitespace_perf /out:string_null_or_whitespace_perf.exe /SUBSYSTEMVERSION:5.0 string_null_or_whitespace_perf.cs
+csc /target:module /addmodule:tmp.mod /out:tmp2.mod string_null_or_whitespace_perf.cs
+link /entry:string_null_or_whitespace_perf.Main /out:string_null_or_whitespace_perf.exe /LTCG /SUBSYSTEM:CONSOLE tmp.mod tmp2.mod
 del tmp.mod
 del tmp2.mod
 
