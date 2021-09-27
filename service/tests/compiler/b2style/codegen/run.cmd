40a9d9f95b5aa54defd559354439dd95dd302d25
Hzj_jie <hzj_jie@hotmail.com>
2021-09-27 14:28:42 -0700

    Rename b2style test names from .txt into .b2

--- service/tests/compiler/b2style/codegen/run.cmd
+++ service/tests/compiler/b2style/codegen/run.cmd
@@ -1,7 +1,7 @@
 
 setlocal
 path %PATH%;..\..\..\..\resource\gen;
-gen _b2style_test_data case1 case1.txt case2 case2.txt bool_and_bool bool_and_bool.txt str_unescape str_unescape.txt _1_to_100 _1_to_100.txt self_add self_add.txt biguint biguint.txt negative_int negative_int.txt another_1_to_100 another_1_to_100.txt loaded_method loaded_method.txt ufloat_std_out ufloat_std_out.txt ufloat_operators ufloat_operators.txt while_1_to_100 while_1_to_100.txt while_0_to_1 while_0_to_1.txt pi_integral_0_1 pi_integral_0_1.txt calculate_pi_bbp calculate_pi_bbp.txt shift shift.txt order_of_operators order_of_operators.txt include include.txt include2 include2.txt ifndef ifndef.txt namespaces namespaces.txt multiline_string multiline_string.txt comments comments.txt > b2style_test_data.vb
+gen _b2style_test_data case1 case1.b2 case2 case2.b2 bool_and_bool bool_and_bool.b2 str_unescape str_unescape.b2 _1_to_100 _1_to_100.b2 self_add self_add.b2 biguint biguint.b2 negative_int negative_int.b2 another_1_to_100 another_1_to_100.b2 loaded_method loaded_method.b2 ufloat_std_out ufloat_std_out.b2 ufloat_operators ufloat_operators.b2 while_1_to_100 while_1_to_100.b2 while_0_to_1 while_0_to_1.b2 pi_integral_0_1 pi_integral_0_1.b2 calculate_pi_bbp calculate_pi_bbp.b2 shift shift.b2 order_of_operators order_of_operators.b2 include include.b2 include2 include2.b2 ifndef ifndef.b2 namespaces namespaces.b2 multiline_string multiline_string.b2 comments comments.b2 > b2style_test_data.vb
 move /Y b2style_test_data.vb ..\
 endlocal
 
