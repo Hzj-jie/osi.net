a2db08678e72e24f1c96c39c71e5d7983220c057
Hzj_jie <hzj_jie@hotmail.com>
2021-12-09 00:32:10 -0800

    Improve performance of selfhealth

--- root/utt/selfhealth/selfhealth.vb
+++ root/utt/selfhealth/selfhealth.vb
@@ -13,33 +13,18 @@ Public Module selfhealth
         Return self_health
     End Function
 
-    Private Function set_self_health(ByVal b As Boolean) As Boolean
-        Return Not host.cases.stream().
-                              filter(Function(ByVal x As case_info) As Boolean
-                                         Return TypeOf x.case Is failure_case
-                                     End Function).
-                              find_first().
-                              empty()
-    End Function
-
     Public Function run() As Boolean
-        Dim selectors As vector(Of String) = Nothing
-        selectors = New vector(Of String)()
+        Dim selectors As New vector(Of String)()
         selectors.push_back(GetType(failure_case).AssemblyQualifiedName())
 
-        Dim r As Int32 = 0
-        r = rnd_int(1, 10)
+        Dim r As Int32 = rnd_int(1, 10)
         For i As Int32 = 0 To r - 1
-            If Not set_self_health(True) Then
-                Return False
-            End If
             self_health = True
             host.run(selectors)
             self_health = False
 
             ' TODO: Test expectation.
-            If (assertion.equal(assertion.failure_count(), failure_case.expected_failure_count) AndAlso
-                set_self_health(False)) OrElse
+            If assertion.equal(assertion.failure_count(), failure_case.expected_failure_count) OrElse
                envs.utt_no_assert Then
                 assertion.clear_failure()
             Else
