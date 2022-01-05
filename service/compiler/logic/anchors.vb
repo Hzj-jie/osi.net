9cd67ab46fef475a7f6bc0f59cbef805b3856e03
Hzj_jie <hzj_jie@hotmail.com>
2022-01-05 15:55:07 -0800

    Provide provide_callee_pos_lookup compiler flag in anchors

--- service/compiler/logic/anchors.vb
+++ service/compiler/logic/anchors.vb
@@ -3,6 +3,8 @@ Option Explicit On
 Option Infer Off
 Option Strict On
 
+#Const provide_callee_pos_lookup = False
+
 Imports osi.root.connector
 Imports osi.root.formation
 
@@ -30,11 +32,15 @@ Namespace logic
         Public Shared ReadOnly empty As New anchors()
 
         Private ReadOnly m As New map(Of String, anchor)()
+#If provide_callee_pos_lookup Then
         Private ReadOnly p As New map(Of UInt32, anchor)()
+#End If
 
         Public Sub clear()
             m.clear()
+#If provide_callee_pos_lookup Then
             p.clear()
+#End If
         End Sub
 
         Public Function define(ByVal name As String,
@@ -46,11 +52,18 @@ Namespace logic
             assert(Not o Is Nothing)
             assert(Not return_type.null_or_whitespace())
             Dim a As New anchor(name, o.size(), return_type, parameters)
+#If provide_callee_pos_lookup Then
             If m.find(a.name) = m.end() Then
                 assert(m.emplace(a.name, a).second() AndAlso p.emplace(a.begin, a).second())
                 Return True
             End If
             errors.anchor_redefined(a.name, a.begin, m(a.name), p(a.begin))
+#Else
+            If m.emplace(a.name, a).second() Then
+                Return True
+            End If
+            errors.anchor_redefined(a.name, a.begin, m(a.name), Nothing)
+#End If
             Return False
         End Function
 
@@ -58,8 +71,10 @@ Namespace logic
             Return m.find(name, o)
         End Function
 
+#If provide_callee_pos_lookup Then
         Public Function [of](ByVal begin As UInt32, ByRef o As anchor) As Boolean
             Return p.find(begin, o)
         End Function
+#End If
     End Class
 End Namespace
