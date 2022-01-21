876771adb3dc7d1885defa0a19442754b5450c3f
Zijie He <hzj_jie@hotmail.com>
2022-01-20 16:50:31 -0800

    function-ptr test is now working

--- service/tests/compiler/b2style/b2style_test.vb
+++ service/tests/compiler/b2style/b2style_test.vb
@@ -707,6 +707,17 @@ Public NotInheritable Class b2style_test
         assertion.equal(io.output(), "2abc1")
     End Sub
 
+    <test>
+    Private Shared Sub function_ptr()
+        Dim io As New console_io.test_wrapper()
+        Dim e As executor = Nothing
+        assertion.is_true(b2style.with_functions(New interrupts(+io)).
+                                  parse(_b2style_test_data.function_ptr.as_text(), e))
+        assertion.is_not_null(e)
+        e.assert_execute_without_errors()
+        assertion.equal(io.output(), "10199")
+    End Sub
+
     Private Sub New()
     End Sub
 End Class
