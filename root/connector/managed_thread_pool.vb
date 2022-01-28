c8273a05dede95f7b32c9c0b43e3b9c1206d4f7b
Zijie He <hzj_jie@hotmail.com>
2022-01-27 16:33:58 -0800

    break osi.root.connector.threadpool

---
+++ root/connector/managed_thread_pool.vb
@@ -0,0 +1,12 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+' Provides similar APIs as osi.root.threadpool.thread_pool and error protections, but delegates the works to the
+' ManagedThreadPool in .Net.
+Public NotInheritable Class managed_thread_pool
+
+    Private Sub New()
+    End Sub
+End Class
