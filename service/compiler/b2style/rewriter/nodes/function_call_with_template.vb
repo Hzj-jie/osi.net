758674946adf427655ce7769642c8a7fc21696e1
Zijie He <hzj_jie@hotmail.com>
2022-01-29 17:30:27 -0800

    Work with l.typed(Of function_call)

--- service/compiler/b2style/rewriter/nodes/function_call_with_template.vb
+++ service/compiler/b2style/rewriter/nodes/function_call_with_template.vb
@@ -4,7 +4,6 @@ Option Infer Off
 Option Strict On
 
 Imports osi.root.connector
-Imports osi.root.formation
 Imports osi.service.automata
 Imports osi.service.compiler.rewriters
 Imports osi.service.constructor
@@ -24,17 +23,11 @@ Partial Public NotInheritable Class b2style
         Public Function build(ByVal n As typed_node, ByVal o As typed_node_writer) As Boolean _
                 Implements code_gen(Of typed_node_writer).build
             assert(Not n Is Nothing)
-            assert(n.child_count() = 6 OrElse n.child_count() = 7)
             Dim extended_type As String = Nothing
-            If Not scope.current().template().resolve(l, n, extended_type) Then
+            If Not scope.current().template().resolve(l, n.child(0), extended_type) Then
                 Return False
             End If
-            ' extended_type = _namespace.bstyle_format.of(extended_type)
-            scope.current().call_hierarchy().to(extended_type)
-            Return o.append(extended_type) AndAlso
-                   o.append(n.child(4)) AndAlso
-                   o.append(n.child(5)) AndAlso
-                   If(n.child_count() = 7, o.append(n.child(6)), True)
+            Return l.typed(Of function_call).build(extended_type, n, o)
         End Function
     End Class
 End Class
