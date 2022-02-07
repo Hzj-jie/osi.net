df814f1b38db6b3c1fcfb0be19d42c020de5db0f
Zijie He <hzj_jie@hotmail.com>
2022-02-07 10:04:37 -0800

    Use a dedicated build of osi.tests.service.resource/tar_gen to speed up generating tar files

--- service/compiler/bstyle/lib/run.cmd
+++ service/compiler/bstyle/lib/run.cmd
@@ -1,4 +1,4 @@
 
 del /s *.un~
-..\..\..\..\root\utt\bin\Release\osi.root.utt tar_gen --output=bstyle_lib
+..\..\..\resource\gen\tar_gen\osi.root.utt tar_gen --output=bstyle_lib
 move /Y *.vb ..\
--- service/tests/compiler/b2style/self-test/run.cmd
+++ service/tests/compiler/b2style/self-test/run.cmd
@@ -1,4 +1,4 @@
 
 del /s *.un~
-..\..\..\..\..\root\utt\bin\Release\osi.root.utt tar_gen --pattern=*.txt --output=b2style_self_test_cases
+..\..\..\..\resource\gen\tar_gen\osi.root.utt tar_gen --pattern=*.txt --output=b2style_self_test_cases
 move /Y *.vb ..\
