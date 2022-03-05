a97ffbe799ccb663c13ea2c41f91ad9e55dfe378
Hzj_jie <hzj_jie@hotmail.com>
2022-03-04 22:49:16 -0800

    start vector

---
+++ service/compiler/b2style/lib/std/types.h
@@ -0,0 +1,21 @@
+﻿
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+#include <bstyle.h>
+
+namespace std {
+
+typedef ::string string;
+typedef ::void void;
+typedef ::bool bool;
+typedef ::biguint biguint;
+typedef ::long long;
+typedef ::int int;
+typedef ::byte byte;
+typedef ::ufloat ufloat;
+
+}  // namespace std
+
+#endif  // STD_TYPES_H
+
