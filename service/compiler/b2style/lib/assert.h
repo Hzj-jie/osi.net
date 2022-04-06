c9a100bd44c66fa1c56c7e7bd1ab1b5e2dd1bae3
Hzj_jie <hzj_jie@hotmail.com>
2022-04-06 14:34:26 -0700

    bstyle::str_empty and str_len

--- service/compiler/b2style/lib/assert.h
+++ service/compiler/b2style/lib/assert.h
@@ -4,15 +4,28 @@
 
 #include <b2style/stdio.h>
 #include <b2style/types.h>
+#include <bstyle/str.h>
 
-void assert(bool v, string msg) {
+void assert(string statement, bool v, string msg) {
   if (v) return;
-  b2style::std_out(msg);
+  if (bstyle::str_empty(statement)) {
+    b2style::std_out(msg);
+  } else {
+    b2style::std_out(statement + ": " + msg);
+  }
   logic "stop";
 }
 
+void assert(bool v, string msg) {
+  assert("", v, msg);
+}
+
+void assert(string statement, bool v) {
+  assert(statement, v, "Assertion failure");
+}
+
 void assert(bool v) {
-  assert(v, "Assertion failure");
+  assert("", v);
 }
 
 #endif  // B2STYLE_LIB_ASSERT_H
--- service/compiler/bstyle/lib/bstyle/str.h
+++ service/compiler/bstyle/lib/bstyle/str.h
@@ -19,4 +19,19 @@ string bstyle__to_str(byte i) {
   return bstyle__str_concat(s, i);
 }
 
+int bstyle__str_len(string s) {
+  int r;
+  logic "sizeof r s";
+  return r;
+}
+
+bool bstyle__str_empty(string s) {
+  // Note, empty means the s == null rather than s.length == 0.
+  int r = bstyle__str_len(s);
+  bool result;
+  int zero = 0;
+  logic "equal result r zero";
+  return result;
+}
+
 #endif  // BSTYLE_LIB_BSTYLE_STR_H
