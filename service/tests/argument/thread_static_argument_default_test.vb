66af4076d9163cdfbde75411202e75402d778974
Hzj_jie <Hzj_jie@hotmail.com>
2022-04-11 00:31:51 -0700

    Implement thread_static_argument_default and use it in disable_cycle_dependency_check (#44)
    * Implement thread_static_argument_default
    
    * Use thread_static_argument_default in automata.syntaxer/matching
    
    * More thread_static_argument_default_test

---
+++ service/tests/argument/thread_static_argument_default_test.vb
@@ -0,0 +1,50 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports System.Threading
+Imports osi.root.connector
+Imports osi.root.delegates
+Imports osi.root.utt
+Imports osi.root.utt.attributes
+
+<test>
+Public NotInheritable Class thread_static_argument_default_test
+    Private Shared arg As argument(Of Boolean)
+    Private Shared arg2 As argument(Of Int32)
+
+    <test>
+    Private Shared Sub current_thread()
+        If Not expectation.is_false(-arg) Then
+            Return
+        End If
+        assertion.is_false(arg Or thread_static_argument_default(Of thread_static_argument_default_test).of(False))
+        Using thread_static_argument_default(Of Boolean, thread_static_argument_default_test).scoped_register(True)
+            assertion.is_true(arg Or thread_static_argument_default(Of thread_static_argument_default_test).of(False))
+        End Using
+    End Sub
+
+    <test>
+    Private Shared Sub multiple_threads()
+        If Not expectation.is_false(-arg2) Then
+            Return
+        End If
+        Dim m As New AutoResetEvent(False)
+        Using thread_static_argument_default(Of Int32, thread_static_argument_default_test).scoped_register(1)
+            assertion.equal(arg2 Or thread_static_argument_default(Of thread_static_argument_default_test).of(2), 1)
+            Dim t As New Thread(
+                Sub()
+                    assertion.equal(arg2 Or
+                                    thread_static_argument_default(Of thread_static_argument_default_test).of(2),
+                                    2)
+                    assertion.is_true(m.force_set())
+                End Sub)
+            t.Start()
+            assertion.is_true(m.wait())
+        End Using
+    End Sub
+
+    Private Sub New()
+    End Sub
+End Class
