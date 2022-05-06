7e3597911cfacb7d137bab108948202b7fd58aac
Hzj_jie <hzj_jie@hotmail.com>
2022-05-05 23:38:48 -0700

    Remove unused imports

--- service/compiler/bstyle/logic/nodes/value.target.vb
+++ service/compiler/bstyle/logic/nodes/value.target.vb
@@ -5,7 +5,6 @@ Option Strict On
 
 Imports osi.root.connector
 Imports osi.root.formation
-Imports osi.service.automata
 Imports osi.service.compiler.logic
 Imports target = osi.service.compiler.bstyle.scope.value_target_t.target
 
