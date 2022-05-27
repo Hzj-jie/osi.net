c81c34719928439cfaf7869aface0e948eeb8f2d
Hzj_jie <hzj_jie@hotmail.com>
2022-05-27 01:00:48 -0700

    raw_heap_ptr

---
+++ service/compiler/b2style/lib/b2style/raw_heap_ptr.h
@@ -0,0 +1,50 @@
+﻿
+#ifndef B2STYLE_LIB_B2STYLE_RAW_HEAP_PTR
+#define B2STYLE_LIB_B2STYLE_RAW_HEAP_PTR
+
+#include <b2style.h>
+#include <assert.h>
+
+namespace b2style {
+
+class raw_heap_ptr {
+  type_ptr p;
+
+  bool is_null() {
+    bool r;
+    logic "empty b2style__r this.p";
+    return r;
+  }
+
+  template <T>
+  T get() {
+    type_ptr v = this.p;
+    static_cast(v, T);
+    return v;
+  }
+
+  void set_null() {
+    type_ptr x;
+    this.p = x;
+  }
+
+  // TODO: Find a good way to automatically dealloc heap resources.
+  template <T>
+  void destruct() {
+    ::assert(!this.is_null());
+    T x = this.get<T>();
+    dealloc(x);
+    type_ptr x;
+    this.p = x;
+  }
+
+  template <T>
+  void set(T t) {
+    static_cast(t, type_ptr);
+    this.p = t;
+  }
+};
+
+}  // namespace b2style
+
+#endif  // B2STYLE_LIB_B2STYLE_RAW_HEAP_PTR
