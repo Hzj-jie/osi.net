c3b2031de4ca2dfefd372e9e5d0eef1a2052350b
Zijie He <hzj_jie@hotmail.com>
2022-06-14 18:28:15 -0700

    function-call-with-template now supports struct functions

--- service/compiler/b2style/rewriter/nodes/function_call_with_template.vb
+++ service/compiler/b2style/rewriter/nodes/function_call_with_template.vb
@@ -4,6 +4,7 @@ Option Infer Off
 Option Strict On
 
 Imports osi.root.connector
+Imports osi.root.formation
 Imports osi.service.automata
 Imports osi.service.compiler.rewriters
 
@@ -15,10 +16,18 @@ Partial Public NotInheritable Class b2style
                 Implements code_gen(Of typed_node_writer).build
             assert(Not n Is Nothing)
             Dim extended_type As String = Nothing
-            If Not scope.current().template().resolve(n.child(0), extended_type) Then
-                Return False
-            End If
-            Return code_gens().typed(Of function_call).build(_namespace.with_global_namespace(extended_type), n, o)
+            Return scope.current().template().resolve(
+                       Function(ByRef name As String) As Boolean
+                           Dim t As tuple(Of String, String) = Nothing
+                           If function_call.split_struct_function(n.child(0).child(0).input_without_ignored(), t) Then
+                               name = t.second()
+                               Return True
+                           End If
+                           Return False
+                       End Function,
+                       n.child(0),
+                       extended_type) AndAlso
+                   code_gens().typed(Of function_call).build(_namespace.with_global_namespace(extended_type), n, o)
         End Function
     End Class
 End Class
