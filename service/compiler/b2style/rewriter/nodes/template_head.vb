ddf14013e58765b13daa158a94e7fb911d6481bf
Zijie He <hzj_jie@hotmail.com>
2022-06-22 18:04:05 -0700

    Remove unnecessary template_head.build

--- service/compiler/b2style/rewriter/nodes/template_head.vb
+++ service/compiler/b2style/rewriter/nodes/template_head.vb
@@ -6,18 +6,9 @@ Option Strict On
 Imports osi.root.connector
 Imports osi.root.formation
 Imports osi.service.automata
-Imports osi.service.compiler.rewriters
 
 Partial Public NotInheritable Class b2style
     Private NotInheritable Class template_head
-        Implements code_gen(Of typed_node_writer)
-
-        Public Function build(ByVal n As typed_node,
-                              ByVal o As typed_node_writer) As Boolean Implements code_gen(Of typed_node_writer).build
-            assert(False)
-            Return False
-        End Function
-
         Public Function type_param_list(ByVal n As typed_node) As vector(Of String)
             assert(Not n Is Nothing)
             assert(n.child_count() = 4)
