836bd5be50c2deae19c90c06970cd44aa513d4cc
Hzj_jie <hzj_jie@hotmail.com>
2022-08-14 00:18:47 -0700

    Move scope.accessor.vb into scope.vb

--- service/compiler/b2style/scope.vb
+++ service/compiler/b2style/scope.vb
@@ -4,6 +4,7 @@ Option Infer Off
 Option Strict On
 
 Imports osi.root.connector
+Imports osi.root.formation
 Imports osi.service.constructor
 
 Partial Public NotInheritable Class b2style
@@ -37,5 +38,32 @@ Partial Public NotInheritable Class b2style
         Public Shared Function wrap() As scope
             Return current().start_scope()
         End Function
+
+        Protected Overrides Function get_accessor() As scope(Of scope).accessor_t
+            Return New accessor_t(Me)
+        End Function
+
+        Protected Shadows Class accessor_t
+            Inherits scope(Of scope).accessor_t
+
+            Private ReadOnly s As scope
+
+            Public Sub New(ByVal s As scope)
+                assert(Not s Is Nothing)
+                Me.s = s
+            End Sub
+
+            Public Overrides Function includes() As includes_t
+                Return s.incs
+            End Function
+
+            Public Overrides Function defines() As define_t
+                Return s.d
+            End Function
+
+            Public Overrides Function current_function_name() As [optional](Of String)
+                Return scope.current().current_function().name()
+            End Function
+        End Class
     End Class
 End Class
--- service/compiler/bstyle/scope.vb
+++ service/compiler/bstyle/scope.vb
@@ -47,5 +47,61 @@ Partial Public NotInheritable Class bstyle
         Public Function params() As params_t
             Return ps
         End Function
+
+        Protected Overrides Function get_accessor() As scope(Of scope).accessor_t
+            Return New accessor_t(Me)
+        End Function
+
+        Protected Shadows Class accessor_t
+            Inherits scope(Of scope).accessor_t
+
+            Private ReadOnly s As scope
+
+            Public Sub New(ByVal s As scope)
+                assert(Not s Is Nothing)
+                Me.s = s
+            End Sub
+
+            Public Overrides Function includes() As includes_t
+                Return s.incs
+            End Function
+
+            Public Overrides Function defines() As define_t
+                Return s.d
+            End Function
+
+            Public Overrides Function type_alias() As type_alias_t
+                Return s.ta
+            End Function
+
+            Public Overrides Function current_function() As current_function_t
+                Return s.cf
+            End Function
+
+            Public Overrides Sub current_function(ByVal c As current_function_t)
+                s.cf = c
+            End Sub
+
+            ' TODO: Return struct_proxy.
+            Public Overrides Function is_struct_type(ByVal i As String) As Boolean
+                Return s.structs().types().defined(i)
+            End Function
+
+            Public Overrides Function delegates() As delegate_t
+                Return s.de
+            End Function
+
+            Public Overrides Function structs() As struct_t
+                Return s.s
+            End Function
+
+            Public Overrides Function variables() As variable_t
+                Return s.v
+            End Function
+
+            Public Overrides Function temp_logic_name() As temp_logic_name_t
+                Return s.t
+            End Function
+        End Class
     End Class
 End Class
