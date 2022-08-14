dde7448d9116888c722c441849266c49399bd1f0
Zijie He <hzj_jie@hotmail.com>
2022-08-14 15:17:58 -0700

    include b2style_self_compile_error_cases into b3style_nlp_tests

--- service/tests/compiler/b3style/b3style_nlp_test.vb
+++ service/tests/compiler/b3style/b3style_nlp_test.vb
@@ -13,61 +13,101 @@ Imports osi.service.compiler
 Imports osi.service.resource
 
 <test>
-Public NotInheritable Class b3style_nlp_test
-    Inherits compiler_self_test_runner
+Public NotInheritable Class b3style_nlp_tests
+    <test>
+    Public NotInheritable Class non_self_tests
+        ' These compile errors are coming from compiler implementation but not the nlexer or syntaxer, nlp should be
+        ' able to handle them correctly.
+        Private Shared ReadOnly ignored_compile_errors As unordered_set(Of String) = unordered_set.emplace_of(
+            "errors_class_initializer_for_non_class",
+            "errors_cycle_typedef",
+            "errors_duplicate_template_type_parameters",
+            "errors_function_return_struct_type_mismatch",
+            "errors_reinterpret_cast_heap_with_index",
+            "errors_reinterpret_cast_unknown_type",
+            "errors_reinterpret_cast_unknown_variable",
+            "errors_reinterpret_cast_without_type_id",
+            "errors_undefined_value_clause",
+            "errors_value_clause_struct_type_mismatch",
+            "errors_value_clause_struct_type_mismatch2"
+        )
 
-    ' These compile errors are coming from compiler implementation but not the nlexer or syntaxer, nlp should be able to
-    ' handle them correctly.
-    Private Shared ReadOnly ignored_compile_errors As unordered_set(Of String) = unordered_set.emplace_of(
-        "errors_class_initializer_for_non_class",
-        "errors_cycle_typedef",
-        "errors_duplicate_template_type_parameters",
-        "errors_function_return_struct_type_mismatch",
-        "errors_reinterpret_cast_heap_with_index",
-        "errors_reinterpret_cast_unknown_type",
-        "errors_reinterpret_cast_unknown_variable",
-        "errors_reinterpret_cast_without_type_id",
-        "errors_undefined_value_clause",
-        "errors_value_clause_struct_type_mismatch",
-        "errors_value_clause_struct_type_mismatch2"
-    )
+        Private Shared Sub run_tests(ByVal t As Type)
+            For Each v As FieldInfo In t.GetFields(binding_flags.static_public)
+                Dim s As String = direct_cast(Of Byte())(v.GetValue(Nothing)).as_text()
+                If v.Name().StartsWith("errors_") AndAlso
+                   ignored_compile_errors.find(v.Name()) = ignored_compile_errors.end() Then
+                    assertion.is_false(b3style.nlp().parse(s), v.full_name())
+                Else
+                    assertion.is_true(b3style.nlp().parse(s), v.full_name())
+                End If
+            Next
+        End Sub
 
-    Public Sub New()
-        MyBase.New("*", b2style_self_test_cases.data)
-    End Sub
+        <test>
+        Private Shared Sub bstyle_tests()
+            run_tests(GetType(_bstyle_test_data))
+        End Sub
 
-    Private Shared Sub run_test(ByVal s As String, ByVal name As String)
-        assertion.is_true(b3style.nlp().parse(s), name)
-    End Sub
+        <test>
+        Private Shared Sub b2style_tests()
+            run_tests(GetType(_b2style_test_data))
+        End Sub
 
-    Protected Overrides Sub execute(ByVal name As String, ByVal text As String)
-        run_test(text, name)
-    End Sub
+    End Class
 
     <test>
-    Private Shadows Sub run()
-        MyBase.run()
-    End Sub
+    Public NotInheritable Class b2style_self
+        Inherits compiler_self_test_runner
 
-    Private Shared Sub run_tests(ByVal t As Type)
-        For Each v As FieldInfo In t.GetFields(binding_flags.static_public)
-            Dim s As String = direct_cast(Of Byte())(v.GetValue(Nothing)).as_text()
-            If v.Name().StartsWith("errors_") AndAlso
-               ignored_compile_errors.find(v.Name()) = ignored_compile_errors.end() Then
-                assertion.is_false(b3style.nlp().parse(s), v.full_name())
+        Public Sub New()
+            MyBase.New("*", b2style_self_test_cases.data)
+        End Sub
+
+        Protected Overrides Sub execute(ByVal name As String, ByVal text As String)
+            assertion.is_true(b3style.nlp().parse(text), name)
+        End Sub
+
+        <test>
+        Private Shadows Sub run()
+            MyBase.run()
+        End Sub
+    End Class
+
+    <test>
+    Public NotInheritable Class b2style_self_compile
+        Inherits compiler_self_test_runner
+
+        ' These compile errors are coming from compiler implementation but not the nlexer or syntaxer, nlp should be
+        ' able to handle them correctly.
+        Private Shared ReadOnly ignored_compile_errors As unordered_set(Of String) = unordered_set.emplace_of(
+            "copy-struct-to-primitive.txt",
+            "copy-primitive-to-struct.txt",
+            "duplicate-var-name-in-inherited-class.txt",
+            "duplicate-func-name-in-class.txt",
+            "struct-with-myself.txt",
+            "templates-with-same-name.txt",
+            "type-parameters-with-same-name.txt"
+        )
+
+        Public Sub New()
+            MyBase.New("*", b2style_self_compile_error_cases.data)
+        End Sub
+
+        Protected Overrides Sub execute(ByVal name As String, ByVal text As String)
+            If ignored_compile_errors.find(name) = ignored_compile_errors.end() Then
+                assertion.is_false(b3style.nlp().parse(text), name)
             Else
-                run_test(s, v.full_name())
+                assertion.is_true(b3style.nlp().parse(text), name)
             End If
-        Next
-    End Sub
+        End Sub
 
-    <test>
-    Private Shared Sub bstyle_tests()
-        run_tests(GetType(_bstyle_test_data))
-    End Sub
+        <test>
+        Private Shadows Sub run()
+            MyBase.run()
+        End Sub
+    End Class
 
-    <test>
-    Private Shared Sub b2style_tests()
-        run_tests(GetType(_b2style_test_data))
+    Private Sub New()
     End Sub
 End Class
