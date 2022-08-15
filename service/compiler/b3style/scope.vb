6eaba42af1ee578faeffeae1bdd14b4718142219
Zijie He <hzj_jie@hotmail.com>
2022-08-14 17:14:54 -0700

    b3style/scope

---
+++ service/compiler/b3style/scope.vb
@@ -0,0 +1,21 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.service.constructor
+
+Partial Public NotInheritable Class b3style
+    Partial Public NotInheritable Class scope
+        Inherits scope(Of scope)
+
+        <inject_constructor>
+        Public Sub New(ByVal parent As scope)
+            MyBase.New(parent)
+        End Sub
+
+        Public Sub New()
+            Me.New(Nothing)
+        End Sub
+    End Class
+End Class
