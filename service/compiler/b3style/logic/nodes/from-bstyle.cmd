07ecc2caaaf86f66ce02c66513e17f5bc7623ae8
Hzj_jie <hzj_jie@hotmail.com>
2023-01-01 18:53:21 -0800

    Avoid repeating .vb in from-bstyle.cmd

--- service/compiler/b3style/logic/nodes/from-bstyle.cmd
+++ service/compiler/b3style/logic/nodes/from-bstyle.cmd
@@ -3,32 +3,32 @@ setlocal
 
 path %PATH%;..\..\..\..\..\root\codegen\sed;
 
-call :run raw_value.vb
-call :run biguint.vb
-call :run bool.vb
-call :run integer.vb
-call :run string.vb
-call :run ufloat.vb
+call :run raw_value
+call :run biguint
+call :run bool
+call :run integer
+call :run string
+call :run ufloat
 
-call :run logic_name.vb
-call :run function.vb
-call :run function_call.vb
-call :run ignore_result_function_call.vb
-call :run param.vb
-call :run return_clause.vb
-call :run value_clause.vb
-call :run value_declaration.vb
-call :run heap_declaration.vb
-call :run value_definition.vb
-call :run heap_name.vb
-call :run struct.vb
-call :run raw_variable_name.vb
-call :run value_list.vb
-call :run typedef.vb
-call :run static_cast.vb
+call :run logic_name
+call :run function
+call :run function_call
+call :run ignore_result_function_call
+call :run param
+call :run return_clause
+call :run value_clause
+call :run value_declaration
+call :run heap_declaration
+call :run value_definition
+call :run heap_name
+call :run struct
+call :run raw_variable_name
+call :run value_list
+call :run typedef
+call :run static_cast
 
 exit /b 0
 
 :RUN
-sed " Class bstyle" " Class b3style" ..\..\..\bstyle\logic\nodes\%~1 > %~1
+sed " Class bstyle" " Class b3style" ..\..\..\bstyle\logic\nodes\%~1.vb > %~1.vb
 exit /b 0
