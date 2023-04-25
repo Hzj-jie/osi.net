f1cf579d8fa4aed4a6a0b4f49425e6fea1084e05
Hzj_jie <hzj_jie@hotmail.com>
2023-04-24 20:50:21 -0700

    start normalized_type

---
+++ service/compiler/scope/normalized_type.vb
@@ -0,0 +1,17 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.root.delegates
+Imports osi.root.formation
+
+Partial Public Class scope(Of WRITER As {lazy_list_writer, New},
+                              __BUILDER As func_t(Of String, WRITER, Boolean),
+                              __CODE_GENS As func_t(Of code_gens(Of WRITER)),
+                              T As scope(Of WRITER, __BUILDER, __CODE_GENS, T))
+    Public NotInheritable Class normalized_type
+
+    End Class
+End Class
