73e259b30c09d4040230bd17ee37999ce4679735
Hzj_jie <hzj_jie@hotmail.com>
2023-05-06 17:23:25 -0700

    normalized_parameter missed ref in of(builder.parameter)

--- service/compiler/scope/normalized_type.vb
+++ service/compiler/scope/normalized_type.vb
@@ -13,12 +13,8 @@ Partial Public Class scope(Of WRITER As {lazy_list_writer, New},
                               T As scope(Of WRITER, __BUILDER, __CODE_GENS, T))
     ' A helper to always de-alias and apply namespace.
     Public NotInheritable Class normalized_type
-        Public Shared Function [of](ByVal type As String) As builders.parameter_type
-            Return New builders.parameter_type(type).map_type(AddressOf map_type)
-        End Function
-
         Public Shared Function logic_type_of(ByVal type As String) As String
-            Return [of](type).logic_type()
+            Return New builders.parameter_type(type).map_type(AddressOf map_type).logic_type()
         End Function
 
         Public Shared Function map_type(ByVal i As String) As String
@@ -31,16 +27,15 @@ Partial Public Class scope(Of WRITER As {lazy_list_writer, New},
 
     Public NotInheritable Class normalized_parameter
         Public Shared Function [of](ByVal type As String, ByVal name As String) As builders.parameter
-            Return New builders.parameter(type, name).
-                                map_type(AddressOf normalized_type.map_type).
-                                map_name(Function(ByVal i As String) As String
-                                             Return current_namespace_t.of(i)
-                                         End Function)
+            Return [of](New builders.parameter(type, name))
         End Function
 
         Public Shared Function [of](ByVal i As builders.parameter) As builders.parameter
             assert(Not i Is Nothing)
-            Return [of](i.type, i.name)
+            Return i.map_type(AddressOf normalized_type.map_type).
+                     map_name(Function(ByVal x As String) As String
+                                  Return current_namespace_t.of(x)
+                              End Function)
         End Function
 
         Private Sub New()
