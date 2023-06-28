Hzj-jie/osi.net

commit 9daa1f7f1803b5ef7394bf6f67b1f35f4b1b2b55

Allow argument ignoring short switchers so that -* in utt can work

service/argument/config.vb
@@ -5,8 +5,9 @@ Option Strict On
 
 Imports osi.root.connector
 Imports osi.root.constants
+Imports envs = osi.root.envs
 
-Public Class config
+Public NotInheritable Class config
     Implements ICloneable, ICloneable(Of config)
 
     Public Shared ReadOnly [default] As config
@@ -16,22 +17,29 @@ Public Class config
     End Sub
 
     Public argument_separator As String = character.blank
-    Public switcher_prefix As String = character.minus_sign + character.left_slash
+    Public switcher_prefix As String = default_switcher_prefix
     Public full_switcher_prefix As String = character.tilde
     Public arg_prefix As String = character.minus_sign + character.minus_sign
     Public arg_key_value_separator As String = character.equal_sign
     Public case_sensitive As Boolean = True
 
+    Private Shared ReadOnly default_switcher_prefix As String =
+        Function() As String
+            If envs.env_bool(envs.env_keys("argument", "no", "short", "switcher")) Then
+                Return Nothing
+            End If
+            Return character.minus_sign + character.left_slash
+        End Function()
+
     Public Sub New()
     End Sub
 
-    <copy_constructor>
-    Protected Sub New(ByVal argument_separator As String,
-                      ByVal switcher_prefix As String,
-                      ByVal full_switcher_prefix As String,
-                      ByVal arg_prefix As String,
-                      ByVal arg_key_value_separator As String,
-                      ByVal case_sensitive As Boolean)
+    Private Sub New(ByVal argument_separator As String,
+                    ByVal switcher_prefix As String,
+                    ByVal full_switcher_prefix As String,
+                    ByVal arg_prefix As String,
+                    ByVal arg_key_value_separator As String,
+                    ByVal case_sensitive As Boolean)
         Me.argument_separator = argument_separator
         Me.switcher_prefix = switcher_prefix
         Me.full_switcher_prefix = Me.full_switcher_prefix
@@ -56,18 +64,12 @@ Public Class config
         Return strjoin(argument_separator, kvs)
     End Function
 
-    Private Function is_not_any_prefix(ByVal c As Char) As Boolean
-        Return strindexof(switcher_prefix, c, case_sensitive) = npos AndAlso
-               strindexof(arg_prefix, c, case_sensitive) = npos AndAlso
-               strindexof(full_switcher_prefix, c, case_sensitive) = npos
-    End Function
-
     Private Function is_switcher_prefix(ByVal i As String) As Boolean
         Return Not i.null_or_empty() AndAlso
                Not switcher_prefix.null_or_empty() AndAlso
                strlen(i) > 1 AndAlso
                strindexof(switcher_prefix, i(0), case_sensitive) <> npos AndAlso
-               is_not_any_prefix(i(1))
+               Not is_arg_prefix(i)
     End Function
 
     Private Function is_full_switcher_prefix(ByVal i As String) As Boolean
@@ -75,15 +77,14 @@ Public Class config
                Not full_switcher_prefix.null_or_empty() AndAlso
                strlen(i) > 1 AndAlso
                strindexof(full_switcher_prefix, i(0), case_sensitive) <> npos AndAlso
-               is_not_any_prefix(i(1))
+               Not is_arg_prefix(i)
     End Function
 
     Private Function is_arg_prefix(ByVal i As String) As Boolean
         Return Not i.null_or_empty() AndAlso
                Not arg_prefix.null_or_empty() AndAlso
                strlen(i) > strlen(arg_prefix) AndAlso
-               strsame(i, arg_prefix, strlen(arg_prefix)) AndAlso
-               is_not_any_prefix(i(strlen_i(arg_prefix)))
+               strsame(i, arg_prefix, strlen(arg_prefix))
     End Function
 
     Friend Function is_switcher(ByVal i As String, ByRef o As String) As Boolean
@@ -124,19 +125,15 @@ Public Class config
     End Function
 
     Public Function Clone() As Object Implements ICloneable.Clone
-        Return cloneT()
+        Return CloneT()
     End Function
 
     Public Function CloneT() As config Implements ICloneable(Of config).Clone
-        Return clone(Of config)()
-    End Function
-
-    Public Function clone(Of R As config)() As R
-        Return copy_constructor(Of R).invoke(argument_separator,
-                                             switcher_prefix,
-                                             full_switcher_prefix,
-                                             arg_prefix,
-                                             arg_key_value_separator,
-                                             case_sensitive)
+        Return New config(argument_separator,
+                          switcher_prefix,
+                          full_switcher_prefix,
+                          arg_prefix,
+                          arg_key_value_separator,
+                          case_sensitive)
     End Function
 End Class

root/utt/app.vb
@@ -13,6 +13,8 @@ Imports this_process = osi.root.envs.this_process
 Public Module _app
     Public Sub main(ByVal args() As String)
         debugpause()
+        ' - or / style switcher conflicts with negative string patterns.
+        assert(envs.set_env("argument-no-short-switcher", "true"))
         global_init.execute(load_assemblies:=True)
         Dim start_ms As Int64 = Now().milliseconds()
         If envs.utt_no_assert Then

commit 740eb3d7689d659910305b63a07d0aa4371d47da

9daa1f7f1803b5ef7394bf6f67b1f35f4b1b2b55 broke osi.tests.service.argument.*

root/utt/host.vb
@@ -136,15 +136,11 @@ Partial Friend NotInheritable Class host
         Return has_fit_true
     End Function
 
-    Public Shared Sub clear_selection()
+    Public Shared Function run(ByVal selector As vector(Of String)) As Int32
         cases.stream().foreach(Sub(ByVal x As case_info)
                                    x.finished = False
                                End Sub)
-    End Sub
-
-    Public Shared Function run(ByVal selector As vector(Of String)) As Int32
         Dim r As Int32 = 0
-        clear_selection()
         cases.stream().foreach(Sub(ByVal x)
                                    If [select](selector, x) Then
                                        r += 1

root/utt/selfhealth/selfhealth.vb
@@ -6,14 +6,14 @@ Option Strict On
 Imports osi.root.connector
 Imports osi.root.formation
 
-Public Module selfhealth
-    Private self_health As Boolean = False
+Public NotInheritable Class selfhealth
+    Private Shared self_health As Boolean = False
 
-    Public Function self_health_stage() As Boolean
+    Public Shared Function self_health_stage() As Boolean
         Return self_health
     End Function
 
-    Public Function run() As Boolean
+    Public Shared Function run() As Boolean
         Dim selectors As New vector(Of String)()
         selectors.push_back(GetType(failure_case).AssemblyQualifiedName())
 
@@ -34,4 +34,7 @@ Public Module selfhealth
 
         Return True
     End Function
-End Module
+
+    Private Sub New()
+    End Sub
+End Class

service/argument/argument_setter.vb
@@ -19,7 +19,7 @@ Public NotInheritable Class argument_setter
                         "process arguments from ",
                         Microsoft.VisualBasic.Command(),
                         ", parsed arguments ",
-                        var.[default])
+                        var.application)
         End Sub
 
         Private Sub New()
@@ -32,7 +32,7 @@ Public NotInheritable Class argument_setter
 
     Private Shared Sub process_assembly(ByVal assembly As Assembly)
         For Each type As Type In assembly.GetTypes()
-            process_type(type, var.[default])
+            process_type(type, var.application)
         Next
     End Sub
 

service/argument/config.vb
@@ -5,33 +5,23 @@ Option Strict On
 
 Imports osi.root.connector
 Imports osi.root.constants
-Imports envs = osi.root.envs
 
 Public NotInheritable Class config
     Implements ICloneable, ICloneable(Of config)
 
-    Public Shared ReadOnly [default] As config
-
-    Shared Sub New()
-        [default] = New config()
-    End Sub
+    Public Shared Function [default]() As config
+        Return New config()
+    End Function
 
     Public argument_separator As String = character.blank
-    Public switcher_prefix As String = default_switcher_prefix
+    Public switcher_prefix As String = character.minus_sign + character.left_slash
     Public full_switcher_prefix As String = character.tilde
     Public arg_prefix As String = character.minus_sign + character.minus_sign
     Public arg_key_value_separator As String = character.equal_sign
     Public case_sensitive As Boolean = True
 
-    Private Shared ReadOnly default_switcher_prefix As String =
-        Function() As String
-            If envs.env_bool(envs.env_keys("argument", "no", "short", "switcher")) Then
-                Return Nothing
-            End If
-            Return character.minus_sign + character.left_slash
-        End Function()
-
-    Public Sub New()
+    ' Anything should start from [default]() then CloneT().
+    Private Sub New()
     End Sub
 
     Private Sub New(ByVal argument_separator As String,
@@ -42,7 +32,7 @@ Public NotInheritable Class config
                     ByVal case_sensitive As Boolean)
         Me.argument_separator = argument_separator
         Me.switcher_prefix = switcher_prefix
-        Me.full_switcher_prefix = Me.full_switcher_prefix
+        Me.full_switcher_prefix = full_switcher_prefix
         Me.arg_prefix = arg_prefix
         Me.arg_key_value_separator = arg_key_value_separator
         Me.case_sensitive = case_sensitive
@@ -137,3 +127,4 @@ Public NotInheritable Class config
                           case_sensitive)
     End Function
 End Class
+

service/argument/var.vb
@@ -6,9 +6,18 @@ Option Strict On
 Imports osi.root.connector
 Imports osi.root.constants
 Imports osi.root.formation
+Imports envs = osi.root.envs
 
 Partial Public NotInheritable Class var
-    Public Shared ReadOnly [default] As var = New var(streams.of(Environment.GetCommandLineArgs()).skip(1).to_array())
+    Public Shared ReadOnly application As var =
+        New var(streams.of(Environment.GetCommandLineArgs()).skip(1).to_array(),
+                Function() As config
+                    Dim r As config = config.default()
+                    If envs.env_bool(envs.env_keys("argument", "no", "short", "switcher")) Then
+                        r.switcher_prefix = Nothing
+                    End If
+                    Return r
+                End Function())
 
     Private ReadOnly raw As map(Of String, vector(Of String))
     Private ReadOnly binded As map(Of String, vector(Of String))
@@ -42,7 +51,7 @@ Partial Public NotInheritable Class var
     End Sub
 
     Public Sub New(Optional ByVal c As config = Nothing)
-        Me.New(If(c Is Nothing, config.default, c),
+        Me.New(If(c Is Nothing, config.default(), c),
                New map(Of String, vector(Of String))(),
                New map(Of String, vector(Of String))(),
                New vector(Of String)())

service/tests/argument/argument_setter_test.vb
@@ -249,7 +249,7 @@ Public NotInheritable Class argument_setter_test
     <test>
     Private Shared Sub unset_bool_should_be_undefined()
         argument_holder9.bool_arg = Nothing
-        argument_setter.process_type(GetType(argument_holder9), var.default)
+        argument_setter.process_type(GetType(argument_holder9), var.application)
         If assertion.is_not_null(argument_holder9.bool_arg) Then
             assertion.is_false(-argument_holder9.bool_arg)
             assertion.is_true(argument_holder9.bool_arg Or True)

service/tests/argument/var_compare_test.vb
@@ -1,5 +1,4 @@
 ﻿
-Imports osi.root.connector
 Imports osi.root.utt
 Imports osi.service.argument
 