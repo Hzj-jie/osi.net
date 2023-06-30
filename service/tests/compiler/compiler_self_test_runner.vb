c4411535dc68a2058c2dbb11073923dc4f72e01e
Hzj_jie <hzj_jie@hotmail.com>
2023-06-29 23:27:54 -0700

    ignore struct-and-primitive-type-with-same-name.txt

--- service/tests/compiler/compiler_self_test_runner.vb
+++ service/tests/compiler/compiler_self_test_runner.vb
@@ -31,7 +31,14 @@ Public MustInherit Class compiler_self_test_runner
                 Dim text As String = content.ReadToEnd()
                 a.emplace_back(Sub()
                                    If Not name.match_pattern(filter) AndAlso
-                                      Not name.match_pattern((filter) + ".txt") Then
+                                      Not name.match_pattern(filter + ".txt") Then
+                                       raise_error(error_type.user,
+                                                   "Ignore test case ",
+                                                   name,
+                                                   " through the filter.")
+                                       Return
+                                   End If
+                                   If ignore_case(name) Then
                                        raise_error(error_type.user, "Ignore test case ", name)
                                        Return
                                    End If
@@ -47,4 +54,8 @@ Public MustInherit Class compiler_self_test_runner
 
     Protected MustOverride Sub execute(ByVal name As String, ByVal text As String)
     Protected MustOverride Function with_current_file(ByVal filename As String) As IDisposable
+
+    Protected Overridable Function ignore_case(ByVal name As String) As Boolean
+        Return False
+    End Function
 End Class
