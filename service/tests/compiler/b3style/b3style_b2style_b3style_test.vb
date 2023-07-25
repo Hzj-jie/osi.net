3606a55fb845518dcfabd37df492b7b839b38dc0
hzj_jie <hzj_jie@hotmail.com>
2023-07-25 01:01:17 -0700

    Add b3style_b2style_test

--- service/tests/compiler/b3style/b3style_b2style_b3style_test.vb
+++ service/tests/compiler/b3style/b3style_b2style_b3style_test.vb
@@ -4,9 +4,7 @@ Option Infer Off
 Option Strict On
 
 Imports osi.root.template
-Imports osi.root.utt
 Imports osi.root.utt.attributes
-Imports osi.service.automata
 Imports osi.service.compiler
 Imports osi.service.interpreter.primitive
 
@@ -23,11 +21,6 @@ Public NotInheritable Class b3style_b2style_b3style_test
             Return New b2style.parse_wrapper_b3style(New interrupts(+i)).parse(j, k)
         End Function
     End Class
-
-    <test>
-    Private Shared Sub nlp_parsable()
-        assertion.is_true(nlp.of_file(b3style.nlexer_rule, b3style.syntaxer_rule, Nothing))
-    End Sub
 
     Private Sub New()
     End Sub
