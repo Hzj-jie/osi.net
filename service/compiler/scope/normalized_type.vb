7c34f2bd298918fba29141bc447bdf7b5ba9a55f
hzj_jie <hzj_jie@hotmail.com>
2023-07-28 01:02:06 -0700

    assert

--- service/compiler/scope/normalized_type.vb
+++ service/compiler/scope/normalized_type.vb
@@ -15,7 +15,7 @@ Partial Public Class scope(Of WRITER As {lazy_list_writer, New},
     Public NotInheritable Class normalized_type
         Public Shared Function full_type_of(ByVal type As String) As String
             ' TODO: Add a prefix for logic_type to avoid being called twice on one type.
-            ' assert(Not type.null_or_whitespace())
+            assert(Not type.null_or_whitespace())
             ' assert(Not type.StartsWith("@"))
             Dim s As String = [of](type).full_type()
             If current().features().with_namespace() Then
