f774201e1dbf5f2dde4349431907549d91d78360
hzj_jie <hzj_jie@hotmail.com>
2023-09-11 21:51:32 -0700

    start implementing b3style/self_value_clause

---
+++ service/compiler/b3style/logic/nodes/self_value_clause.vb
@@ -0,0 +1,20 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.service.automata
+
+Partial Public NotInheritable Class b3style
+    Private NotInheritable Class self_value_clause
+        Implements code_gen(Of logic_writer)
+
+        Private Const self_prefix As String = "self-"
+
+        Public Function build(ByVal n As typed_node,
+                              ByVal o As logic_writer) As Boolean Implements code_gen(Of logic_writer).build
+
+        End Function
+    End Class
+End Class
