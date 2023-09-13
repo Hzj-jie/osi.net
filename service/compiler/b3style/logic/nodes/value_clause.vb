df7a5bdb089fc86303d81335b7573608a8b0e670
hzj_jie <hzj_jie@hotmail.com>
2023-09-12 22:17:32 -0700

    value_clause

--- service/compiler/b3style/logic/nodes/value_clause.vb
+++ service/compiler/b3style/logic/nodes/value_clause.vb
@@ -86,30 +86,16 @@ Partial Public NotInheritable Class b3style
             End Using
         End Function
 
-        Private Shared Function build(ByVal name As typed_node,
-                                      ByVal value As typed_node,
-                                      ByVal struct_copy As Func(Of vector(Of String), Boolean),
-                                      ByVal primitive_copy As Func(Of String, Boolean),
-                                      ByVal o As logic_writer) As Boolean
-            assert(Not value Is Nothing)
-            Return build(name,
-                         Function() As Boolean
-                             Return code_gen_of(value).build(o)
-                         End Function,
-                         value.input_without_ignored(),
-                         struct_copy,
-                         primitive_copy,
-                         o)
-        End Function
-
-        Public Shared Function stack_name_build(ByVal name As typed_node,
-                                                ByVal value As typed_node,
-                                                ByVal o As logic_writer) As Boolean
+        Private Shared Function stack_name_build(ByVal name As typed_node,
+                                                 ByVal value As Func(Of Boolean),
+                                                 ByVal raw_value_str As String,
+                                                 ByVal o As logic_writer) As Boolean
             assert(Not name Is Nothing)
             assert(name.type_name.Equals("name") OrElse name.child().type_name.Equals("name"), name)
             ' TODO: If the value on the right is a temporary value (rvalue), move can be used to reduce memory copy.
             Return build(name,
                          value,
+                         raw_value_str,
                          Function(ByVal r As vector(Of String)) As Boolean
                              Return struct.copy(r, name.input_without_ignored(), o)
                          End Function,
@@ -119,35 +105,67 @@ Partial Public NotInheritable Class b3style
                          o)
         End Function
 
-        Private Function build(ByVal n As typed_node,
-                               ByVal o As logic_writer) As Boolean Implements code_gen(Of logic_writer).build
-            assert(Not n Is Nothing)
+        Public Shared Function stack_name_build(ByVal name As typed_node,
+                                                ByVal value As typed_node,
+                                                ByVal o As logic_writer) As Boolean
+            assert(Not value Is Nothing)
+            Return stack_name_build(name,
+                                    Function() As Boolean
+                                        Return code_gen_of(value).build(o)
+                                    End Function,
+                                    value.input_without_ignored(),
+                                    o)
+        End Function
+
+        Private Shared Function build(ByVal name As typed_node,
+                                      ByVal value As Func(Of Boolean),
+                                      ByVal raw_value_str As String,
+                                      ByVal o As logic_writer) As Boolean
+            assert(Not name Is Nothing)
             assert(Not o Is Nothing)
-            assert(n.child_count() = 3)
-            If Not n.child(0).child().type_name.Equals("heap-name") Then
-                Return stack_name_build(n.child(0).child(), n.child(2), o)
+            If Not name.type_name.Equals("heap-name") Then
+                Return stack_name_build(name, value, raw_value_str, o)
             End If
             Return heap_name.build(
-                       n.child(0).child().child(2),
+                       name.child(2),
                        o,
                        Function(ByVal indexstr As String) As Boolean
-                           Return build(n.child(0).child().child(0),
-                                        n.child(2),
+                           Return build(name.child(0),
+                                        value,
+                                        raw_value_str,
                                         Function(ByVal r As vector(Of String)) As Boolean
                                             Return struct.copy(r,
-                                                               n.child(0).child().child(0).input_without_ignored(),
+                                                               name.child(0).input_without_ignored(),
                                                                indexstr,
                                                                o)
                                         End Function,
                                         Function(ByVal r As String) As Boolean
                                             Return builders.of_copy(
                                                        variable.name_of(
-                                                           n.child(0).child().child(0).input_without_ignored(),
+                                                           name.child(0).input_without_ignored(),
                                                            indexstr),
                                                    r).to(o)
                                         End Function,
                                  o)
                        End Function)
+        End Function
+
+        Public Shared Function build(ByVal name As typed_node,
+                                     ByVal value As Func(Of Boolean),
+                                     ByVal o As logic_writer) As Boolean
+            Return build(name, value, Nothing, o)
+        End Function
+
+        Private Function build(ByVal n As typed_node,
+                               ByVal o As logic_writer) As Boolean Implements code_gen(Of logic_writer).build
+            assert(Not n Is Nothing)
+            assert(n.child_count() = 3)
+            Return build(n.child(0).child(),
+                         Function() As Boolean
+                             Return code_gen_of(n.child(2)).build(o)
+                         End Function,
+                         n.child(2).input_without_ignored(),
+                         o)
         End Function
     End Class
 End Class
