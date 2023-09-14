6e3942678b35f81077f174965fab74d2d8721ca9
Hzj_jie <Hzj_jie@hotmail.com>
2023-09-14 00:52:10 -0700

    Decouple bstyle b3style (#84)
    * remove sed head
    
    * remove current_namespace_t.if_supported

--- service/compiler/bstyle/logic/nodes/function_call.vb
+++ service/compiler/bstyle/logic/nodes/function_call.vb
@@ -24,14 +24,12 @@ Partial Public NotInheritable Class bstyle
             assert(n.child_count() >= 3)
             Dim bc As Func(Of vector(Of String), Boolean) =
                 Function(ByVal parameters As vector(Of String)) As Boolean
-                    Dim function_name As String = n.child(0).input_without_ignored()
-                    If scope.current().variables().try_resolve(function_name, Nothing) Then
-                        Return build_caller_ref(function_name, parameters)
+                    If scope.current().variables().try_resolve(n.child(0).input_without_ignored(), Nothing) Then
+                        Return build_caller_ref(n.child(0).input_without_ignored(), parameters)
                     End If
 
-                    function_name = scope.current_namespace_t.of(function_name)
                     Dim name As String = Nothing
-                    If Not logic_name.of_function_call(function_name, parameters, name) Then
+                    If Not logic_name.of_function_call(n.child(0).input_without_ignored(), parameters, name) Then
                         Return False
                     End If
                     scope.current().call_hierarchy().to(name)
