a1c92eb253735472ed8ed8257434f1b1412e25de
hzj_jie <hzj_jie@hotmail.com>
2023-09-23 02:39:25 -0700

    unnecessary const

--- service/compiler/b3style/logic/nodes/self_value_clause.vb
+++ service/compiler/b3style/logic/nodes/self_value_clause.vb
@@ -10,8 +10,6 @@ Partial Public NotInheritable Class b3style
     Private NotInheritable Class self_value_clause
         Implements code_gen(Of logic_writer)
 
-        Private Const self_prefix As String = "self-"
-
         Private Function build(ByVal n As typed_node,
                                ByVal o As logic_writer) As Boolean Implements code_gen(Of logic_writer).build
             assert(Not n Is Nothing)
@@ -20,7 +18,7 @@ Partial Public NotInheritable Class b3style
                                       Function() As Boolean
                                           Return binary_operation_value.build(
                                                          n.child(0),
-                                                         n.child(1).child().type_name.Substring(self_prefix.Length()),
+                                                         n.child(1).child().type_name.Substring("self-".Length()),
                                                          n.child(2),
                                                          o)
                                       End Function,
