77c609dd9e08e80269cff5b3ed0adb9b57458258
Zijie He <hzj_jie@hotmail.com>
2023-10-06 09:52:55 -0700

    from-b2style.cmd

---
+++ service/compiler/b3style/logic/nodes/from-b2style.cmd
@@ -0,0 +1,12 @@
+﻿
+setlocal
+
+path %PATH%;..\..\..\..\..\root\codegen\sed;
+
+call :run class
+
+exit /b 0
+
+:RUN
+sed " Class b2style" " Class b3style" ..\..\..\b2style\rewriter\nodes\%~1.vb | sed " typed_node_writer" " logic_writer" > %~1.vb
+exit /b 0
