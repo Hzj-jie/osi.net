e57d17761cab3629c6b9f6aebae1e14e0bfa31a4
hzj_jie <hzj_jie@hotmail.com>
2023-10-06 03:37:32 -0700

    from-bstyle

---
+++ service/compiler/b3style/logic/nodes/from-bstyle.cmd
@@ -0,0 +1,31 @@
+﻿
+setlocal
+
+path %PATH%;..\..\..\..\..\root\codegen\sed;
+
+call :run biguint
+call :run bool
+call :run integer
+call :run string
+call :run ufloat
+
+call :run logic_name
+call :run param
+call :run return_clause
+call :run heap_declaration
+call :run heap_name
+call :run struct
+call :run raw_variable_name
+call :run static_cast
+call :run multi_sentence_paragraph
+call :run condition
+call :run for_loop
+call :run while
+call :run delegate
+call :run reinterpret_cast
+
+exit /b 0
+
+:RUN
+sed " Class bstyle" " Class b3style" ..\..\..\bstyle\logic\nodes\%~1.vb > %~1.vb
+exit /b 0
