f570734fc5fbc230453d1e97f09f01a17811ab06
hzj_jie <hzj_jie@hotmail.com>
2024-03-05 23:20:01 -0800

    minor coding style changes

--- root/procedure/promise/promise.vb
+++ root/procedure/promise/promise.vb
@@ -4,9 +4,16 @@ Option Infer Off
 Option Strict On
 
 Imports osi.root.connector
+Imports osi.root.delegates
 Imports osi.root.lock
 
 Partial Public NotInheritable Class promise
+    Private Enum status
+        pending
+        fulfilled
+        rejected
+    End Enum
+
     Private ReadOnly t As thenable
     Private appended As singleentry
 
@@ -23,42 +30,58 @@ Partial Public NotInheritable Class promise
         Me.New(New thenable())
     End Sub
 
-    Public Function pending() As Boolean
-        Return t.pending()
-    End Function
-
-    Public Function fulfilled() As Boolean
-        Return t.fulfilled()
-    End Function
-
-    Public Function rejected() As Boolean
-        Return t.rejected()
-    End Function
-
-    Private Sub _resolve(ByVal result As Object)
-        t.resolve(result)
-    End Sub
-
-    Private Sub _reject(ByVal reason As Object)
-        t.reject(reason)
-    End Sub
-
     Public Function [then](ByVal on_resolve As Func(Of Object, Object), ByVal on_reject As Action(Of Object)) As promise
         assert(Not on_resolve Is Nothing)
         If Not appended.mark_in_use() Then
             Return Nothing
         End If
-        Dim r As promise = Nothing
-        r = New promise()
+        Dim r As New promise()
         t.then(Sub(ByVal result As Object)
-                   r._resolve(on_resolve(result))
+                   r.t.resolve(on_resolve(result))
                End Sub,
                Sub(ByVal reason As Object)
                    If Not on_reject Is Nothing Then
                        on_reject(reason)
                    End If
-                   r._reject(reason)
+                   r.t.reject(reason)
                End Sub)
         Return r
+    End Function
+
+    ' Nice-to-have shortcuts.
+
+    Public Sub New(ByVal executor As Action(Of Action(Of Object)))
+        Me.New(Sub(ByVal resolve As Action(Of Object), ByVal reject As Action(Of Object))
+                   assert(Not executor Is Nothing)
+                   executor(resolve)
+               End Sub)
+    End Sub
+
+    Public Sub New(ByVal executor As Func(Of Object))
+        Me.New(Sub(ByVal resolve As Action(Of Object), ByVal reject As Action(Of Object))
+                   assert(Not executor Is Nothing)
+                   resolve(executor())
+               End Sub)
+    End Sub
+
+    Public Function [then](ByVal on_resolve As Func(Of Object, Object)) As promise
+        Return [then](on_resolve, Nothing)
+    End Function
+
+    Public Function [then](ByVal on_resolve As Action(Of Object), ByVal on_reject As Action(Of Object)) As promise
+        assert(Not on_resolve Is Nothing)
+        Return [then](Function(ByVal result As Object) As Object
+                          on_resolve(result)
+                          Return result
+                      End Function,
+                      on_reject)
+    End Function
+
+    Public Function [then](ByVal on_resolve As Action(Of Object)) As promise
+        Return [then](on_resolve, Nothing)
+    End Function
+
+    Public Function [then](ByVal on_resolve As Action) As promise
+        Return [then](on_resolve.parameter_erasure(Of Object)(), Nothing)
     End Function
 End Class
