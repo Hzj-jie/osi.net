28a1357f318ddf265bbf443720db59f4eb43a2e5
hzj_jie <hzj_jie@hotmail.com>
2024-04-23 01:19:25 -0700

    dump_results.intersect

--- service/tests/ml/dump_results.vb
+++ service/tests/ml/dump_results.vb
@@ -14,6 +14,29 @@ Public NotInheritable Class dump_results
     Private Shared inputs As argument(Of vector(Of String))
     Private Shared output As argument(Of String)
 
+    Public Shared Function intersect(ByVal models As _do(Of unordered_map(Of String, Double), Boolean)) _
+                                    As unordered_map(Of String, Double)
+        assert(Not models Is Nothing)
+        Dim m As unordered_map(Of String, Double) = Nothing
+        Dim i As unordered_map(Of String, Double) = Nothing
+        While models(i)
+            If m Is Nothing Then
+                m = i
+            Else
+                Dim o As unordered_map(Of String, Double) = m
+                m = New unordered_map(Of String, Double)()
+                i.stream().foreach(Sub(ByVal p As first_const_pair(Of String, Double))
+                                       assert(Not p Is Nothing)
+                                       Dim it As unordered_map(Of String, Double).iterator = o.find(p.first)
+                                       If it <> o.end() Then
+                                           m(p.first) = p.second * (+it).second
+                                       End If
+                                   End Sub)
+            End If
+        End While
+        Return m
+    End Function
+
     <command_line_specified>
     <test>
     Private Shared Sub intersect_ssv()
@@ -31,31 +54,27 @@ Public NotInheritable Class dump_results
                     Return r
                 End Function
 
-        Dim m As unordered_map(Of String, Double) = Nothing
-        For Each i As String In ++inputs
-            If m Is Nothing Then
-                ' The first one
-                m = load_ssv(i)
-            Else
-                Dim o As unordered_map(Of String, Double) = m
-                m = New unordered_map(Of String, Double)()
-                load_ssv(i).stream().foreach(Sub(ByVal p As first_const_pair(Of String, Double))
-                                                 assert(Not p Is Nothing)
-                                                 Dim it As unordered_map(Of String, Double).iterator = o.find(p.first)
-                                                 If it <> o.end() Then
-                                                     m(p.first) = p.second * (+it).second
-                                                 End If
-                                             End Sub)
-            End If
+        Dim i As New queue(Of String)()
+        For Each s As String In ++inputs
+            assert(i.emplace(s))
         Next
-        Using code_block
-            Using w As TextWriter = If(-output, New StreamWriter(New FileStream(+output, FileMode.Create)), Console.Out)
-                Dim it As unordered_map(Of String, Double).iterator = m.begin()
-                While it <> m.end()
-                    w.WriteLine(String.Concat((+it).first, " ", (+it).second))
-                    it += 1
-                End While
-            End Using
+
+        Dim m As unordered_map(Of String, Double) = intersect(
+            Function(ByRef o As unordered_map(Of String, Double)) As Boolean
+                Dim s As String = Nothing
+                If Not i.pop(s) Then
+                    Return False
+                End If
+                o = load_ssv(s)
+                Return True
+            End Function)
+
+        Using w As TextWriter = If(-output, New StreamWriter(New FileStream(+output, FileMode.Create)), Console.Out)
+            Dim it As unordered_map(Of String, Double).iterator = m.begin()
+            While it <> m.end()
+                w.WriteLine(String.Concat((+it).first, " ", (+it).second))
+                it += 1
+            End While
         End Using
     End Sub
 End Class
