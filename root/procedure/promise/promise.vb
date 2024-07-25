0121232d71e75bc02410a1453527e76930bd97fb
Zijie He <hzj_jie@hotmail.com>
2024-07-25 15:36:42 -0700

    Reapply "minor coding style changes"
    This reverts commit 16764712d599b682555377f2f6cbdb8f25761d0c.

--- root/procedure/promise/promise.vb
+++ root/procedure/promise/promise.vb
@@ -4,6 +4,7 @@ Option Infer Off
 Option Strict On
 
 Imports osi.root.connector
+Imports osi.root.delegates
 Imports osi.root.lock
 
 Partial Public NotInheritable Class promise
@@ -23,41 +24,20 @@ Partial Public NotInheritable Class promise
         Me.New(New thenable())
     End Sub
 
-    Public Function pending() As Boolean
-        Return t.pending()
-    End Function
-
-    Public Function fulfilled() As Boolean
-        Return t.fulfilled()
-    End Function
-
-    Public Function rejected() As Boolean
-        Return t.rejected()
-    End Function
-
-    Private Sub _resolve(ByVal result As Object)
-        t.resolve(result)
-    End Sub
-
-    Private Sub _reject(ByVal reason As Object)
-        t.reject(reason)
-    End Sub
-
     Public Function [then](ByVal on_resolve As Func(Of Object, Object), ByVal on_reject As Action(Of Object)) As promise
         assert(Not on_resolve Is Nothing)
         If Not appended.mark_in_use() Then
             Return Nothing
         End If
-        Dim r As promise = Nothing
-        r = New promise()
+        Dim r As New promise()
         t.then(Sub(ByVal result As Object)
-                   r._resolve(on_resolve(result))
+                   r.t.resolve(on_resolve(result))
                End Sub,
                Sub(ByVal reason As Object)
                    If Not on_reject Is Nothing Then
                        on_reject(reason)
                    End If
-                   r._reject(reason)
+                   r.t.reject(reason)
                End Sub)
         Return r
     End Function
