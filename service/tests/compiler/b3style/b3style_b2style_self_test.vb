cdae45f8b5b032b781dadfcd492e329f14c82d8b
hzj_jie <hzj_jie@hotmail.com>
2024-07-29 01:16:34 -0700

    b3style_b2style_self_test

---
+++ service/tests/compiler/b3style/b3style_b2style_self_test.vb
@@ -0,0 +1,30 @@
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports osi.root.connector
+Imports osi.root.utt.attributes
+Imports osi.service.compiler
+Imports osi.service.interpreter.primitive
+
+<command_line_specified>
+<test>
+Public NotInheritable Class b3style_b2style_self_test
+    Inherits b2style_self_test_runner
+
+    Protected Overrides Function parse(ByVal functions As interrupts,
+                                       ByVal content As String,
+                                       ByRef e As executor) As Boolean
+        Return New b3style.parse_wrapper(functions).parse(content, e)
+    End Function
+
+    Protected Overrides Function with_current_file(ByVal filename As String) As IDisposable
+        Return b3style.parse_wrapper.with_current_file(filename)
+    End Function
+
+    Protected Overrides Function ignore_case(ByVal name As String) As Boolean
+        assert(Not name.null_or_whitespace())
+        Return False
+    End Function
+End Class
