2103a4987cee181b34bc8f254ca279ba08156f4a
hzj_jie <hzj_jie@hotmail.com>
2024-08-07 23:26:36 -0700

    compile-only

--- service/tests/compiler/b3style/b3style_compile_only_test.vb
+++ service/tests/compiler/b3style/b3style_compile_only_test.vb
@@ -12,7 +12,15 @@ Imports osi.service.resource
 Public NotInheritable Class b3style_compile_only_test
     <test>
     Private Shared Sub template_template_case1()
+        ' No main defined, but b3style should still be able to generate logic code.
         assertion.is_true(b3style.with_default_functions().generate(
                           _b2style_test_data.template_template_case1.as_text(), New logic_writer()))
+    End Sub
+
+    <test>
+    Private Shared Sub two_template_type_parameters()
+        ' No main defined, but b3style should still be able to generate logic code.
+        assertion.is_true(b3style.with_default_functions().generate(
+                          _b2style_test_data.two_template_type_parameters.as_text(), New logic_writer()))
     End Sub
 End Class
