544eb4a7a7726cfcd4c7aa4b710c0223b17032c4
hzj_jie <hzj_jie@hotmail.com>
2024-08-15 23:56:17 -0700

    merge

--- service/tests/compiler/b3style/b3style_compile_only_test.vb
+++ service/tests/compiler/b3style/b3style_compile_only_test.vb
@@ -13,14 +13,14 @@ Public NotInheritable Class b3style_compile_only_test
     <test>
     Private Shared Sub template_template_case1()
         ' No main defined, but b3style should still be able to generate logic code.
-        assertion.is_true(b3style.with_default_functions().generate(
+        assertion.is_true(b3style.with_default_functions().build(
                           _b2style_test_data.template_template_case1.as_text(), New logic_writer()))
     End Sub
 
     <test>
     Private Shared Sub two_template_type_parameters()
         ' No main defined, but b3style should still be able to generate logic code.
-        assertion.is_true(b3style.with_default_functions().generate(
+        assertion.is_true(b3style.with_default_functions().build(
                           _b2style_test_data.two_template_type_parameters.as_text(), New logic_writer()))
     End Sub
 End Class
