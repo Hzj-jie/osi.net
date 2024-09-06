af34869bc85ea5e0780f78e4eca92aab009b1161
Zijie He <hzj_jie@hotmail.com>
2024-09-06 16:11:24 -0700

    split importer_test

--- service/tests/compiler/logic/importer_test.vb
+++ service/tests/compiler/logic/importer_test.vb
@@ -7,18 +7,14 @@ Imports osi.root.constants
 Imports osi.root.connector
 Imports osi.root.formation
 Imports osi.root.utt
+Imports osi.root.utt.attributes
 Imports osi.service.interpreter.primitive
 Imports osi.service.compiler.logic
 Imports osi.service.math
 
 Namespace logic
-    Public Class importer_test
-        Inherits [case]
-
-        Private Shared ReadOnly importable_cases() As String
-        Private Shared ReadOnly not_importable_cases() As String
-        Private Shared ReadOnly cases() As pair(Of String, Byte()())
-
+    <test>
+    Public NotInheritable Class importer_test
         Private Shared Function make_case(ByVal ParamArray lines() As String) As String
             assert(Not isemptyarray(lines))
             Dim v As New vector(Of String)()
@@ -31,8 +27,17 @@ Namespace logic
             Return pair.emplace_of(make_case(lines), result)
         End Function
 
-        Shared Sub New()
-            importable_cases = {
+        <test>
+        Private Shared Function import_empty() As Boolean
+            Dim e As executor = Nothing
+            assertion.is_false(New importer().import("", e))
+            assertion.is_null(e)
+            assertion.is_false(New importer().import(Nothing, e))
+            assertion.is_null(e)
+            Return True
+        End Function
+
+        Private Shared ReadOnly importable_cases() As String = {
                 make_case("define v type*"),
                 make_case("type int 4",
                           "define v int",
@@ -40,7 +45,17 @@ Namespace logic
                           "sizeof s v")
             }
 
-            not_importable_cases = {
+        <test>
+        Private Shared Function importable() As Boolean
+            For i As UInt32 = 0 To array_size(importable_cases) - uint32_1
+                Dim e As executor = Nothing
+                assertion.is_true(New importer().import(importable_cases(CInt(i)), e))
+                assertion.is_not_null(e)
+            Next
+            Return True
+        End Function
+
+        Private Shared ReadOnly not_importable_cases() As String = {
                 make_case("define v type*",
                           "define v type*"),
                 make_case("unknown"),
@@ -48,7 +63,17 @@ Namespace logic
                           "add v v v")
             }
 
-            cases = {
+        <test>
+        Private Shared Function not_importable() As Boolean
+            For i As UInt32 = 0 To array_size(not_importable_cases) - uint32_1
+                Dim e As executor = Nothing
+                assertion.is_false(New importer().import(not_importable_cases(CInt(i)), e))
+                assertion.is_null(e)
+            Next
+            Return True
+        End Function
+
+        Private Shared ReadOnly cases() As pair(Of String, Byte()()) = {
                 make_case({(New big_uint(1000)).as_bytes(),
                            (New big_uint(1)).as_bytes(),
                            (New big_uint(1000)).as_bytes(),
@@ -71,35 +96,8 @@ Namespace logic
                           "less continue i upper_bound",
                           "}")
             }
-        End Sub
 
-        Private Shared Function import_empty() As Boolean
-            Dim e As executor = Nothing
-            assertion.is_false(New importer().import("", e))
-            assertion.is_null(e)
-            assertion.is_false(New importer().import(Nothing, e))
-            assertion.is_null(e)
-            Return True
-        End Function
-
-        Private Shared Function importable() As Boolean
-            For i As UInt32 = 0 To array_size(importable_cases) - uint32_1
-                Dim e As executor = Nothing
-                assertion.is_true(New importer().import(importable_cases(CInt(i)), e))
-                assertion.is_not_null(e)
-            Next
-            Return True
-        End Function
-
-        Private Shared Function not_importable() As Boolean
-            For i As UInt32 = 0 To array_size(not_importable_cases) - uint32_1
-                Dim e As executor = Nothing
-                assertion.is_false(New importer().import(not_importable_cases(CInt(i)), e))
-                assertion.is_null(e)
-            Next
-            Return True
-        End Function
-
+        <test>
         Private Shared Function execute_cases() As Boolean
             For i As UInt32 = 0 To array_size(cases) - uint32_1
                 Dim e As executor = Nothing
@@ -123,11 +121,7 @@ Namespace logic
             Return True
         End Function
 
-        Public Overrides Function run() As Boolean
-            Return import_empty() AndAlso
-                   importable() AndAlso
-                   not_importable() AndAlso
-                   execute_cases()
-        End Function
+        Private Sub New()
+        End Sub
     End Class
 End Namespace
