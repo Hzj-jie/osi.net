ff25ef2aefe3fb30674ff25f42f1ecbfd388d327
hzj_jie <hzj_jie@hotmail.com>
2024-09-09 20:09:54 -0700

    test

--- service/tests/compiler/b3style/b3style_test.vb
+++ service/tests/compiler/b3style/b3style_test.vb
@@ -33,7 +33,18 @@ Public NotInheritable Class b3style_test
                           _b3style_test_data.destruction_in_declaration.as_text(), e))
         assertion.is_not_null(e)
         e.assert_execute_without_errors()
-        assertion.equal(io.output(), "constructfinishdestruct")
+        assertion.equal(io.output(), String.Concat("construct", "finish", "destruct"))
+    End Sub
+
+    <test>
+    Private Shared Sub class_destruct_in_definition()
+        Dim io As New console_io.test_wrapper()
+        Dim e As executor = Nothing
+        assertion.is_true(b3style.with_functions(New interrupts(+io)).compile(
+                          _b3style_test_data.destruction_in_definition.as_text(), e))
+        assertion.is_not_null(e)
+        e.assert_execute_without_errors()
+        assertion.equal(io.output(), String.Concat("create", "construct", "finish", "destruct"))
     End Sub
 
     Private Sub New()
