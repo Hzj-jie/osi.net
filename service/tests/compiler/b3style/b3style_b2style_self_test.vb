9317edbe13c9a9b07dad7528ca1823c42b85de7c
hzj_jie <hzj_jie@hotmail.com>
2025-01-01 21:56:23 -0800

    Remove the ignore_case from b3style_b2style_self_test

--- service/tests/compiler/b3style/b3style_b2style_self_test.vb
+++ service/tests/compiler/b3style/b3style_b2style_self_test.vb
@@ -3,8 +3,6 @@ Option Explicit On
 Option Infer Off
 Option Strict On
 
-Imports osi.root.connector
-Imports osi.root.formation
 Imports osi.root.utt.attributes
 Imports osi.service.compiler
 Imports osi.service.interpreter.primitive
@@ -22,12 +20,5 @@ Public NotInheritable Class b3style_b2style_self_test
 
     Protected Overrides Function with_current_file(ByVal filename As String) As IDisposable
         Return b3style.parse_wrapper.with_current_file(filename)
-    End Function
-
-    Protected Overrides Function ignore_case(ByVal name As String) As Boolean
-        assert(Not name.null_or_whitespace())
-        Return unordered_set.of("static_cast_ptr_type.txt",
-                                "static_cast_ptr_type3.txt",
-                                "static_cast_ptr_type_bool.txt").find(name).is_not_end()
     End Function
 End Class
