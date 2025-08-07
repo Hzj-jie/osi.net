1f14e1142b889e78ba173daaf4cc96d9df7fee99
hzj_jie <hzj_jie@hotmail.com>
2025-08-07 01:32:56 -0700

    Add reinterpret_cast_to_a_different_class_type test

--- service/tests/compiler/b2style/b2style_test.vb
+++ service/tests/compiler/b2style/b2style_test.vb
@@ -787,6 +787,16 @@ Public NotInheritable Class b2style_test
         ' But it may not be handled by b2style.
     End Sub
 
+    <test>
+    Private Shared Sub reinterpret_cast_to_a_different_class_type()
+        Dim e As executor = Nothing
+        assertion.is_true(parse(New console_io.test_wrapper(),
+                                _b2style_test_data.reinterpret_cast_to_a_different_class_type.as_text(),
+                                e))
+        assertion.is_not_null(e)
+        e.assert_execute_without_errors()
+    End Sub
+
     Private Sub New()
     End Sub
 End Class
