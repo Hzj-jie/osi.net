Hzj-jie/osi.net

commit 86fad4f6e2e73862d5516628d36941619bd4bd7c

Use utt.host.forward_current_case in multithreading_case_wrapper

root/utt/case_wrapper/multithreading_case_wrapper.vb
@@ -45,10 +45,9 @@ Public Class multithreading_case_wrapper
         Return tc
     End Function
 
-    Private Sub workon(ByVal input As Object)
+    Private Sub workon(ByVal id As Int32)
         Interlocked.Increment(running_thread)
         assert(start_are.Set())
-        id = direct_cast(Of Int32)(input)
         this = Me
         Using defer.to(Sub()
                            id = npos
@@ -97,9 +96,13 @@ Public Class multithreading_case_wrapper
         Dim ts() As Thread = Nothing
         ReDim ts(CInt(threadcount()) - 1)
         For i As Int32 = 0 To CInt(threadcount()) - 1
-            ts(i) = New Thread(AddressOf workon)
+            Dim j As Int32 = i
+            ts(i) = New Thread(host.forward_current_case(Sub()
+                                                             workon(j)
+                                                         End Sub).
+                               to_thread_start())
             ts(i).Name() = "MULTITHREADING_CASE_WRAPPER_THREAD"
-            ts(i).Start(i)
+            ts(i).Start()
         Next
         Dim wait_round As Int32 = 1000
         While _dec(wait_round) > 0

root/connector/delegate/delegate2.vb
@@ -5,6 +5,7 @@ Option Strict On
 
 Imports System.Runtime.CompilerServices
 Imports System.Text
+Imports System.Threading
 Imports osi.root.constants
 
 Public Module _delegate2
@@ -84,5 +85,12 @@ Public Module _delegate2
         End If
 
         Return "#CANNOT_GET_INVOKE_IDENTITY#"
+    End Function
+
+    <Extension()> Public Function to_thread_start(ByVal a As Action) As ThreadStart
+        assert(Not a Is Nothing)
+        Return Sub()
+                   a()
+               End Sub
     End Function
 End Module

root/utt/host.run.vb
@@ -6,12 +6,12 @@ Option Strict On
 Imports System.Threading
 Imports osi.root.connector
 Imports osi.root.constants
+Imports osi.root.formation
 Imports osi.root.procedure
 Imports osi.root.utils
 Imports counter = osi.root.utils.counter
 
 Partial Friend NotInheritable Class host
-    <ThreadStatic()> Private Shared _current_case As [case]
     Private Shared expected_end_ms As Int64
     Private Shared using_threads As Int32 = 0
     Private Shared running_cases As Int32 = 0
@@ -23,11 +23,7 @@ Partial Friend NotInheritable Class host
 
     Public Shared Function execute_case(ByVal c As [case]) As Boolean
         assert(Not c Is Nothing)
-        assert(_current_case Is Nothing)
-        _current_case = c
-        Using defer.to(Sub()
-                           _current_case = Nothing
-                       End Sub)
+        Using instance_stack(Of [case], host).with(c)
             Return (assertion.is_true(do_(AddressOf c.prepare, False), "failed to prepare case ", c.full_name) AndAlso
                     assertion.is_true(do_(AddressOf c.run, False), "failed to run case ", c.full_name)) And
                    assertion.is_true(do_(AddressOf c.finish, False), "failed to finish case ", c.full_name)
@@ -38,21 +34,21 @@ Partial Friend NotInheritable Class host
     ' Thread.post(forward_current_case(d))
     Public Shared Function forward_current_case(ByVal d As Action) As Action
         assert(Not d Is Nothing)
-        Dim c As [case] = current_case()
+        Dim c As [case] = instance_stack(Of [case], host).current()
         assert(Not c Is Nothing)
         Return Sub()
-                   assert(_current_case Is Nothing)
-                   _current_case = c
-                   Using defer.to(Sub()
-                                      _current_case = Nothing
-                                  End Sub)
+                   Using instance_stack(Of [case], host).with(c)
                        d()
                    End Using
                End Sub
     End Function
 
     Public Shared Function current_case() As [case]
-        Return _current_case
+        Dim r As [case] = Nothing
+        If instance_stack(Of [case], host).back(r) Then
+            Return r
+        End If
+        Return Nothing
     End Function
 
     Private Shared Function execute_case(ByVal c As case_info) As Boolean

commit ca4187b9148e918c62ec1633f91afe739f917564

multithreading_case_wrapper was broken by https://github.com/Hzj-jie/osi.net/commit/86fad4f6e2e73862d5516628d36941619bd4bd7c

root/utt/case_wrapper/multithreading_case_wrapper.vb
@@ -48,6 +48,7 @@ Public Class multithreading_case_wrapper
     Private Sub workon(ByVal id As Int32)
         Interlocked.Increment(running_thread)
         assert(start_are.Set())
+        multithreading_case_wrapper.id = id
         this = Me
         Using defer.to(Sub()
                            id = npos