e61ed73d8f83253ad69ac665bac560df93d190e3
hzj_jie <hzj_jie@hotmail.com>
2025-08-20 23:08:30 -0700

    ifdef

--- service/compiler/b2style/lib/b2style/loaded_method.h
+++ service/compiler/b2style/lib/b2style/loaded_method.h
@@ -11,35 +11,31 @@ void load_method(string m) {
   ::load_method(m);
 }
 
+template <T>
+T execute_loaded_method() {
+  T result;
 #ifdef B3STYLE
-template <T>
-T execute_loaded_method() {
-  T result;
   logic "interrupt execute_loaded_method @@prefixes@temps@string result";
-  return result;
-}
-
-template <T, RT>
-RT execute_loaded_method(T p) {
-  RT result;
-  logic "interrupt execute_loaded_method p result";
-  return result;
-}
-#else
-template <T>
-T execute_loaded_method() {
-  T result;
+#else #ifdef B2STYLE
   logic "interrupt execute_loaded_method @@prefixes@temps@string b2style__result";
+#else
+  // TODO: Trigger an #error
+#endif
   return result;
 }
 
 template <T, RT>
 RT execute_loaded_method(T p) {
   RT result;
+#ifdef B3STYLE
+  logic "interrupt execute_loaded_method p result";
+#else #ifdef B2STYLE
   logic "interrupt execute_loaded_method b2style__p b2style__result";
+#else
+  // TODO: Trigger an #error
+#endif
   return result;
 }
-#endif
 	
 }  // namespace b2style
 
