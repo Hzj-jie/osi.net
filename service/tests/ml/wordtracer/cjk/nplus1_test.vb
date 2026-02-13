5b94a21d0ad9d751815d4ffb428867ea00984334
Hzj_jie <hzj_jie@hotmail.com>
2026-02-13 08:01:15 -0800

    do not use concurrency_runner in nplus1_test

--- service/tests/ml/wordtracer/cjk/nplus1_test.vb
+++ service/tests/ml/wordtracer/cjk/nplus1_test.vb
@@ -3,7 +3,6 @@ Option Explicit On
 Option Infer Off
 Option Strict On
 
-Imports osi.root.connector
 Imports osi.root.constants
 Imports osi.root.delegates
 Imports osi.root.utils
@@ -42,70 +41,60 @@ Namespace wordtracer.cjk
         <command_line_specified>
         Private Shared Sub from_tar_2()
             Dim num_of_shards As UInt32 = (nplus1_test.num_of_shards Or 8)
-            concurrency_runner.execute(
-                num_of_shards,
-                Sub(ByVal i As UInt32)
-                    Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 2)
-                    n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
-                    n.dump(percentage Or 0.9).
-                      dump(String.Concat(output Or "cjk.nplus1.2", ".", i, ".bin"))
-                End Sub)
+            For i As UInt32 = 0 To num_of_shards - uint32_1
+                Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 2)
+                n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
+                n.dump(percentage Or 0.9).
+                  dump(String.Concat(output Or "cjk.nplus1.2", ".", i, ".bin"))
+            Next
         End Sub
 
         <test>
         <command_line_specified>
         Private Shared Sub from_tar_raw_2()
             Dim num_of_shards As UInt32 = (nplus1_test.num_of_shards Or 257)
-            concurrency_runner.execute(
-                num_of_shards,
-                Sub(ByVal i As UInt32)
-                    Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 2)
-                    n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
-                    n.dump_raw(percent Or 0.2).
-                      dump(String.Concat(output Or "cjk.nplus1.2", ".", i, ".raw.bin"))
-                End Sub)
+            For i As UInt32 = 0 To num_of_shards - uint32_1
+                Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 2)
+                n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
+                n.dump_raw(percent Or 0.2).
+                  dump(String.Concat(output Or "cjk.nplus1.2", ".", i, ".raw.bin"))
+            Next
         End Sub
 
         <test>
         <command_line_specified>
         Private Shared Sub from_tar_3()
             Dim num_of_shards As UInt32 = (nplus1_test.num_of_shards Or 32)
-            concurrency_runner.execute(
-                num_of_shards,
-                Sub(ByVal i As UInt32)
-                    Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 3)
-                    n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
-                    n.dump(percentage Or 0.9).
-                      dump(String.Concat(output Or "cjk.nplus1.3", ".", i, ".bin"))
-                End Sub)
+            For i As UInt32 = 0 To num_of_shards - uint32_1
+                Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 3)
+                n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
+                n.dump(percentage Or 0.9).
+                  dump(String.Concat(output Or "cjk.nplus1.3", ".", i, ".bin"))
+            Next
         End Sub
 
         <test>
         <command_line_specified>
         Private Shared Sub from_tar_raw_3()
             Dim num_of_shards As UInt32 = (nplus1_test.num_of_shards Or 1031)
-            concurrency_runner.execute(
-                num_of_shards,
-                Sub(ByVal i As UInt32)
-                    Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 3)
-                    n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
-                    n.dump_raw(percent Or 0.2).
-                      dump(String.Concat(output Or "cjk.nplus1.3", ".", i, ".raw.bin"))
-                End Sub)
+            For i As UInt32 = 0 To num_of_shards - uint32_1
+                Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 3)
+                n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
+                n.dump_raw(percent Or 0.2).
+                  dump(String.Concat(output Or "cjk.nplus1.3", ".", i, ".raw.bin"))
+            Next
         End Sub
 
         <test>
         <command_line_specified>
         Private Shared Sub from_tar_4()
             Dim num_of_shards As UInt32 = (nplus1_test.num_of_shards Or 128)
-            concurrency_runner.execute(
-                num_of_shards,
-                Sub(ByVal i As UInt32)
-                    Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 4)
-                    n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
-                    n.dump(percentage Or 0.9).
-                      dump(String.Concat(output Or "cjk.nplus1.4", ".", i, ".bin"))
-                End Sub)
+            For i As UInt32 = 0 To num_of_shards - uint32_1
+                Dim n As New nplus1(New shard(Of String)(i, num_of_shards), 4)
+                n.train(tar.reader.unzip(New tar.selector() With {.pattern = input Or "tar_manual_test.zip_*"}))
+                n.dump(percentage Or 0.9).
+                  dump(String.Concat(output Or "cjk.nplus1.4", ".", i, ".bin"))
+            Next
         End Sub
     End Class
 End Namespace
