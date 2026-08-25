a04d9f1419854da4bd5a5ab7fc7e21bb2c03e645
zijiehe-google-com <zijiehe@google.com>
2026-08-24 22:24:44 -0700

    Update reference_count_runner_test2 with Options, NotInheritable, and NET8_0_OR_GREATER conditional thread termination

--- root/tests/utils/reference_count_runner_test2.vb
+++ root/tests/utils/reference_count_runner_test2.vb
@@ -1,10 +1,14 @@
 
+Option Explicit On
+Option Infer Off
+Option Strict On
+
 Imports System.Threading
 Imports osi.root.connector
 Imports osi.root.utils
 Imports osi.root.utt
 
-Public Class reference_count_runner_test2
+Public NotInheritable Class reference_count_runner_test2
     Inherits [case]
 
     Public Overrides Function run() As Boolean
@@ -16,22 +20,36 @@ Public Class reference_count_runner_test2
                                            this.mark_started()
                                            sleep(10)
                                        End Sub)
+#If NET8_0_OR_GREATER Then
+        ' Thread.Abort() is not supported on .NET 8+ / CoreCLR and unconditionally throws
+        ' PlatformNotSupportedException. Terminate the background thread cooperatively.
         Dim stop_thread As Boolean = False
+#End If
         Dim t As Thread = Nothing
         t = New Thread(Sub()
+#If NET8_0_OR_GREATER Then
                            While Not stop_thread
                                If are.wait(50) Then
                                    r.mark_stopped()
                                End If
                            End While
+#Else
+                           While True
+                               assert(are.wait())
+                               r.mark_stopped()
+                           End While
+#End If
                        End Sub)
         t.Start()
         For i As Int32 = 0 To 1000
             assertion.is_true(r.bind())
             assertion.is_true(r.release())
         Next
+#If NET8_0_OR_GREATER Then
         stop_thread = True
-        are.force_set()
+#Else
+        t.Abort()
+#End If
         t.Join()
         are.Close()
         Return True
